88808810e8079b464c7a97e0c3fab42b3576fd04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 15:04:07 +0000

    Add package docext/0.1.10/docext-0.1.10.tar.gz

---
+++ packages/docext/docext-0.1.10.tar.gz/docext-0.1.10/docext/app/args.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="DocExt: Onprem information extraction from documents",
+    )
+    parser.add_argument(
+        "--vlm_server_port",
+        type=int,
+        default=8000,
+        help="Port for the vLLM/OLLAMA server",
+    )
+    parser.add_argument(
+        "--vlm_server_host",
+        type=str,
+        default="0.0.0.0",
+        help="Host for the vLLM/OLLAMA server",
+    )
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="hosted_vllm/Qwen/Qwen2.5-VL-7B-Instruct-AWQ",
+        help="Name of the model to use. Use 'ollama/' prefix for OLLAMA models and 'hosted_vllm/' prefix for hosted vLLM models.",
+    )
+    parser.add_argument(
+        "--ui_port",
+        type=int,
+        default=7860,
+        help="Port for the gradio UI",
+    )
+    parser.add_argument(
+        "--max_model_len",
+        type=int,
+        default=15000,
+        help="Maximum length of the model. Use small values for low memory devices.",
+    )
+    parser.add_argument(
+        "--gpu_memory_utilization",
+        type=float,
+        default=0.9,
+        help="GPU memory utilization. Use a value between 0 and 1.",
+    )
+    parser.add_argument(
+        "--max_num_imgs",
+        type=int,
+        default=5,
+        help="Maximum number of images to process in a single prompt.",
+    )
+    parser.add_argument(
+        "--vllm_start_timeout",
+        type=int,
+        default=300,
+        help="Timeout for the vLLM server to start.",
+    )
+    parser.add_argument(
+        "--no-share",
+        action="store_true",
+        dest="share",  # This will set 'share' to False when --no-share is used
+        help="Disable sharing of the UI on the web.",
+    )
+    parser.add_argument(
+        "--log_level",
+        type=str,
+        default="debug",
+        help="Log level. Can be 'debug', 'info', 'warning', 'error', 'critical'.",
+    )
+    parser.add_argument(
+        "--max_img_size",
+        type=int,
+        default=2048,
+        help="Maximum size of the image to process. Use 1024 for low memory devices.",
+    )
+    parser.add_argument(
+        "--concurrency_limit",
+        type=int,
+        default=1,
+        help="Maximum number of concurrent requests. Increase this value if you want to process more requests in parallel.",
+    )
+    return parser.parse_args()
---
+++ packages/docext/docext-0.1.10.tar.gz/docext-0.1.10/docext/core/extract.py
@@ -0,0 +1,134 @@
+from __future__ import annotations
+
+from concurrent.futures import ThreadPoolExecutor
+from typing import Dict
+from typing import Union
+
+import json_repair
+import mdpd
+import pandas as pd
+from loguru import logger
+
+from docext.core.client import sync_request
+from docext.core.confidence import get_fields_confidence_score_messages_binary
+from docext.core.prompts import get_fields_messages
+from docext.core.prompts import get_tables_messages
+from docext.core.utils import resize_images
+from docext.core.utils import validate_fields_and_tables
+from docext.core.utils import validate_file_paths
+
+
+def extract_fields_from_documents(
+    file_paths: list[str],
+    model_name: str,
+    fields: list[dict],
+):
+    if len(fields) == 0:
+        return pd.DataFrame()
+    field_names = [field["name"] for field in fields]
+    fields_description = [field.get("description", "") for field in fields]
+    messages = get_fields_messages(field_names, fields_description, file_paths)
+
+    format_fields = {
+        "type": "object",
+        "properties": {field_name: {"type": "string"} for field_name in field_names},
+    }
+
+    logger.info(f"Sending request to {model_name}")
+    response = sync_request(messages, model_name, format=format_fields)["choices"][0][
+        "message"
+    ]["content"]
+    logger.info(f"Response: {response}")
+
+    # conf score
+    messages = get_fields_confidence_score_messages_binary(
+        messages,
+        response,
+        field_names,
+    )
+
+    format_fields_conf_score = {
+        "type": "object",
+        "properties": {
+            field_name: {"type": "string", "enum": ["High", "Low"]}
+            for field_name in field_names
+        },
+    }
+
+    response_conf_score = sync_request(
+        messages,
+        model_name,
+        format=format_fields_conf_score,
+    )["choices"][0]["message"]["content"]
+    logger.info(f"Response conf score: {response_conf_score}")
+
+    extracted_fields = json_repair.loads(response)
+    conf_scores = json_repair.loads(response_conf_score)
+
+    df = pd.DataFrame(
+        {
+            "fields": field_names,
+            "answer": [extracted_fields.get(field, "") for field in field_names],
+            "confidence": [conf_scores.get(field, "Low") for field in field_names],
+        },
+    )
+    return df
+
+
+def extract_tables_from_documents(
+    file_paths: list[str],
+    model_name: str,
+    columns: list[dict],
+):
+    if len(columns) == 0:
+        return pd.DataFrame()
+    columns_names = [column["name"] for column in columns if column["type"] == "table"]
+    columns_description = [
+        column.get("description", "") for column in columns if column["type"] == "table"
+    ]
+    messages = get_tables_messages(columns_names, columns_description, file_paths)
+
+    logger.info(f"Sending request to {model_name}")
+    response = sync_request(messages, model_name)["choices"][0]["message"]["content"]
+    logger.info(f"Response: {response}")
+
+    response = response[response.index("|") : response.rindex("|") + 1]
+    df = mdpd.from_md(response)
+
+    return df
+
+
+def extract_information(
+    file_inputs: list[tuple],
+    model_name: str,
+    max_img_size: int,
+    fields_and_tables: dict[str, list[dict]] | pd.DataFrame,
+):
+    fields_and_tables = validate_fields_and_tables(fields_and_tables)
+    if len(fields_and_tables["fields"]) == 0 and len(fields_and_tables["tables"]) == 0:
+        return pd.DataFrame(), pd.DataFrame()
+    file_paths: list[str] = [
+        file_input[0] if isinstance(file_input, tuple) else file_input
+        for file_input in file_inputs
+    ]
+    validate_file_paths(file_paths)
+    resize_images(file_paths, max_img_size)
+
+    # call fields and tables extraction in parallel
+    with ThreadPoolExecutor() as executor:
+        future_fields = executor.submit(
+            extract_fields_from_documents,
+            file_paths,
+            model_name,
+            fields_and_tables["fields"],
+        )
+        future_tables = executor.submit(
+            extract_tables_from_documents,
+            file_paths,
+            model_name,
+            fields_and_tables["tables"],
+        )
+
+        fields_df = future_fields.result()
+        tables_df = future_tables.result()
+    return fields_df, tables_df
