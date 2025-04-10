fe0cb785f39ff84612a7f97f9eca7df2ecf7d6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:03:51 +0000

    Add package yhddnotification/3.0.0/yhddnotification-3.0.0.tar.gz

---
+++ packages/yhddnotification/yhddnotification-3.0.0.tar.gz/yhddnotification-3.0.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="yhddnotification",
+    version="3.0.0",
+    description="test mode",
+    packages=["yunhan"],
+    py_modules=["yunhan.main"],
+    author="BrYan",
+    author_email="mail0426@163.com",
+    url="https://www.qq.com",
+    long_description="this is test module"
+)
