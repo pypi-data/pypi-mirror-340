1155935e3504f1deb8312da6727a5335c81923a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 15:04:09 +0000

    Add package prefect/3.3.4.dev3/prefect-3.3.4.dev3.tar.gz

---
+++ packages/prefect/prefect-3.3.4.dev3.tar.gz/prefect-3.3.4.dev3/src/prefect/server/ui/assets/404-BI4BUowC.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,p as t,dB as r}from"./index-3fXafZ5y.js";import{u as s}from"./usePageTitle-C9Pn2rAa.js";const m=e({__name:"404",setup(n){return s("Page Not Found"),(c,p)=>(a(),o(t(r)))}});export{m as default};
+//# sourceMappingURL=404-BI4BUowC.js.map
---
+++ packages/prefect/prefect-3.3.4.dev3.tar.gz/prefect-3.3.4.dev3/src/prefect/server/ui/assets/usePrefectApi-dhzta1eN.js
@@ -0,0 +1,2 @@
+import{cC as r}from"./index-3fXafZ5y.js";import{p as e}from"./api-XlNkKBhr.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-dhzta1eN.js.map
