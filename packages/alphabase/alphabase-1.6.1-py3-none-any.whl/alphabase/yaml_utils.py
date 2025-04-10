11ba32cdb890e7af6a153be04a599ef8cc9dbc40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 16:04:14 +0000

    Add package alphabase/1.6.1/alphabase-1.6.1-py3-none-any.whl

---
+++ packages/alphabase/alphabase-1.6.1-py3-none-any.whl/alphabase/yaml_utils.py
@@ -0,0 +1,12 @@
+import yaml
+
+
+def load_yaml(filename) -> dict:
+    with open(filename) as f:
+        settings = yaml.load(f, Loader=yaml.FullLoader)
+    return settings
+
+
+def save_yaml(filename, settings):
+    with open(filename, "w") as file:
+        yaml.dump(settings, file, sort_keys=False)
