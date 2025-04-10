5754ab62688e53dc160faa4a476a43002e7a15cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:03:58 +0000

    Add package coverdl/0.2.6/coverdl-0.2.6-py3-none-any.whl

---
+++ packages/coverdl/coverdl-0.2.6-py3-none-any.whl/coverdl/providers/provider.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from coverdl.providers.source import Source
+from dataclasses import dataclass
+
+@dataclass
+class Cover:
+    artist: str
+    title: str
+    source: Source
+    cover_url: str
+    confidence: int = 1
+
+class Provider(ABC):
+    base_url: str
+    source: Source
+
+    def __init__(self, base_url: str, source: Source):
+        self.base_url = base_url
+        self.source = source
+
+    @abstractmethod
+    def get_covers(self, artist: str, album: str, country: str) -> list[Cover]:
+        pass
