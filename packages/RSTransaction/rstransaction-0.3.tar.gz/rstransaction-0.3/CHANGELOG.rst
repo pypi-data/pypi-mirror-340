89d76e8e760c18f75b77c0cf2d969dc89c91cf85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 15:04:50 +0000

    Add package RSTransaction/0.3/rstransaction-0.3.tar.gz

---
+++ packages/RSTransaction/rstransaction-0.3.tar.gz/rstransaction-0.3/CHANGELOG.rst
@@ -0,0 +1,17 @@
+
+Rstransaction 0.3
+======================
+
+- Drop Python2 support, and check Python 3.7->3.13 support
+
+
+Rstransaction 0.2
+======================
+
+- Add Tox testing
+
+
+Rstransaction 0.1
+======================
+
+- Initial implementation of the transaction core, supporting python2.7 and python3.3+.
---
+++ packages/RSTransaction/rstransaction-0.3.tar.gz/rstransaction-0.3/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys, os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # security
+
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+classifiers = """\
+Development Status :: 2 - Pre-Alpha
+Intended Audience :: Developers
+Intended Audience :: Information Technology
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Microsoft :: Windows
+Operating System :: Unix
+Operating System :: MacOS :: MacOS X
+"""
+
+
+setup(
+    name='RSTransaction',
+    version=read("VERSION"),
+    author='Pascal Chambon',
+    author_email='pythoniks@gmail.com',
+    url='https://github.com/pakal/rstransaction',
+    license="http://www.opensource.org/licenses/mit-license.php",
+    platforms=["any"],
+    description="Extendable transaction system, to build workflows with commit/rollback semantics.",
+    classifiers=filter(None, classifiers.split("\n")),
+    long_description=read("README.rst"),
+    packages=("rstransaction",),
+)
+
