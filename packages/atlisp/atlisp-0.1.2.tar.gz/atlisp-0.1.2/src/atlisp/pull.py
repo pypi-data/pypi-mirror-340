4ecadc1818594498f2c9558d8aa19a28657798c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 16:04:10 +0000

    Add package atlisp/0.1.2/atlisp-0.1.2.tar.gz

---
+++ packages/atlisp/atlisp-0.1.2.tar.gz/atlisp-0.1.2/src/atlisp/pull.py
@@ -0,0 +1,15 @@
+import win32com.client
+import time
+
+def pull(pkgname):
+    acadapp =win32com.client.Dispatch("AutoCAD.application")
+    # 等待CAD忙完
+    while (not acadapp.GetAcadState().IsQuiescent):
+        time.sleep(1)
+    acadapp.ActiveDocument.SendCommand('(progn(vl-load-com)(setq s strcat h "http" o(vlax-create-object (s"win"h".win"h"request.5.1"))v vlax-invoke e eval r read)(v o(quote open) "get" (s h"://atlisp.""cn/@"):vlax-true)(v o(quote send))(v o(quote WaitforResponse) 1000)(e(r(vlax-get-property o(quote ResponseText))))) ')
+    acadapp.ActiveDocument.SendCommand('(@::package-install '+ pkgname)
+    while (not acadapp.GetAcadState().IsQuiescent):
+        time.sleep(1)
+    acadapp.ActiveDocument.Close(False)
+    acadapp.Quit()
+    
