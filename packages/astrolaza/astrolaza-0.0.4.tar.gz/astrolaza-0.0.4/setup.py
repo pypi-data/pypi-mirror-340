882e0c91020470329f1881df4fe795378103ae41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 17:04:02 +0000

    Add package astrolaza/0.0.4/astrolaza-0.0.4.tar.gz

---
+++ packages/astrolaza/astrolaza-0.0.4.tar.gz/astrolaza-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from setuptools import setup,find_packages
+
+setup(
+	name='astrolaza',
+	version='0.0.4',
+	author='A. Laza-Ramos',
+	author_email='andres.laza@uv.es',
+	packages=setuptools.find_packages(),
+	install_requires=["numpy>=2.1.0","matplotlib>=3.8.0","bagpipes>=1.1.0","scipy>=1.15.0","astropy>=7.0.1","natsort>=8.4","moviepy>=2.0.0"],
+	include_package_data = True,
+    	classifiers=(
+        	"Programming Language :: Python :: 3",
+        	"License :: OSI Approved :: MIT License",
+        	"Operating System :: OS Independent",
+        	),
+        )
+        	
+		
+#[testpypi]
+#  username = __token__
+#  password = pypi-AgENdGVzdC5weXBpLm9yZwIkYjdiYTc5MWYtMjkyMi00M2NiLThmYzUtOWZmN2VmMWVjYTEyAAIqWzMsIjYxNDU0ZTc2LWU5ZTItNDJkMy04OWVmLTljNWY4ZjQ0YWFiNyJdAAAGIIUJLhIVHhAxzDoMFDNMeQmNobTuY0zlhRXRxz22s_f_	
+	
+
