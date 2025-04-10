5b371e5099d292ff9d9e1dad9e8e1be0b906d3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 16:04:32 +0000

    Add package ormy/0.1.22/ormy-0.1.22-py3-none-any.whl

---
+++ packages/ormy/ormy-0.1.22-py3-none-any.whl/ormy/document/arango/__init__.py
@@ -0,0 +1,13 @@
+from .config import ArangoConfig, ArangoCredentials, ArangoGraphConfig
+from .wrapper import ArangoBase, ArangoBaseEdge, ArangoGraph
+
+# ----------------------- #
+
+__all__ = [
+    "ArangoConfig",
+    "ArangoCredentials",
+    "ArangoGraphConfig",
+    "ArangoBase",
+    "ArangoBaseEdge",
+    "ArangoGraph",
+]
