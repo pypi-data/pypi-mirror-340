8b1005bb0267a0dea128aed3e525492922713790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 15:04:11 +0000

    Add package observa/1.1.1/observa-1.1.1.tar.gz

---
+++ packages/observa/observa-1.1.1.tar.gz/observa-1.1.1/observa/__init__.py
@@ -0,0 +1,3 @@
+from .observer import LangChainObserver, ObserverConfig, SessionConfig, TracerConfig
+
+__all__ = ['LangChainObserver', 'ObserverConfig', 'SessionConfig', 'TracerConfig']
---
+++ packages/observa/observa-1.1.1.tar.gz/observa-1.1.1/observa/config.py
@@ -0,0 +1,5 @@
+import os
+
+API_KEY = os.getenv('API_KEY', '')
+API_HOST = os.getenv('API_HOST', 'https://observa-server.steamory.com')
+OUTPUT_LOG_SWITCH = os.getenv('OUTPUT_LOG_SWITCH', 'false').lower() == 'true'
