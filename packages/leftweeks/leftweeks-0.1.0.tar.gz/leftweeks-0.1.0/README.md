6bf6dbc04c9a9d1c3ec2c19b76c45fcf05e1aa3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:03:45 +0000

    Add package leftweeks/0.1.0/leftweeks-0.1.0.tar.gz

---
+++ packages/leftweeks/leftweeks-0.1.0.tar.gz/leftweeks-0.1.0/README.md
@@ -0,0 +1,9 @@
+# Left of weeks
+
+How many weeks you have in your life.
+
+## file structure
+
+- cli.py: contains the Click command line
+- interface.plot.py: contains the plotting code.
+- utils.py: contains a few miscellaneous functions for validation and filename handling.
---
+++ packages/leftweeks/leftweeks-0.1.0.tar.gz/leftweeks-0.1.0/src/leftweeks/cli.py
@@ -0,0 +1,34 @@
+import click
+from leftweeks.plot import create_life_plot
+from leftweeks.utils import validate_date, process_filename
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("lifeweeks")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.argument("birthday", type=str)
+@click.option(
+    "--output",
+    "-o",
+    default="lifeweeks.pdf",
+    help="Output filename (default: lifeweeks.pdf)",
+)
+@click.version_option(
+    __version__, "-v", "--version", help="Show the version and exit."
+)
+def main(birthday, output):
+    """
+    \b
+    Generate a life-in-weeks chart for a given BIRTHDAY.
+    Birthday should be entered as YYYY-MM-DD.
+
+    Example: lifeweeks 1980-02-14
+    """
+    validate_date(birthday)
+    output_filename = process_filename(output)
+    create_life_plot(birthday, output_filename)
+    click.echo(f"Life-in-weeks chart saved to {output_filename}")
