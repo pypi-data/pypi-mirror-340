babd7161db01a22b9eec0702a7fcb15818914e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 16:04:23 +0000

    Add package pythonPackages-rgoyal/1.0.0/pythonpackages_rgoyal-1.0.0.tar.gz

---
+++ packages/pythonPackages-rgoyal/pythonpackages_rgoyal-1.0.0.tar.gz/pythonpackages_rgoyal-1.0.0/src/calculator/main.py
@@ -0,0 +1,5 @@
+from .operations import sub
+
+def main():
+    print("main file ran 🤘🏻")
+    print(sub(4,3))
---
+++ packages/pythonPackages-rgoyal/pythonpackages_rgoyal-1.0.0.tar.gz/pythonpackages_rgoyal-1.0.0/src/calculator/operations.py
@@ -0,0 +1,12 @@
+def add (*args):
+    return sum(args)
+
+def sub(a,b):
+    return a-b
+
+def multiply(*args):
+    return multiply(args)
+
+def divide(a,b):
+    return a/b
+
