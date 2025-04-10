e4fcdb9ca8bb469022609f22ca7befd0aadff7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:04:18 +0000

    Add package clipx/0.1.14/clipx-0.1.14.tar.gz

---
+++ packages/clipx/clipx-0.1.14.tar.gz/clipx-0.1.14/clipx/__init__.py
@@ -0,0 +1,8 @@
+"""
+Clipx - Image background removal and mask generation tool.
+"""
+
+from clipx.core import Clipx
+
+__version__ = '0.1.14'
+__all__ = ['Clipx']
---
+++ packages/clipx/clipx-0.1.14.tar.gz/clipx-0.1.14/clipx/models/cascadepsp/__init__.py
@@ -0,0 +1,8 @@
+"""
+CascadePSP module for segmentation refinement.
+Original project: https://github.com/hkchengrex/CascadePSP
+"""
+
+from clipx.models.cascadepsp.model import CascadePSPModel
+
+__all__ = ['CascadePSPModel']
