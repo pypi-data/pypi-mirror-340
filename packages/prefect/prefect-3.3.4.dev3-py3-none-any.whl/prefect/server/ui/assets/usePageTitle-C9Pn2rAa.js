cac4efa3e4ae129b7a48e3594ae5da8e42bd7ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 15:04:09 +0000

    Add package prefect/3.3.4.dev3/prefect-3.3.4.dev3-py3-none-any.whl

---
+++ packages/prefect/prefect-3.3.4.dev3-py3-none-any.whl/prefect/server/ui/assets/usePageTitle-C9Pn2rAa.js
@@ -0,0 +1,2 @@
+import{j as a,p as c,bw as n}from"./index-3fXafZ5y.js";function i(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>c(e)).filter(e=>e!==null).join(" • "));n(()=>document.title=o.value)}export{i as u};
+//# sourceMappingURL=usePageTitle-C9Pn2rAa.js.map
