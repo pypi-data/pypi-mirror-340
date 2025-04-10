019af1e52923d33088bd3ea5521273dff2d79ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 16:03:52 +0000

    Add package essreduce/25.4.1/essreduce-25.4.1-py3-none-any.whl

---
+++ packages/essreduce/essreduce-25.4.1-py3-none-any.whl/ess/reduce/time_of_flight/__init__.py
@@ -0,0 +1,47 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
+
+"""
+Utilities for computing real neutron time-of-flight from chopper settings and
+neutron time-of-arrival at the detectors.
+"""
+
+from .eto_to_tof import default_parameters, providers, resample_tof_data
+from .simulation import simulate_beamline
+from .to_events import to_events
+from .types import (
+    DistanceResolution,
+    LookupTableRelativeErrorThreshold,
+    Ltotal,
+    LtotalRange,
+    PulsePeriod,
+    PulseStride,
+    PulseStrideOffset,
+    RawData,
+    ResampledTofData,
+    SimulationResults,
+    TimeOfFlightLookupTable,
+    TimeResolution,
+    TofData,
+)
+
+__all__ = [
+    "DistanceResolution",
+    "LookupTableRelativeErrorThreshold",
+    "Ltotal",
+    "LtotalRange",
+    "PulsePeriod",
+    "PulseStride",
+    "PulseStrideOffset",
+    "RawData",
+    "ResampledTofData",
+    "SimulationResults",
+    "TimeOfFlightLookupTable",
+    "TimeResolution",
+    "TofData",
+    "default_parameters",
+    "providers",
+    "resample_tof_data",
+    "simulate_beamline",
+    "to_events",
+]
