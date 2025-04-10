3319f0b0384dcf3a4013df07247fb8d5feda018d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:04:10 +0000

    Add package clipx/0.1.16/clipx-0.1.16-py3-none-any.whl

---
+++ packages/clipx/clipx-0.1.16-py3-none-any.whl/clipx/__init__.py
@@ -0,0 +1,5 @@
+from clipx.core import Clipx
+from clipx.logging import set_log_level, enable_console_logging
+
+__version__ = '0.1.16'
+__all__ = ['Clipx', 'set_log_level', 'enable_console_logging']
