6e63d99f6128237eceeb62206d2284a3d4831e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 15:04:22 +0000

    Add package aspubsub/0.1.1/aspubsub-0.1.1.tar.gz

---
+++ packages/aspubsub/aspubsub-0.1.1.tar.gz/aspubsub-0.1.1/src/aspubsub/__init__.py
@@ -0,0 +1,20 @@
+"""
+Asynchronous PubSub interface for python web frameworks.
+"""
+
+__version__ = "0.1.1"
+
+
+from loguru import logger
+logger.disable(__name__)
+
+from . import backends
+
+from .aspubsub import (
+    ClientNotFound,
+    ChannelOrPatternNotFound,
+    ConnectionObjectNotProvided,
+    DumbBroadcaster,
+    GeneralPurposePubSubManager,
+)
+
