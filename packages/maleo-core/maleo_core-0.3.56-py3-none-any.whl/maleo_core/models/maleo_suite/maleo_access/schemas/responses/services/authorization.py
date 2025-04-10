e41cbad5618dddd7f765482f3b51cbc6ad3a77ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:04:02 +0000

    Add package maleo-core/0.3.56/maleo_core-0.3.56-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.3.56-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_access/schemas/responses/services/authorization.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from maleo_core.models.base.schemas.responses.general import BaseGeneralResponsesSchemas
+from maleo_core.models.maleo_suite.maleo_access.transfers.results.service.authorization import MaleoAccessAuthorizationServiceResults
+
+class MaleoAccessAuthorizationServiceResponsesSchemas:
+    #* ----- ----- Response ----- ----- *#
+    class FailResponse(BaseGeneralResponsesSchemas.Fail):
+        code:str = "ACC-ATH-001"
+        message:str = "Authorization Failed"
+        description:str = "External error: Authorization failed"
+        other:str = "Ensure parameter(s) are correct"
+
+    class LoginResponse(BaseGeneralResponsesSchemas.SingleData):
+        code:str = "ACC-ATH-002"
+        message:str = "Login Successful"
+        description:str = "Login attempt is successful"
+        data:MaleoAccessAuthorizationServiceResults.LoginData
+
+    class LogoutResponse(BaseGeneralResponsesSchemas.SingleData):
+        code:str = "ACC-ATH-003"
+        message:str = "Login Failed"
+        description:str = "Logout attempt is successful"
+        data:None
+
+    #* ----- ----- Responses Class ----- ----- *#
+    other_responses={
+        **FailResponse,
+        **BaseGeneralResponsesSchemas.other_responses
+    }
