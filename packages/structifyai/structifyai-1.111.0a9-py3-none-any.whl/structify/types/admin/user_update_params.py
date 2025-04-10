3a986b90a11fe7911f47ef746d35d7f63fffd1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:04:10 +0000

    Add package structifyai/1.111.0a9/structifyai-1.111.0a9-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.111.0a9-py3-none-any.whl/structify/types/admin/user_update_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["UserUpdateParams"]
+
+
+class UserUpdateParams(TypedDict, total=False):
+    current_email: Required[str]
+
+    new_email: Optional[str]
+
+    new_feature_flags: Optional[
+        List[Literal["functional_test", "pdf_parsing", "boredm_construction_model", "generic_suspicious_queue", "none"]]
+    ]
+
+    new_permissions: Optional[List[Literal["labeler", "qa_labeler", "debug", "human_llm", "none"]]]
---
+++ packages/structifyai/structifyai-1.111.0a9-py3-none-any.whl/structify/types/dataset_view_table_params.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Union, Optional
+from datetime import datetime
+from typing_extensions import Literal, Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["DatasetViewTableParams", "SortBy"]
+
+
+class DatasetViewTableParams(TypedDict, total=False):
+    dataset: Required[str]
+
+    name: Required[str]
+
+    job_id: Optional[str]
+
+    last_updated: Annotated[Union[str, datetime, None], PropertyInfo(format="iso8601")]
+
+    limit: int
+
+    offset: int
+
+    sort_by: SortBy
+
+
+class SortBy(TypedDict, total=False):
+    col_id: Required[Literal["creation_time"]]
+
+    sort: Required[Literal["asc", "desc"]]
