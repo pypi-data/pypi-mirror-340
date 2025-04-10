385e4beed8035e1c19cbe1e94cdedb8aa3f52836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 16:03:52 +0000

    Add package granite-io/0.3.0/granite_io-0.3.0-py3-none-any.whl

---
+++ packages/granite-io/granite_io-0.3.0-py3-none-any.whl/granite_io/io/consts.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Common shared constants
+"""
+
+# Strings that a Granite 3.2 model uses to separate Chain of Thought output from
+# responses.
+_GRANITE_3_2_COT_START = "Here is my thought process:"
+_GRANITE_3_2_COT_END = "Here is my response:"
+
+_GRANITE_3_2_2B_OLLAMA = "granite3.2:2b"
+_GRANITE_3_2_2B_HF = "ibm-granite/granite-3.2-2b-instruct"
+_GRANITE_3_2_MODEL_NAME = "Granite 3.2"
