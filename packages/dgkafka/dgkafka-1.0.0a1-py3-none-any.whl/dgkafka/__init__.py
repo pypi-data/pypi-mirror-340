57a028e87736c23715f9489d58ce320787c33c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:03:38 +0000

    Add package dgkafka/1.0.0a1/dgkafka-1.0.0a1-py3-none-any.whl

---
+++ packages/dgkafka/dgkafka-1.0.0a1-py3-none-any.whl/dgkafka/__init__.py
@@ -0,0 +1,5 @@
+from .consumer import KafkaConsumer
+from .avro_consumer import AvroKafkaConsumer
+from .producer import KafkaProducer
+from .avro_producer import AvroKafkaProducer
+from .json_consumer import JsonKafkaConsumer
