d648c3abac518c3778f6ae5730153370b9191f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:04:19 +0000

    Add package sai-mujoco/0.0.6/sai_mujoco-0.0.6-py3-none-any.whl

---
+++ packages/sai-mujoco/sai_mujoco-0.0.6-py3-none-any.whl/sai_mujoco/__init__.py
@@ -0,0 +1,21 @@
+from gymnasium import register
+
+register(
+    id="HumanoidWalk-v0",
+    entry_point="sai_mujoco.humanoid_walk:HumanoidWalkEnv",
+)
+
+register(
+    id="HumanoidObstacle-v0",
+    entry_point="sai_mujoco.humanoid_obstacle:HumanoidObstacleEnv",
+)
+
+register(
+    id="InvertedPendulumWheel-v0",
+    entry_point="sai_mujoco.inverted_pendulum_wheel:InvertedPendulumWheelEnv",
+)
+
+register(
+    id="XArm7-Lift-v0",
+    entry_point="sai_mujoco.xarm7_lift:XArm7LiftEnv",
+)
---
+++ packages/sai-mujoco/sai_mujoco-0.0.6-py3-none-any.whl/sai_mujoco/utils/sai_mujoco_base.py
@@ -0,0 +1,12 @@
+class SaiMujocoBase:
+    def _rescale_action(self, action):
+        expected_bounds = [-1, 1]
+        action_percent = (action - expected_bounds[0]) / (expected_bounds[1] - expected_bounds[0])
+        bounded_percent = np.min(np.max(action_percent, 0), 1)
+        return self.action_space.low + (self.action_space.high - self.action_space.low) * bounded_percent
+
+    def _toggle_overlay(self, show_overlay, render_mode):
+        if render_mode == "human":
+            if self.mujoco_renderer.viewer is None:
+                self.mujoco_renderer._get_viewer(render_mode=render_mode)
+            self.mujoco_renderer.viewer._hide_menu = not show_overlay
