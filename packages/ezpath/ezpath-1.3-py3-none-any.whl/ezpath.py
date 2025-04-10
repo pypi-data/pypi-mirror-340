bc9bbaf7da62466f3effc4cc6b3675a5b2a8b0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 15:04:24 +0000

    Add package ezpath/1.3/ezpath-1.3-py3-none-any.whl

---
+++ packages/ezpath/ezpath-1.3-py3-none-any.whl/ezpath.py
@@ -0,0 +1,47 @@
+
+import os
+import sys
+import inspect
+
+def get_abs_path(rel_path="", up=0, follow_links=False):
+
+	getpath = os.path.realpath if follow_links else os.path.abspath
+
+	current_file = getpath(__file__)
+	stack = inspect.stack()
+	for frame in stack:
+		caller = getpath(frame.filename)
+		if caller != current_file:
+			break
+
+	file_path = os.path.dirname(getpath(caller))
+	file_path = os.path.join(file_path, rel_path)
+	ret_path  = getpath(file_path)
+
+	for i in range(up):
+		ret_path = os.path.dirname(ret_path)
+
+	return ret_path
+
+
+def add_abs_path(abs_path):
+	if abs_path not in sys.path:
+		sys.path += [abs_path]
+
+
+def add_rel_path(rel_path, follow_links=False):
+	add_abs_path(get_abs_path(rel_path, follow_links))
+
+
+def join_path(*args):
+	return os.path.join(*args)
+
+
+def to_os_path(path):
+	return path.replace('/', os.sep).replace('\\', os.sep)
+
+
+def get_basename(path, up=0):
+	for i in range(up):
+		path = os.path.dirname(path)
+	return os.path.basename(path)
