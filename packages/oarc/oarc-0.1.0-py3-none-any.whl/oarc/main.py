26f0374cc2d73b7107bd6ae5603a9b56fec514c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:03:55 +0000

    Add package oarc/0.1.0/oarc-0.1.0-py3-none-any.whl

---
+++ packages/oarc/oarc-0.1.0-py3-none-any.whl/oarc/main.py
@@ -0,0 +1,11 @@
+"""Main functionality for the OARC package."""
+
+import sys
+from oarc.cli.router import handle
+
+def main(**kwargs):
+    """Main CLI entry point."""
+    return handle(**kwargs)
+
+if __name__ == "__main__":
+    sys.exit(main())
