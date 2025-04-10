0020e32f1ab50164429a0919cb655e319cff72f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 15:04:41 +0000

    Add package pylix/0.1.1/pylix-0.1.1-py3-none-any.whl

---
+++ packages/pylix/pylix-0.1.1-py3-none-any.whl/pylix/algebra/__init__.py
@@ -0,0 +1,12 @@
+from pylix.algebra.vector import Vector
+from pylix.algebra import statics
+from pylix.algebra.matrix import Matrix, Axis
+from pylix.algebra.equations import Polynomial
+
+__all__ = [
+    "Vector",
+    "statics",
+    "Matrix",
+    "Polynomial",
+    "Axis"
+]
