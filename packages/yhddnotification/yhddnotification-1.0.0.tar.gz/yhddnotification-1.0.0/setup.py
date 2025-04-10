df89f085999f961af55fec775d16103d4fb3f0a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:04:10 +0000

    Add package yhddnotification/1.0.0/yhddnotification-1.0.0.tar.gz

---
+++ packages/yhddnotification/yhddnotification-1.0.0.tar.gz/yhddnotification-1.0.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="yhddnotification",
+    version="1.0.0",
+    description="test mode",
+    py_modules=["main"],
+    author="BrYan",
+    author_email="mail0426@163.com",
+    long_description="this is test module"
+)
