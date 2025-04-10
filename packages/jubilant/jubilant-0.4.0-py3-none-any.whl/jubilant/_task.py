7e08b76ae555253cfb633e1d98c143ed154e9ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:03:39 +0000

    Add package jubilant/0.4.0/jubilant-0.4.0-py3-none-any.whl

---
+++ packages/jubilant/jubilant-0.4.0-py3-none-any.whl/jubilant/_task.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import dataclasses
+from typing import Any, Literal
+
+
+@dataclasses.dataclass(frozen=True)
+class Task:
+    """A task holds the results of Juju running an action or exec command on a single unit."""
+
+    id: str
+    """Task ID of the action, for use with ``juju show-task``."""
+
+    status: Literal['aborted', 'cancelled', 'completed', 'error', 'failed']
+    """Status of the action (Juju operation). Typically "completed" or "failed"."""
+
+    results: dict[str, Any] = dataclasses.field(default_factory=dict)
+    """Results of the action provided by the charm.
+
+    This excludes the special "return-code", "stdout", and "stderr" keys
+    inserted by Juju; those values are provided by separate attributes.
+    """
+
+    return_code: int = 0
+    """Return code from executing the charm action hook."""
+
+    stdout: str = ''
+    """Stdout printed by the action hook."""
+
+    stderr: str = ''
+    """Stderr printed by the action hook."""
+
+    message: str = ''
+    """Failure message, if the charm provided a message when it failed the action."""
+
+    log: list[str] = dataclasses.field(default_factory=list)
+    """List of messages logged by the action hook."""
+
+    @classmethod
+    def _from_dict(cls, d: dict[str, Any]) -> Task:
+        results: dict[str, Any] = d.get('results') or {}
+        return_code = results.pop('return-code', 0)
+        stdout = results.pop('stdout', '')
+        stderr = results.pop('stderr', '')
+        return cls(
+            id=d['id'],
+            status=d['status'],
+            results=results,
+            return_code=return_code,
+            stdout=stdout,
+            stderr=stderr,
+            message=d.get('message') or '',
+            log=d.get('log') or [],
+        )
+
+    @property
+    def success(self) -> bool:
+        """Whether the action was successful."""
+        return self.status == 'completed' and self.return_code == 0
+
+    def raise_on_failure(self):
+        """If task was not successful, raise a :class:`TaskError`."""
+        if not self.success:
+            raise TaskError(self)
+
+
+class TaskError(Exception):
+    """Exception raised when an action or exec command fails."""
+
+    task: Task
+    """Associated task."""
+
+    def __init__(self, task: Task):
+        self.task = task
