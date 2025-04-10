63563f10e7b7a44c3dff907e22ac6d08862d40cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:03:41 +0000

    Add package snippy-nextgen/0.4.0/snippy_nextgen-0.4.0.tar.gz

---
+++ packages/snippy-nextgen/snippy_nextgen-0.4.0.tar.gz/snippy_nextgen-0.4.0/src/snippy_ng/__about__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024-present Wytamma Wirth <wytamma.wirth@me.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.4.0"
+
+# Version and executable information
+EXE = "snippy-ng"
+AUTHOR = "Wytamma Wirth & Torsten Seemann"
+URL = "https://github.com/centre-pathogen-genomics/snippy-ng"
