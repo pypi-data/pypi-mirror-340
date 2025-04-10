bbf148cc5b0623a8e451c87619c9d7a73615968e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:04:10 +0000

    Add package maleo-core/0.3.57/maleo_core-0.3.57.tar.gz

---
+++ packages/maleo-core/maleo_core-0.3.57.tar.gz/maleo_core-0.3.57/maleo_core/models/maleo_suite/maleo_security/schemas/responses/services/__init__.py
@@ -0,0 +1,24 @@
+# This file serves all MaleoSecurity's services responses schemas
+
+from __future__ import annotations
+from .secret import MaleoSecuritySecretServiceResponsesSchemas
+from .key import MaleoSecurityKeyServiceResponsesSchemas
+from .encryption import MaleoSecurityEncryptionServicesResponsesSchemas
+from .hash import MaleoSecurityHashServiceResponsesSchemas
+from .signature import MaleoSecuritySignatureServiceResponsesSchemas
+
+class MaleoSecurityServicesResponsesSchemas:
+    Secret = MaleoSecuritySecretServiceResponsesSchemas
+    Key = MaleoSecurityKeyServiceResponsesSchemas
+    Encryption = MaleoSecurityEncryptionServicesResponsesSchemas
+    Hash = MaleoSecurityHashServiceResponsesSchemas
+    Signature = MaleoSecuritySignatureServiceResponsesSchemas
+
+__all__ = [
+    "MaleoSecurityServicesResponsesSchemas",
+    "MaleoSecuritySecretServiceResponsesSchemas",
+    "MaleoSecurityKeyServiceResponsesSchemas",
+    "MaleoSecurityEncryptionServicesResponsesSchemas",
+    "MaleoSecurityHashServiceResponsesSchemas",
+    "MaleoSecuritySignatureServiceResponsesSchemas"
+]
