dcc906a1f528e7f7a8484c702bf6e1ffa755ded0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 16:04:43 +0000

    Add package decision-tree-visualizer/1.0.0/decision_tree_visualizer-1.0.0.tar.gz

---
+++ packages/decision-tree-visualizer/decision_tree_visualizer-1.0.0.tar.gz/decision_tree_visualizer-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="decision-tree-visualizer",
+    version="1.0.0",
+    description="A library to visualize sklearn Decision Tree Classifiers.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="yashksaini-coder",
+    author_email="ys3853428@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy>=2.0.0",
+        "matplotlib>=3.9.0",
+        "Jinja2>=3.1.4",
+        "scikit-learn>=1.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+)
+
