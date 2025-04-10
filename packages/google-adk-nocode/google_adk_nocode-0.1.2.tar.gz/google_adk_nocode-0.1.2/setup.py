130fb4b5d0b854f4b5cc5a405d055a1fcc17177f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 16:04:04 +0000

    Add package google-adk-nocode/0.1.2/google_adk_nocode-0.1.2.tar.gz

---
+++ packages/google-adk-nocode/google_adk_nocode-0.1.2.tar.gz/google_adk_nocode-0.1.2/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""Setup script for google-adk-nocode."""
+
+import os
+from setuptools import setup
+
+# Force setuptools to use the latest version
+if __name__ == "__main__":
+    setup()
