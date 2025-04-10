0924d47ddd74b6220d5a816931f23587a4cdde93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:03:56 +0000

    Add package pyasd/0.1.7/pyasd-0.1.7.tar.gz

---
+++ packages/pyasd/pyasd-0.1.7.tar.gz/pyasd-0.1.7/asd/__init__.py
@@ -0,0 +1,8 @@
+__built_time__        =  "built at Thu Apr 10 11:40:10 2025_CST"
+__name__              =  "pyasd"
+__version__           =  "0.1.7"
+__author__            =  "Shunhong Zhang"
+__author_email__      =  "zhangshunhong.pku@gmail.com"
+__url__               =  "https://pypi.org/project/pyasd/"
+__license__           =  "MIT LICENSE"
+__platform__          =  "linux"
