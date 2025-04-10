2b34931b97f08471f78aa4928d78217c54e0046d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 16:04:00 +0000

    Add package airqloudanalysis/0.0.730/airqloudanalysis-0.0.730.tar.gz

---
+++ packages/airqloudanalysis/airqloudanalysis-0.0.730.tar.gz/airqloudanalysis-0.0.730/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+VERSION = '0.0.730'
+DESCRIPTION = 'This is used for analyzing the obtain from IoT AirQo devices'
+LONG_DESCRIPTION = README 
+
+# Setting up
+setup(
+    name="airqloudanalysis",
+    version=VERSION,
+    author="AirQo",
+    author_email="<gibson@airqo.net>",
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OlukaGibson/deviceAnalysisLibrary.git",
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'requests', 'pytz', 'python-dateutil', 'beautifulsoup4', 'matplotlib', 'seaborn', 'plotly', 'cufflinks'],
+    keywords=['python', 'IoT', 'AirQo', 'data', 'analysis', 'insights'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ]
+)
