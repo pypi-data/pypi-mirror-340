25635470c54aabe3226433860da63882ce7ef6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 04:05:03 +0000

    Add package taskcluster-taskgraph/14.1.1/taskcluster_taskgraph-14.1.1.tar.gz

---
+++ packages/taskcluster-taskgraph/taskcluster_taskgraph-14.1.1.tar.gz/taskcluster_taskgraph-14.1.1/test/test_docker.py
@@ -0,0 +1,81 @@
+import pytest
+
+from taskgraph import docker
+
+
+@pytest.fixture(autouse=True, scope="module")
+def mock_docker_path(module_mocker, datadir):
+    module_mocker.patch(
+        "taskgraph.util.docker.IMAGE_DIR", str(datadir / "taskcluster" / "docker")
+    )
+
+
+@pytest.fixture
+def mock_docker_build(mocker):
+    def side_effect(topsrcdir, context_dir, out_file, image_name=None, args=None):
+        out_file.write(b"xyz")
+
+    m_stream = mocker.patch.object(docker.docker, "stream_context_tar")
+    m_stream.side_effect = side_effect
+
+    m_run = mocker.patch.object(docker.subprocess, "run")
+    return (m_stream, m_run)
+
+
+def test_build_image(capsys, mock_docker_build):
+    m_stream, m_run = mock_docker_build
+    image = "hello-world-tag"
+    tag = f"test/{image}:1.0"
+
+    assert docker.build_image(image, None) is None
+    m_stream.assert_called_once()
+    m_run.assert_called_once_with(
+        ["docker", "image", "build", "--no-cache", f"-t={tag}", "-"],
+        input=b"xyz",
+        check=True,
+    )
+
+    out, _ = capsys.readouterr()
+    assert f"Successfully built {image} and tagged with {tag}" in out
+    assert "Image is not suitable for deploying/pushing" not in out
+
+
+def test_build_image_no_tag(capsys, mock_docker_build):
+    m_stream, m_run = mock_docker_build
+    image = "hello-world"
+
+    assert docker.build_image(image, None) is None
+    m_stream.assert_called_once()
+    m_run.assert_called_once_with(
+        ["docker", "image", "build", "--no-cache", "-"],
+        input=b"xyz",
+        check=True,
+    )
+
+    out, _ = capsys.readouterr()
+    assert f"Successfully built {image}" in out
+    assert "Image is not suitable for deploying/pushing" in out
+
+
+def test_build_image_error(capsys, mock_docker_build):
+    m_stream, m_run = mock_docker_build
+
+    def mock_run(*popenargs, check=False, **kwargs):
+        if check:
+            raise docker.subprocess.CalledProcessError(1, popenargs)
+        return 1
+
+    m_run.side_effect = mock_run
+    image = "hello-world"
+
+    with pytest.raises(Exception):
+        docker.build_image(image, None)
+    m_stream.assert_called_once()
+    m_run.assert_called_once_with(
+        ["docker", "image", "build", "--no-cache", "-"],
+        input=b"xyz",
+        check=True,
+    )
+
+    out, _ = capsys.readouterr()
+    assert f"Successfully built {image}" not in out
