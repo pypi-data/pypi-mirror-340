e755188b358ee12ca69e0403b60bead53f721aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 15:04:36 +0000

    Add package wardy-core/0.1.4.3/wardy_core-0.1.4.3.tar.gz

---
+++ packages/wardy-core/wardy_core-0.1.4.3.tar.gz/wardy_core-0.1.4.3/core/__init__.py
@@ -0,0 +1,7 @@
+"""Common core utilities.
+
+These modules provide functions that are useful to multiple scripts.
+
+Example usage:
+    >>> from core.emailing import EmailArgs, build_email_args, email
+"""
