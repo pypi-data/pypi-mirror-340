fe5b0ecc249ecc2a2331c70e2c3ce85b37fb1221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 03:18:05 +0000

    Add package mitm-tooling/0.5.1/mitm_tooling-0.5.1-py3-none-any.whl

---
+++ packages/mitm-tooling/mitm_tooling-0.5.1-py3-none-any.whl/mitm_tooling/transformation/superset/asset_bundles/identifier.py
@@ -0,0 +1,44 @@
+from typing import Self
+from uuid import UUID
+
+import pydantic
+
+from mitm_tooling.transformation.superset.definitions import DatasetIdentifierMap, \
+    DatabaseIdentifier, MitMDatasetIdentifier, BaseSupersetDefinition, DashboardIdentifier, SupersetMitMDatasetDef
+
+VizDashboardIdentifierMap = dict[str, DashboardIdentifier]
+VizCollectionIdentifierMap = dict[str, VizDashboardIdentifierMap]
+
+
+class DatasourceIdentifierBundle(BaseSupersetDefinition):
+    database: DatabaseIdentifier | None = None
+    ds_id_map: DatasetIdentifierMap = pydantic.Field(default_factory=dict)
+
+    @property
+    def database_uuid(self) -> UUID | None:
+        if self.database is not None:
+            return self.database.uuid
+
+    @classmethod
+    def from_mitm_dataset(cls, mitm_dataset: SupersetMitMDatasetDef) -> Self:
+        return cls(database=DatabaseIdentifier(uuid=mitm_dataset.database_uuid),
+                   ds_id_map={t.table_name: t for t in (mitm_dataset.tables or [])})
+
+
+class MitMDatasetIdentifierBundle(DatasourceIdentifierBundle):
+    mitm_dataset: MitMDatasetIdentifier | None = None
+    viz_id_map: VizCollectionIdentifierMap = pydantic.Field(default_factory=dict)
+
+    @property
+    def mitm_dataset_uuid(self) -> UUID | None:
+        if self.mitm_dataset is not None:
+            return self.mitm_dataset.uuid
+
+    @classmethod
+    def from_mitm_dataset(cls, mitm_dataset: SupersetMitMDatasetDef) -> Self:
+        return cls(mitm_dataset=mitm_dataset.identifier,
+                   database=DatabaseIdentifier(uuid=mitm_dataset.database_uuid),
+                   ds_id_map={t.table_name: t for t in (mitm_dataset.tables or [])},
+                   viz_id_map={
+                       'default': {dash.dashboard_title: dash for dash in
+                                   (mitm_dataset.dashboards or [])}})
