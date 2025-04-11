6d9a30ece80e0c595ec0fef48acab97895b3a43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:16 +0000

    Add package clipx/0.2.1/clipx-0.2.1.tar.gz

---
+++ packages/clipx/clipx-0.2.1.tar.gz/clipx-0.2.1/clipx/models/u2net/__init__.py
@@ -0,0 +1,4 @@
+from clipx.models.u2net.model import U2NetPredictor
+from clipx.models.u2net.download import get_model_path, download_u2net_model, ClipxError
+
+__all__ = ['U2NetPredictor', 'get_model_path', 'download_u2net_model', 'ClipxError']
