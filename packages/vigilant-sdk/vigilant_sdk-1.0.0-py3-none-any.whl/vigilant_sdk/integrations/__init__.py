11724f7cac189a5bb8936a7a2fcc8ae805ca71bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 14:04:22 +0000

    Add package vigilant-sdk/1.0.0/vigilant_sdk-1.0.0-py3-none-any.whl

---
+++ packages/vigilant-sdk/vigilant_sdk-1.0.0-py3-none-any.whl/vigilant_sdk/integrations/__init__.py
@@ -0,0 +1,4 @@
+from vigilant_sdk.integrations.fastapi import add_http_middleware, add_websocket_middleware, MiddlewareConfig
+
+__all__ = ["add_http_middleware",
+           "add_websocket_middleware", "MiddlewareConfig"]
