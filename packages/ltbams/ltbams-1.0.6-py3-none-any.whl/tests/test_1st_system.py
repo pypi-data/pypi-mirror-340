de03480c7676c7874362d28508b5124dc0dfed80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 14:04:35 +0000

    Add package ltbams/1.0.6/ltbams-1.0.6-py3-none-any.whl

---
+++ packages/ltbams/ltbams-1.0.6-py3-none-any.whl/tests/test_1st_system.py
@@ -0,0 +1,33 @@
+import unittest
+
+import ams
+
+
+class TestCodegen(unittest.TestCase):
+    """
+    Test code generation.
+    """
+
+    def test_1_docs(self) -> None:
+        sp = ams.system.System()
+        out = ''
+        for tp in sp.types.values():
+            out += tp.doc_all()
+
+        out = ''
+        for group in sp.groups.values():
+            out += group.doc_all()
+
+    def test_docum(self) -> None:
+        sp = ams.load(ams.get_case('5bus/pjm5bus_demo.xlsx'),
+                      setup=True, no_output=True)
+        sp.DCOPF.init()
+        docum = sp.DCOPF.docum
+        for export in ['plain', 'rest']:
+            docum._obj_doc(max_width=78, export=export)
+            docum._constr_doc(max_width=78, export=export)
+            docum._exprc_doc(max_width=78, export=export)
+            docum._var_doc(max_width=78, export=export)
+            docum._service_doc(max_width=78, export=export)
+            docum._param_doc(max_width=78, export=export)
+            docum.config.doc(max_width=78, export=export)
