e7356a409f0b5eb4c6aa4328028b8252f4778795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 06:05:07 +0000

    Add package airbyte-cdk/6.45.0.post24.dev14387467928/airbyte_cdk-6.45.0.post24.dev14387467928-py3-none-any.whl

---
+++ packages/airbyte-cdk/airbyte_cdk-6.45.0.post24.dev14387467928-py3-none-any.whl/airbyte_cdk/test/fixtures/auto.py
@@ -0,0 +1,14 @@
+"""Auto-use fixtures for pytest.
+
+WARNING: Importing this module will automatically apply these fixtures. If you want to selectively
+enable fixtures in a different context, you can import directly from the `fixtures.general` module.
+
+
+Usage:
+
+```python
+from airbyte_cdk.test.fixtures import auto
+# OR
+from airbyte_cdk.test.fixtures.auto import *
+```
+"""
