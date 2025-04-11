07e2ef062ff17c18e52cec2ecb2826b31bba01e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 17:09:47 +0000

    Add package QUARK-framework/0.1.6/quark_framework-0.1.6.tar.gz

---
+++ packages/QUARK-framework/quark_framework-0.1.6.tar.gz/quark_framework-0.1.6/src/quark/plugin_manager/factory.py
@@ -0,0 +1,19 @@
+from typing import Any, Callable
+
+from quark.core import Core
+
+plugin_creation_funcs: dict[str, Callable[..., Core]] = {}
+
+def register(plugin_type: str, creator_fn: Callable[..., Core]) -> None:
+    plugin_creation_funcs[plugin_type] = creator_fn
+
+def unregister(plugin_type: str) -> None:
+    plugin_creation_funcs.pop(plugin_type, None)
+
+def create(plugin_type: str, arguments: dict[str, Any]) -> Core:
+    try:
+        creator_func = plugin_creation_funcs[plugin_type]
+    except KeyError as exc:
+        raise ValueError(f"Unknown plugin type {plugin_type!r}") from exc
+
+    return creator_func(**(arguments or {}))
