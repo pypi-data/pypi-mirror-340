c10a90d26a81a33fc4041f0ad76f1368e07cb722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 14:04:47 +0000

    Add package bearshell/0.1.1/bearshell-0.1.1.tar.gz

---
+++ packages/bearshell/bearshell-0.1.1.tar.gz/bearshell-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bearshell',
+    version='0.1.1',
+    author='Bradley Reimers',
+    author_email='b.a.reimers@gmail.com',
+    description='A (relatively) secure and easy-to-use subprocess implementation for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/breimers/bear-shell',  # Replace with your repository URL
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Topic :: System :: Shells',
+    ],
+    python_requires='>=3.7',
+)
