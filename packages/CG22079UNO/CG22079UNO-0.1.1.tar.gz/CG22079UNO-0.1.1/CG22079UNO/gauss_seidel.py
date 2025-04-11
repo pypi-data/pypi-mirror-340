7f77ece151bb01abdf87d4dff4d991e3ebdd7c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 15:04:26 +0000

    Add package CG22079UNO/0.1.1/CG22079UNO-0.1.1.tar.gz

---
+++ packages/CG22079UNO/CG22079UNO-0.1.1.tar.gz/CG22079UNO-0.1.1/CG22079UNO/gauss_seidel.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def gauss_seidel(A, b, x0=None, tol=1e-10, max_iterations=100):
+    A = np.array(A, float)
+    b = np.array(b, float)
+    n = len(b)
+    if x0 is None:
+        x0 = np.zeros(n)
+    x = np.array(x0, float)
+
+    for _ in range(max_iterations):
+        x_new = np.copy(x)
+        for i in range(n):
+            s1 = sum(A[i][j] * x_new[j] for j in range(i))
+            s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
+            x_new[i] = (b[i] - s1 - s2) / A[i][i]
+        if np.linalg.norm(x_new - x, ord=np.inf) < tol:
+            return x_new
+        x = x_new
+
+    return x
