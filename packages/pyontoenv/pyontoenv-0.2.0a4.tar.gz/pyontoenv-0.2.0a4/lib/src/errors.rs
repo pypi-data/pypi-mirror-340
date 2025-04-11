a95b8521d429788fd5601a7ec590b078a97a4dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 08:04:21 +0000

    Add package pyontoenv/0.2.0a4/pyontoenv-0.2.0a4.tar.gz

---
+++ packages/pyontoenv/pyontoenv-0.2.0a4.tar.gz/pyontoenv-0.2.0a4/lib/src/errors.rs
@@ -0,0 +1,20 @@
+// OfflineRetrieval error
+
+use std::fmt;
+
+#[derive(Debug)]
+pub struct OfflineRetrievalError {
+    pub file: String,
+}
+
+impl fmt::Display for OfflineRetrievalError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "OFFLINE enabled: Failed to fetch ontology from {}",
+            self.file
+        )
+    }
+}
+
+impl std::error::Error for OfflineRetrievalError {}
