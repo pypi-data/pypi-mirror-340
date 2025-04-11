fa29524f41c7b37abf6de272cf83fb90d6e2c9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 15:04:48 +0000

    Add package pyasd/0.1.8/pyasd-0.1.8.tar.gz

---
+++ packages/pyasd/pyasd-0.1.8.tar.gz/pyasd-0.1.8/asd/__init__.py
@@ -0,0 +1,8 @@
+__built_time__        =  "built at Fri Apr 11 14:42:11 2025_CST"
+__name__              =  "pyasd"
+__version__           =  "0.1.8"
+__author__            =  "Shunhong Zhang"
+__author_email__      =  "zhangshunhong.pku@gmail.com"
+__url__               =  "https://pypi.org/project/pyasd/"
+__license__           =  "MIT LICENSE"
+__platform__          =  "linux"
