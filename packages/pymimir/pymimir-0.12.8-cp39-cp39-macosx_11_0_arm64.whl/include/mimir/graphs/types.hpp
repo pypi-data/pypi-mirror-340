01c062d22882ee590b58c668ab1fb96828bcc102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 03:18:45 +0000

    Add package pymimir/0.12.8/pymimir-0.12.8-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/pymimir/pymimir-0.12.8-cp39-cp39-macosx_11_0_arm64.whl/include/mimir/graphs/types.hpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2023 Dominik Drexler and Simon Stahlberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MIMIR_GRAPHS_TYPES_HPP_
+#define MIMIR_GRAPHS_TYPES_HPP_
+
+// Do not include headers with transitive dependencies.
+#include "mimir/common/types.hpp"
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <limits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace mimir::graphs
+{
+
+/**
+ * Types
+ */
+
+using VertexIndex = Index;
+using VertexIndexList = std::vector<VertexIndex>;
+using VertexIndexSet = std::unordered_set<VertexIndex>;
+
+using EdgeIndex = Index;
+using EdgeIndexList = std::vector<EdgeIndex>;
+using EdgeIndexSet = std::unordered_set<EdgeIndex>;
+
+using Degree = uint32_t;
+using DegreeList = std::vector<Degree>;
+using DegreeMap = std::unordered_map<VertexIndex, Degree>;
+
+using Color = uint32_t;
+using ColorList = std::vector<Color>;
+template<size_t K>
+using ColorArray = std::array<Color, K>;
+template<size_t K>
+using ColorArrayList = std::vector<ColorArray<K>>;
+using ColorSet = std::unordered_set<Color>;
+template<typename T>
+using ColorMap = std::unordered_map<Color, T>;
+
+}
+
+#endif
