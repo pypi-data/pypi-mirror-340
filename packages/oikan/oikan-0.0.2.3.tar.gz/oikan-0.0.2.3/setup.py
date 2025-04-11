175d687b9205a7a859b635b16b8e76ca166015e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 08:04:14 +0000

    Add package oikan/0.0.2.3/oikan-0.0.2.3.tar.gz

---
+++ packages/oikan/oikan-0.0.2.3.tar.gz/oikan-0.0.2.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oikan",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "numpy",
+        "scikit-learn"
+    ]
+)
