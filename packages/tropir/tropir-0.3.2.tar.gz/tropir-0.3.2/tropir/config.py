4d4a96d11cbc06b513c6b740e4097a8833c835b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 14:04:49 +0000

    Add package tropir/0.3.2/tropir-0.3.2.tar.gz

---
+++ packages/tropir/tropir-0.3.2.tar.gz/tropir-0.3.2/tropir/config.py
@@ -0,0 +1,25 @@
+"""
+Configuration settings for the Tropir Agent.
+"""
+
+import os
+
+# API configuration
+DEFAULT_API_URL = "http://localhost:8080/"
+
+# Default configuration
+DEFAULTS = {
+    "enabled": True,
+    "api_url": DEFAULT_API_URL + "log",
+}
+
+def get_config():
+    """
+    Get configuration from environment variables or defaults.
+    """
+    return {
+        "enabled": os.getenv("TROPIR_ENABLED", "1") == "1",
+        "api_url": os.getenv("TROPIR_API_URL", DEFAULT_API_URL) + "api/log",
+        "api_key": os.getenv("TROPIR_API_KEY"),
+        "local": os.getenv("TROPIR_LOCAL", "0") == "1",
+    } 
