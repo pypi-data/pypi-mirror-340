2476aee3dff70cd2f575cc0a1c7ecc2970cb4623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:06:03 +0000

    Add package pypipr/1.0.168/pypipr-1.0.168-py3-none-any.whl

---
+++ packages/pypipr/pypipr-1.0.168-py3-none-any.whl/pypipr/dirname.py
@@ -0,0 +1,33 @@
+import os
+
+from .filter_empty import filter_empty
+
+
+def dirname(path, indeks=-1, abs_path=None):
+    """
+    Mengembalikan nama folder dari path.
+    Tanpa trailing slash di akhir.
+
+    ```python
+    print(dirname("/ini/nama/folder/ke/file.py"))
+    ```
+    """
+    
+    paths = path.split(os.sep)
+
+    paths = list(filter_empty(paths))
+
+    if indeks < 0:
+        paths = paths[: len(paths) + indeks]
+
+    path = os.sep.join(paths)
+
+    if abs_path is None:
+        return path
+
+    if abs_path:
+        return os.path.abspath(path)
+
+    return os.path.relpath(path, os.getcwd())
+
+    return os.path.dirname(path)
