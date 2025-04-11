aef3393dc6002c18f7ec65a17cfd2c2f85302407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:09 +0000

    Add package tgbotgen/0.1.3/tgbotgen-0.1.3.tar.gz

---
+++ packages/tgbotgen/tgbotgen-0.1.3.tar.gz/tgbotgen-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tgbotgen",
+    version="0.1.3",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "tg-bot-init=tgbotgen.scaffold:run_scaffold",
+        ],
+    },
+    install_requires=[
+        "aiogram==3.3.0",
+        "SQLAlchemy==2.0.40",
+        "python-dotenv==0.9.0",
+        "APScheduler==3.11.0",
+        "aiosqlite==0.21.0",
+    ],
+    author="Александр Аваков",
+    description="Генератор структуры проекта для Telegram-бота",
+    python_requires=">=3.8",
+)
