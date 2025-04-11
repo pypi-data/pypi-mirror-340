0e37674062bc6325d94516d4e3837dfc130ed435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:13 +0000

    Add package draive/0.57.2/draive-0.57.2.tar.gz

---
+++ packages/draive/draive-0.57.2.tar.gz/draive-0.57.2/src/draive/generation/__init__.py
@@ -0,0 +1,20 @@
+from draive.generation.image import ImageGeneration, ImageGenerator, generate_image
+from draive.generation.model import (
+    ModelGeneration,
+    ModelGenerator,
+    ModelGeneratorDecoder,
+    generate_model,
+)
+from draive.generation.text import TextGenerating, TextGeneration
+
+__all__ = (
+    "ImageGeneration",
+    "ImageGenerator",
+    "ModelGeneration",
+    "ModelGenerator",
+    "ModelGeneratorDecoder",
+    "TextGenerating",
+    "TextGeneration",
+    "generate_image",
+    "generate_model",
+)
---
+++ packages/draive/draive-0.57.2.tar.gz/draive-0.57.2/src/draive/generation/model/__init__.py
@@ -0,0 +1,10 @@
+from draive.generation.model.call import generate_model
+from draive.generation.model.state import ModelGeneration
+from draive.generation.model.types import ModelGenerator, ModelGeneratorDecoder
+
+__all__ = (
+    "ModelGeneration",
+    "ModelGenerator",
+    "ModelGeneratorDecoder",
+    "generate_model",
+)
