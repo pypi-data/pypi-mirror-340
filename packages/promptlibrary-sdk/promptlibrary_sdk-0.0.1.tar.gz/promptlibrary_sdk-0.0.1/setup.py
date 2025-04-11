98c62c7c5f04f5e5c76dca90a1be0c98ee63acc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:05:29 +0000

    Add package promptlibrary-sdk/0.0.1/promptlibrary_sdk-0.0.1.tar.gz

---
+++ packages/promptlibrary-sdk/promptlibrary_sdk-0.0.1.tar.gz/promptlibrary_sdk-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="promptlibrary_sdk",
+    version="0.0.1",
+    author="Pouya Nafisi",
+    author_email="pouya@promptlibrary.com",
+    description="Reserved namespace for PromptLibrary",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
