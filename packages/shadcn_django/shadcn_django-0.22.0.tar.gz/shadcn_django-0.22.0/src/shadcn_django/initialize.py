9b7ca7fe9c0c5ee5a9d7faae7551c71cb0e873ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:05:29 +0000

    Add package shadcn_django/0.22.0/shadcn_django-0.22.0.tar.gz

---
+++ packages/shadcn_django/shadcn_django-0.22.0.tar.gz/shadcn_django-0.22.0/src/shadcn_django/initialize.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import copier
+import typer
+from rich.panel import Panel
+from rich.status import Status
+
+from .console import console
+from .constants import COMPONENTS_REPO_URL, DEFAULT_COMPONENTS_DIRECTORY
+
+app = typer.Typer(no_args_is_help=True, add_completion=False)
+
+
+@app.command(name="init")
+def init():
+    """
+    Initialize setup for shadcn_django components
+    """
+    DEFAULT_COMPONENTS_DIRECTORY.mkdir(parents=True, exist_ok=True)
+    with Status("Adding tailwind config for shadcn components"):
+        copier.run_copy(
+            src_path=COMPONENTS_REPO_URL,
+            dst_path=Path.cwd(),
+            vcs_ref="main",
+            exclude=["*", "!input.css"],
+        )
+    console.print(
+        Panel(
+            "[bold green]"
+            ":rocket: Initialized shadcn_django components!\n\n"
+            ":heavy_check_mark: Created "
+            + f"'{DEFAULT_COMPONENTS_DIRECTORY}'\n"
+            ":heavy_check_mark: Added TailwindCSS config required for shadcn components\n\n"
+            "[/bold green]"
+            "[bold yellow]"
+            ":bookmark_tabs: Next steps:\n"
+            "[/bold yellow]"
+            ":arrow_right_hook: Ensure you have tailwind v4 and alpine.js installed\n"
+            ":arrow_right_hook: Add <link rel='stylesheet' href='{% static 'css/output.css' %}'> to your base HTML template\n"
+            ":arrow_right_hook: Run 'npx @tailwindcss/cli -i input.css -o static/css/output.css --watch'\n",
+            title="Initialization Complete",
+            border_style="bold green",
+        )
+    )
