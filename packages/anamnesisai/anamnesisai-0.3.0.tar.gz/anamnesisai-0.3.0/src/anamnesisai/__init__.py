138c0da2387e3c560e50d44cc15fbf3f68b9087e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 07:04:45 +0000

    Add package anamnesisai/0.3.0/anamnesisai-0.3.0.tar.gz

---
+++ packages/anamnesisai/anamnesisai-0.3.0.tar.gz/anamnesisai-0.3.0/src/anamnesisai/__init__.py
@@ -0,0 +1,18 @@
+"""anamnesis.ai."""
+
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    """Return the program version."""
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "0.3.0"  # semantic-release
+
+
+version = get_version()
+
+__version__ = version
+__author__ = "Satarupa Deb, Ivan Ogasawara"
+__email__ = "satarupa2212@gmail.com, ivan.ogasawara@gmail.com"
