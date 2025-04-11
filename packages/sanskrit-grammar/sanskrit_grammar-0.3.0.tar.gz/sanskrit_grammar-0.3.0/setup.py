cfb2565af453bdf69d94486cb62cef13783e2fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:07:41 +0000

    Add package sanskrit-grammar/0.3.0/sanskrit_grammar-0.3.0.tar.gz

---
+++ packages/sanskrit-grammar/sanskrit_grammar-0.3.0.tar.gz/sanskrit_grammar-0.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "matplotlib",
+    "networkx",
+    "pytesseract",
+    "Pillow"
+]
+setup(
+    name='sanskrit-grammar',
+    version='0.3.0',
+    packages=find_packages(),
+    install_requires=requirements,  # <- use the requirements here
+    author='Saurabh Chandra Patel',
+    author_email='vsaurabhaec@gmail.com',
+    description='Python module provides functions to handle and manipulate various aspects of Sanskrit grammar, including word generation, prefixes etc',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/saurabhchandrapatel/sanskrit-grammar',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
