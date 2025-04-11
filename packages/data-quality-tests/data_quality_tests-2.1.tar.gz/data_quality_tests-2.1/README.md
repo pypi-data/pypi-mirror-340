bc668b6d4f1991ff6f2d01ab4645c9666b8814c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 06:04:50 +0000

    Add package data-quality-tests/2.1/data_quality_tests-2.1.tar.gz

---
+++ packages/data-quality-tests/data_quality_tests-2.1.tar.gz/data_quality_tests-2.1/README.md
@@ -0,0 +1,75 @@
+## DATA QUALITY
+
+A library which acts as a test cases for dataframes. Simply pass in your dataframe after initial import, or at each stage of your EDA to check for data quality with one line of code.
+
+The test cases include(as of now)
+1. check for null values
+2. check for duplicates
+3. check for dtype matching
+4. check for outliers - depricated
+5. check for whitespaces in column headers - depricated
+
+The test cases work as a Pass/Fail type, where Passed indicates, good data quality and Failed indicates bad data quality
+
+Example: 
+
+TEST CASE FOR NULL VALUES: Passed means that the dataframe has no null values. Failed indicates otherwise.
+
+## Requirements
+
+* Python 3+
+* Pandas
+* Numpy
+
+
+## Installation
+
+```python
+pip install data-quality-tests
+```
+
+## Updates & Changes
+
+1. the import function changed from:
+
+```python
+from data_quality import DataQuality
+```
+
+to the following:
+
+```python
+from data_quality_tests import DataQuality
+```
+
+2. new function ```get_row_count()``` has been added in this update, which displays number of rows in a dataframe.  
+*For use case, refer to the get started section*
+
+3. ```data_quality_check``` now checks for column header whitespaces for leading and trailing.
+
+
+## Get Started
+
+How to use this library:
+
+### Data quality check
+
+The most basic usage of this library, here for simplifiction,  
+let's just se the iris dataset from seaborn library.
+
+You can use any dataset.
+
+```python
+from data_quality_tests import DataQuality as dq
+import seaborn as sns
+
+#declare any dataframe
+
+df = sns.load_dataset("iris")
+
+#pass the dataframe as below  
+
+dq.data_quality_check(df)
+
+dq.get_row_count(df)
+```
---
+++ packages/data-quality-tests/data_quality_tests-2.1.tar.gz/data_quality_tests-2.1/data_quality_tests/dq.py
@@ -0,0 +1,200 @@
+import pandas as pd
+import numpy as np
+
+
+class DataQuality():
+    """
+    Initiate a data quality assessment class with (as of now) 4 test cases to check data quality
+    """
+
+    def __init__(self, df):
+        self.df = df
+
+
+    def get_row_count(df):
+        """
+        Returns the count of rows for the user to use
+        """
+        return len(df)
+    
+    def data_quality_check(df):
+        
+        """
+        
+        Data Quality check 
+        Input - dataset
+        Output - data quality as test cases
+
+        The out put will be in the form of the test cases below followed by Passed or Failed
+
+        the test case checks are of conditional checks and logic that goes on in normal EDA.
+        
+        Checks include:
+        
+        1. Test for null values
+        2. Test for duplicates
+        3. Test for dtype matching
+        4. Test for outliers - depricated
+        5. Test for column header whitespaces               
+    
+        """      
+        
+        # TEST FOR NULL VALUES
+        # if null values exist -> condition = true then test failed, else false
+        
+        
+        #print("TEST FOR NULL VALUES:")
+        print("Note:")
+        print("Fail - Failed for test case due to existing condition.")
+        print("Pass - Clear of condition.")
+        print("")
+        print("Number of rows: ", len(df))
+        print("")
+        if df.isnull().values.any() == True:
+            print("Test Case Null Values: Fail")
+        else:
+            print('Test Case Null Values: Pass')
+            
+        
+        #print("Total number of null values: ",null_sum)
+        print("")
+
+        
+        
+        # TEST FOR DUPLICATES
+        
+        if df.duplicated().any() == True:
+            print("Test Case Duplicated Values: Fail")
+        else:
+            print("Test Case Duplicated Values: Pass")
+        #duplicate_sum = df.duplicated().sum()
+        #print("Total number of duplicates: ", duplicate_sum)
+        print("")
+        
+
+
+        # TEST For dtype matching
+        
+        # num_list is checking whether the columns are numeric or not-
+        # a list of true and false where false is not number and true is number
+        num_col_list_bool = df.apply(lambda s: pd.to_numeric(s, errors='coerce').notnull().all()).to_list()
+        # col_list is a list of columns
+        col_list = df.columns
+        # index_cols is a list of tuples of the format - (bool,column_name) example - (False, 'booking_id')
+        index_cols = list(zip(num_col_list_bool, col_list))
+        dtype_truth = []
+        for i in index_cols:
+            if i[0]==False:
+                if df[i[1]].isna().any() == True:
+                    pass
+                elif any(df[i[1]].str.contains(r'\b.*[a-zA-Z]+.*\b')) == False:
+                    my_truth = 'False'
+                    dtype_truth.append(my_truth)
+                else:
+                    my_truth = 'True'
+                    dtype_truth.append(my_truth)
+            else:
+                my_truth = 'True'
+                dtype_truth.append(my_truth)
+        fail = "False"
+        if fail in dtype_truth:
+            print('Test Case Column Data Type Matches Values In Column: Fail')
+        else:
+            print('Test Case Column Data Type Matches Values In Column: Pass')
+        print("")
+
+
+        # TEST FOR COLUMN HEADER LEADING AND TRAILING SPACES
+        col_list = [x for x in df.columns if x.endswith(' ') or x.startswith(' ')]
+    
+        if len(col_list) == 0:
+            print("Test Case Column Header Whitespaces: Pass")
+        else:
+            print("Test Case Column Header Whitespaces: Fail")
+        print("")
+
+
+
+    # # DISPLAY LIST OF OUTLIER COLUMNS
+    # def outlier_columns(df):
+    #     """
+    #     A function that checks for outliers and outputs the columns that have outliers
+        
+    #     Input - data frame
+    #     Output - list of columns containing outliers
+        
+    #     """
+        
+    #     cols_list = df.select_dtypes(include=['int32','int64','float']).columns
+    #     outlier_truth_list =[]
+    #     for i in cols_list:
+            
+    #         q1 = df[i].quantile(0.25)
+    #         q3 = df[i].quantile(0.75)
+    #         iqr = q3-q1 #Interquartile range
+    #         fence_low  = q1-1.5*iqr
+    #         fence_high = q3+1.5*iqr
+            
+    #         if len(df.loc[(df[i] > fence_low) & (df[i] < fence_high)]) > 0:
+    #             truth = 'False' #outliers
+    #             outlier_truth_list.append(truth)
+    #         else:
+    #             truth = 'True' #not outliers
+    #             outlier_truth_list.append(truth)
+    #     truth_dict = dict(zip(cols_list, outlier_truth_list))
+
+    #     filtered = [k for k, v in truth_dict.items() if v == 'False']
+    #     print(filtered)
+
+    
+    # def dtype_columns(df):
+    #     """
+    #     A Function that checks for data type matching and outputs the list of columns that fail dtype matching
+    #     Input - dataframe
+    #     Output - list of columns that failed the test caset
+    #     """
+
+    #     # dtype matching columns
+    
+    #     num_list = df.apply(lambda s: pd.to_numeric(s, errors='coerce').notnull().all()).to_list()
+    #     col_list = df.columns
+    #     index_cols = list(zip(num_list, col_list))
+
+    #     dtype_truth = []
+    #     dtype_column = []
+    #     for i in index_cols:
+    #         if i[0]==False:
+    #             if df[i[1]].isna().any() == True:
+    #                 pass
+    #             elif any(df[i[1]].str.contains(r'\b.*[a-zA-Z]+.*\b')) == False:
+    #                 my_truth = 'False'
+    #                 dtype_truth.append(my_truth)
+    #                 dtype_column.append(i[1])
+    #             else:
+    #                 my_truth = 'True'
+    #                 dtype_truth.append(my_truth)
+    #         else:
+    #             my_truth = 'True'
+    #             dtype_truth.append(my_truth)
+                
+    #     if len(dtype_column) != 0:
+    #         print(dtype_column)
+    #     else:
+    #         print('THE COLUMNS AND DATA TYPES MATCH')
+
+    # """def duplicated_columns(df):
+  
+    #     A function to display the columns of those which have duplicate values
+    #     Input - Dataframe
+    #     Output - List of columns which have duplicate values
+     
+    #     """
+
+        
+
+
+    
+
+
+    
+    
