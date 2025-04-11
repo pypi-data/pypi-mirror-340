3b01d4a49160fa217585a6271631562f9d71e907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 15:04:52 +0000

    Add package model-checker/0.8.32/model_checker-0.8.32.tar.gz

---
+++ packages/model-checker/model_checker-0.8.32.tar.gz/model_checker-0.8.32/src/model_checker/theory_lib/bimodal/examples.py
@@ -0,0 +1,500 @@
+"""
+Examples Module for bimodal logic theory.
+
+This module provides a collection of test cases for bimodal semantic theory,
+which combines temporal and modal operators to reason about what is true
+at different times and in different possible worlds.
+
+Module Structure:
+----------------
+1. Imports:
+   - System utilities (os, sys)
+   - Local semantic definitions (BimodalSemantics, BimodalProposition, BimodalStructure)
+   - Local operator definitions (intensional_operators)
+
+2. Semantic Theory:
+   - default_theory: Default bimodal semantic framework
+   - semantic_theories: Dictionary of semantic theory implementations
+
+3. Example Definitions:
+   - example_range: Dictionary of example test cases
+"""
+
+# Try installed package imports first
+try:
+    from model_checker.theory_lib.bimodal.semantic import (
+        BimodalStructure,
+        BimodalSemantics,
+        BimodalProposition,
+    )
+    from model_checker.theory_lib.bimodal.operators import intensional_operators
+except ImportError:
+    # Fall back to local imports for development
+    from .semantic import (
+        BimodalStructure,
+        BimodalSemantics,
+        BimodalProposition,
+    )
+    from .operators import intensional_operators
+
+#######################
+### DEFAULT SETTINGS ###
+#######################
+
+general_settings = {
+    "print_constraints": False,
+    "print_z3": False,
+    "save_output": False,
+}
+
+
+
+
+####################################
+### DEFINE THE SEMANTIC THEORIES ###
+####################################
+
+bimodal_theory = {
+    "semantics": BimodalSemantics,
+    "proposition": BimodalProposition,
+    "model": BimodalStructure,
+    "operators": intensional_operators,
+    # translation dictionary is only required for comparison theories
+}
+
+
+#################################
+### EXTENSIONAL COUNTERMODELS ###
+#################################
+
+# EX_CM_1: 
+EX_CM_1_premises = ['(A \\vee B)']
+EX_CM_1_conclusions = ['(A \\wedge B)']
+EX_CM_1_settings = {
+    'N' : 1,
+    'M' : 1,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+EX_CM_1_example = [
+    EX_CM_1_premises,
+    EX_CM_1_conclusions,
+    EX_CM_1_settings,
+]
+
+
+
+###########################
+### MODAL COUNTERMODELS ###
+###########################
+
+# MD_CM_1: 
+MD_CM_1_premises = ['\\Box (A \\vee B)']
+MD_CM_1_conclusions = ['(\\Box A \\vee \\Box B)']
+MD_CM_1_settings = {
+    'N' : 1,
+    'M' : 1,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+MD_CM_1_example = [
+    MD_CM_1_premises,
+    MD_CM_1_conclusions,
+    MD_CM_1_settings,
+]
+
+# MD_CM_2: 
+MD_CM_2_premises = ['\\Diamond (A \\vee B)']
+MD_CM_2_conclusions = ['(\\Diamond A \\wedge \\Diamond B)']
+MD_CM_2_settings = {
+    'N' : 1,
+    'M' : 1,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+MD_CM_2_example = [
+    MD_CM_2_premises,
+    MD_CM_2_conclusions,
+    MD_CM_2_settings,
+]
+
+# MD_CM_3: 
+MD_CM_3_premises = ['A', '\\neg B']
+MD_CM_3_conclusions = ['\\Diamond (A \\wedge B)']
+MD_CM_3_settings = {
+    'N' : 1,
+    'M' : 1,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+MD_CM_3_example = [
+    MD_CM_3_premises,
+    MD_CM_3_conclusions,
+    MD_CM_3_settings,
+]
+
+# MD_CM_4: 
+MD_CM_4_premises = ['A']
+MD_CM_4_conclusions = ['\\Box A']
+MD_CM_4_settings = {
+    'N' : 1,
+    'M' : 1,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+MD_CM_4_example = [
+    MD_CM_4_premises,
+    MD_CM_4_conclusions,
+    MD_CM_4_settings,
+]
+
+
+# MD_CM_5: 
+MD_CM_5_premises = ['\\Diamond A', '\\Diamond B']
+MD_CM_5_conclusions = ['\\Diamond (A \\wedge B)']
+MD_CM_5_settings = {
+    'N' : 1,
+    'M' : 1,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+MD_CM_5_example = [
+    MD_CM_5_premises,
+    MD_CM_5_conclusions,
+    MD_CM_5_settings,
+]
+
+
+# MD_CM_6: 
+MD_CM_6_premises = ['\\Diamond A']
+MD_CM_6_conclusions = ['\\Box A']
+MD_CM_6_settings = {
+    'N' : 1,
+    'M' : 1,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+MD_CM_6_example = [
+    MD_CM_6_premises,
+    MD_CM_6_conclusions,
+    MD_CM_6_settings,
+]
+
+
+# MD_CM_7: 
+MD_CM_7_premises = ['\\Diamond A', '\\Diamond B']
+MD_CM_7_conclusions = ['(A \\wedge \\neg B)']
+MD_CM_7_settings = {
+    'N' : 1,
+    'M' : 1,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+MD_CM_7_example = [
+    MD_CM_7_premises,
+    MD_CM_7_conclusions,
+    MD_CM_7_settings,
+]
+
+
+
+###########################
+### TENSE COUNTERMODELS ###
+###########################
+
+# TN_CM_1: 
+TN_CM_1_premises = ['A']
+TN_CM_1_conclusions = ['\\Future A']
+TN_CM_1_settings = {
+    'N' : 1,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 5,
+    'expectation' : True,
+}
+TN_CM_1_example = [
+    TN_CM_1_premises,
+    TN_CM_1_conclusions,
+    TN_CM_1_settings,
+]
+
+# TN_CM_2: 
+TN_CM_2_premises = ['\\future A', '\\future B']
+TN_CM_2_conclusions = ['\\future (A \\wedge B)']
+TN_CM_2_settings = {
+    'N' : 1,
+    'M' : 3,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 5,
+    'expectation' : True,
+}
+TN_CM_2_example = [
+    TN_CM_2_premises,
+    TN_CM_2_conclusions,
+    TN_CM_2_settings,
+]
+
+
+
+
+#############################
+### BIMODAL COUNTERMODELS ###
+#############################
+
+# TN_CM_1: 
+BM_CM_1_premises = ['\\Future A']
+BM_CM_1_conclusions = ['\\Box A']
+BM_CM_1_settings = {
+    'N' : 1,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 5,
+    'expectation' : True,
+}
+BM_CM_1_example = [
+    BM_CM_1_premises,
+    BM_CM_1_conclusions,
+    BM_CM_1_settings,
+]
+
+# TN_CM_2: 
+BM_CM_2_premises = ['\\Past A']
+BM_CM_2_conclusions = ['\\Box A']
+BM_CM_2_settings = {
+    'N' : 1,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 5,
+    'expectation' : True,
+}
+BM_CM_2_example = [
+    BM_CM_2_premises,
+    BM_CM_2_conclusions,
+    BM_CM_2_settings,
+]
+
+
+
+
+
+########################
+### BIMODAL THEOREMS ###
+########################
+
+# BM_TH_1: Necessary Future Perpetuity
+BM_TH_1_premises = ['\\Box A']
+BM_TH_1_conclusions = ['\\Future A']
+# BM_TH_1_conclusions = ['\\Past A', 'A', '\\Future A']
+BM_TH_1_settings = {
+    'N' : 1,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 5,
+    'expectation' : False,
+}
+BM_TH_1_example = [
+    BM_TH_1_premises,
+    BM_TH_1_conclusions,
+    BM_TH_1_settings,
+]
+
+# BM_TH_2: Possible Future Perpetuity
+BM_TH_2_premises = ['\\future A']
+BM_TH_2_conclusions = ['\\Diamond A']
+BM_TH_2_settings = {
+    'N' : 1,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 5,
+    'expectation' : False,
+}
+BM_TH_2_example = [
+    BM_TH_2_premises,
+    BM_TH_2_conclusions,
+    BM_TH_2_settings,
+]
+
+# BM_TH_3: Necessary Past Perpetuity
+BM_TH_3_premises = ['\\Box A']
+BM_TH_3_conclusions = ['\\Past A']
+BM_TH_3_settings = {
+    'N' : 1,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : False,
+}
+BM_TH_3_example = [
+    BM_TH_3_premises,
+    BM_TH_3_conclusions,
+    BM_TH_3_settings,
+]
+
+# BM_TH_4: Possible Past Perpetuity 
+BM_TH_4_premises = ['\\past A']
+BM_TH_4_conclusions = ['\\Diamond A']
+BM_TH_4_settings = {
+    'N' : 1,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : False,
+}
+BM_TH_4_example = [
+    BM_TH_4_premises,
+    BM_TH_4_conclusions,
+    BM_TH_4_settings,
+]
+
+
+
+
+
+
+######################
+### MODAL THEOREMS ###
+######################
+
+# MD_TH_1:
+MD_TH_1_premises = ['\\Box (A \\rightarrow B)']
+MD_TH_1_conclusions = ['(\\Box A \\rightarrow \\Box B)']
+MD_TH_1_settings = {
+    'N' : 2,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : False,
+}
+MD_TH_1_example = [
+    MD_TH_1_premises,
+    MD_TH_1_conclusions,
+    MD_TH_1_settings,  # can use example_settings from above
+]
+
+# MD_TH_2:
+MD_TH_2_premises = ['A']
+MD_TH_2_conclusions = ['\\Future \\past A']
+MD_TH_2_settings = {
+    'N' : 2,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : False,
+}
+MD_TH_2_example = [
+    MD_TH_2_premises,
+    MD_TH_2_conclusions,
+    MD_TH_2_settings,
+]
+
+
+
+###############################################
+### DEFINE EXAMPLES AND THEORIES TO COMPUTE ###
+###############################################
+
+# NOTE: at least one theory is required, multiple are permitted for comparison
+semantic_theories = {
+    "Brast-McKie" : bimodal_theory,
+    # additional theories will require their own translation dictionaries
+}
+
+test_example_range = {
+    ### COUNTERMODELS ###
+
+    # Extensional Countermodels
+    "EX_CM_1" : EX_CM_1_example,
+    
+    # Modal Countermodels
+    "MD_CM_1" : MD_CM_1_example,
+    "MD_CM_2" : MD_CM_2_example,
+    "MD_CM_3" : MD_CM_3_example,
+    "MD_CM_4" : MD_CM_4_example,
+    "MD_CM_5" : MD_CM_5_example,
+    "MD_CM_6" : MD_CM_6_example,
+    "MD_CM_7" : MD_CM_7_example,
+
+    # Tense Countermodels
+    "TN_CM_1" : TN_CM_1_example,
+    "TN_CM_2" : TN_CM_2_example,
+
+    # Bimodal Countermodels
+    "BM_CM_1" : BM_CM_1_example,
+    "BM_CM_2" : BM_CM_2_example,
+
+    ### THEOREMS ###
+
+    # Bimodal Theorems
+    "BM_TH_1" : BM_TH_1_example,
+    "BM_TH_2" : BM_TH_2_example,
+    "BM_TH_3" : BM_TH_3_example,
+    "BM_TH_4" : BM_TH_4_example,
+
+    # Modal Theorems
+    "MD_TH_1" : MD_TH_1_example,
+    "MD_TH_2" : MD_TH_2_example,
+}
+
+# NOTE: at least one example is required, multiple are permitted for comparison
+example_range = {
+
+    ### COUNTERMODELS ###
+
+    # Extensional Countermodels
+    # "EX_CM_1" : EX_CM_1_example,
+    
+    # Modal Countermodels
+    # "MD_CM_1" : MD_CM_1_example,
+    # "MD_CM_2" : MD_CM_2_example,
+    # "MD_CM_3" : MD_CM_3_example,
+    # "MD_CM_4" : MD_CM_4_example,
+    # "MD_CM_5" : MD_CM_5_example,
+    # "MD_CM_6" : MD_CM_6_example,
+    # "MD_CM_7" : MD_CM_7_example,
+
+    # Tense Countermodels
+    # "TN_CM_1" : TN_CM_1_example,
+    # "TN_CM_2" : TN_CM_2_example,
+
+    # Bimodal Countermodels
+    # "BM_CM_1" : BM_CM_1_example,
+    "BM_CM_2" : BM_CM_2_example,
+
+    ### THEOREMS ###
+
+    # Bimodal Theorems
+    "BM_TH_1" : BM_TH_1_example,
+    # "BM_TH_2" : BM_TH_2_example,
+    # "BM_TH_3" : BM_TH_3_example,
+    # "BM_TH_4" : BM_TH_4_example,
+
+    # Modal Theorems
+    # "MD_TH_1" : MD_TH_1_example,
+    # "MD_TH_2" : MD_TH_2_example,
+}
