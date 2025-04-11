fbca1837440b5fd8c873e5ddc8e7d0f25a7c9583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 17:09:41 +0000

    Add package edx-event-bus-kafka/6.1.0/edx_event_bus_kafka-6.1.0-py2.py3-none-any.whl

---
+++ packages/edx-event-bus-kafka/edx_event_bus_kafka-6.1.0-py2.py3-none-any.whl/edx_event_bus_kafka/__init__.py
@@ -0,0 +1,12 @@
+"""
+Kafka implementation for Open edX event bus.
+
+Public API will be in this module for the most part.
+
+See ADR ``docs/decisions/0006-public-api-and-app-organization.rst`` for the reasoning.
+"""
+
+from edx_event_bus_kafka.internal.consumer import KafkaEventConsumer
+from edx_event_bus_kafka.internal.producer import KafkaEventProducer, create_producer
+
+__version__ = '6.1.0'
