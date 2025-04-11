20d524a5589d1d82de67e127025af34a3d9d2ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 04:04:13 +0000

    Add package cg/69.3.1/cg-69.3.1.tar.gz

---
+++ packages/cg/cg-69.3.1.tar.gz/cg-69.3.1/cg/cli/upload/utils.py
@@ -0,0 +1,22 @@
+"""Utility functions for the upload cli commands."""
+
+import logging
+
+import rich_click as click
+
+from cg.constants import Workflow
+from cg.constants.constants import MAX_ITEMS_TO_RETRIEVE
+from cg.store.models import Analysis
+from cg.store.store import Store
+
+LOG = logging.getLogger(__name__)
+
+
+def suggest_cases_to_upload(status_db: Store, workflow: Workflow | None = None) -> None:
+    """Print a list of suggested cases to upload."""
+    LOG.warning("Provide a case, suggestions:")
+    records: list[Analysis] = status_db.get_analyses_to_upload(workflow=workflow)[
+        :MAX_ITEMS_TO_RETRIEVE
+    ]
+    for case_obj in records:
+        click.echo(case_obj)
