89dc88d23c6e8968c8aac3aa672fbc286703db23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 08:04:23 +0000

    Add package ob-metaflow-stubs/6.0.3.154/ob_metaflow_stubs-6.0.3.154-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.154-py2.py3-none-any.whl/metaflow-stubs/mf_extensions/obcheckpoint/plugins/machine_learning_utilities/modeling_utils/exceptions.pyi
@@ -0,0 +1,25 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.7.2+obcheckpoint(0.1.9);ob(v1)                                                    #
+# Generated on 2025-04-10T22:13:14.948431                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.exception
+
+from ......exception import MetaflowException as MetaflowException
+
+class LoadingException(metaflow.exception.MetaflowException, metaclass=type):
+    def __init__(self, message):
+        ...
+    ...
+
+class ModelException(metaflow.exception.MetaflowException, metaclass=type):
+    def __init__(self, message):
+        ...
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.154-py2.py3-none-any.whl/metaflow-stubs/profilers/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.7.2+obcheckpoint(0.1.9);ob(v1)                                                    #
+# Generated on 2025-04-10T22:13:14.873421                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from ..mf_extensions.outerbounds.profilers import gpu as gpu
+from ..mf_extensions.outerbounds.profilers.gpu import gpu_profile as gpu_profile
+
