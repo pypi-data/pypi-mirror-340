464d5ac43ec35aa7353cec9dd1c39b2acc7d09ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 14:04:37 +0000

    Add package pandaplotdocs/0.1.0/pandaplotdocs-0.1.0-py3-none-any.whl

---
+++ packages/pandaplotdocs/pandaplotdocs-0.1.0-py3-none-any.whl/pandaplotdocs/__init__.py
@@ -0,0 +1,7 @@
+"""A simple library to generate a PDF reference for Pandas and Matplotlib."""
+
+from .generator import generate_docs_pdf
+
+__version__ = "0.1.0"  # Initial version
+
+__all__ = ['generate_docs_pdf'] 
