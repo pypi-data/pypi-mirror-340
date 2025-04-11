5acbde197acc7eb5e39a246e4001e069663a782f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 03:17:34 +0000

    Add package gfw-api-python-client/3.0.0/gfw_api_python_client-3.0.0.tar.gz

---
+++ packages/gfw-api-python-client/gfw_api_python_client-3.0.0.tar.gz/gfw_api_python_client-3.0.0/src/gfwapiclient/resources/datasets/__init__.py
@@ -0,0 +1,5 @@
+"""Global Fishing Watch (GFW) API Python Client - Datasets API Resource.
+
+For more details, visit the official
+`Global Fishing Watch Datasets API Documentation https://globalfishingwatch.org/our-apis/documentation#datasets-api`_.
+"""
