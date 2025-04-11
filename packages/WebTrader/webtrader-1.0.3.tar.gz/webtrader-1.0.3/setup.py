f9a0eadcabeea257b46e5313f3d21e5a8bc6666b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:04:50 +0000

    Add package WebTrader/1.0.3/webtrader-1.0.3.tar.gz

---
+++ packages/WebTrader/webtrader-1.0.3.tar.gz/webtrader-1.0.3/setup.py
@@ -0,0 +1,17 @@
+﻿from setuptools import setup, find_packages
+
+with open("readme.md", "r") as f:
+    description=f.read()
+setup(
+    name="WebTrader",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requiress=["pyperclip", "requests"],
+    entry_points={
+         "console_scripts": [
+              "webtrader = WebTrader:webtrader" 
+        ]
+    },
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
