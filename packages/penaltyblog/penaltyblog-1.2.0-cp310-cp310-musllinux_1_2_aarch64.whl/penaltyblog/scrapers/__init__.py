7e55f3cdab643601f7482927b431f3d17d3df5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 06:04:33 +0000

    Add package penaltyblog/1.2.0/penaltyblog-1.2.0-cp310-cp310-musllinux_1_2_aarch64.whl

---
+++ packages/penaltyblog/penaltyblog-1.2.0-cp310-cp310-musllinux_1_2_aarch64.whl/penaltyblog/scrapers/__init__.py
@@ -0,0 +1,5 @@
+from .clubelo import ClubElo  # noqa
+from .fbref import FBRef  # noqa
+from .footballdata import FootballData  # noqa
+from .team_mappings import get_example_team_name_mappings  # noqa
+from .understat import Understat  # noqa
