f95956a011d5b885b97e373c521c00cc87d06386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 06:05:08 +0000

    Add package cionuke/0.7.2b1/cionuke-0.7.2b1-py2.py3-none-any.whl

---
+++ packages/cionuke/cionuke-0.7.2b1-py2.py3-none-any.whl/cionuke/const.py
@@ -0,0 +1,17 @@
+import os
+
+NOT_CONNECTED = "-                 NOT CONNECTED                 -"
+INVALID = "INVALID - Please press Connect"
+
+FEATURE_DEV = os.environ.get("CIO_FEATURE_DEV", False)
+
+VERSION = "dev.999"
+PLUGIN_DIR = os.path.dirname(__file__)
+
+CONDUCTOR_COMMAND_PATH = os.path.join(os.path.dirname(PLUGIN_DIR), "bin", "conductor")
+
+try:
+    with open(os.path.join(PLUGIN_DIR, "VERSION")) as version_file:
+        VERSION = version_file.read().strip()
+except BaseException:
+    pass
