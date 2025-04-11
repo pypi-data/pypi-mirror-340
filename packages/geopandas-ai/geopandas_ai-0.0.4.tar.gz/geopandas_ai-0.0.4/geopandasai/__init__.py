394290fa988c48317abae17eda83a85776822e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 04:04:01 +0000

    Add package geopandas-ai/0.0.4/geopandas_ai-0.0.4.tar.gz

---
+++ packages/geopandas-ai/geopandas_ai-0.0.4.tar.gz/geopandas_ai-0.0.4/geopandasai/__init__.py
@@ -0,0 +1,6 @@
+from .cache import *
+from .config import set_active_lite_llm_config
+from .geodataframe_ai import GeoDataFrameAI
+from .prompt import prompt_with_dataframes
+from .types import *
+from .wrapper import *
