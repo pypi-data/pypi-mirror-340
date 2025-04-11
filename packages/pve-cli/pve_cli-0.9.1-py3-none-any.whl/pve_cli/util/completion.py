60d33751d84d2d699c13479f7c1afddf84f8eb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:06:10 +0000

    Add package pve-cli/0.9.1/pve_cli-0.9.1-py3-none-any.whl

---
+++ packages/pve-cli/pve_cli-0.9.1-py3-none-any.whl/pve_cli/util/completion.py
@@ -0,0 +1,29 @@
+import typer
+
+from ..proxmox.api import Proxmox
+
+
+def endpoint_complete(ctx: typer.Context, incomplete: str):
+    config = ctx.obj['config']
+    endpoints = config.get('endpoint', {})
+    for endpoint in endpoints:
+        if endpoint.startswith(incomplete):
+            yield endpoint, endpoints[endpoint]['host']
+
+
+def vm_complete(ctx: typer.Context, incomplete: str):
+    proxmox_api: Proxmox = ctx.obj['proxmox_api']
+    vms = proxmox_api.vm.list_()
+
+    for vm in vms:
+        yield str(vm['vmid']), vm['name']
+        yield vm['name'], vm['name']
+
+
+def node_complete(ctx: typer.Context, incomplete: str):
+    proxmox_api: Proxmox = ctx.obj['proxmox_api']
+    nodes = proxmox_api.node.list_()
+
+    for node in nodes:
+        if node['node'].startswith(incomplete):
+            yield node['node'], node['node']
