369df35c51fdb5189fdff9712268737d5f9d48ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:04:28 +0000

    Add package weaviate-client/4.13.2/weaviate_client-4.13.2.tar.gz

---
+++ packages/weaviate-client/weaviate_client-4.13.2.tar.gz/weaviate_client-4.13.2/docs/conf.py
@@ -0,0 +1,93 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import contextlib
+import os
+import sys
+
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as version_func
+
+# -- Path setup --------------------------------------------------------------
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
+@contextlib.contextmanager
+def chdir(directory):
+    curdir = os.curdir
+    try:
+        os.chdir(directory)
+        yield
+    finally:
+        os.chdir(curdir)
+
+
+try:
+    version = version_func("weaviate")
+except PackageNotFoundError:
+    # The project is not installed in readthedocs environment. Read the version with setuptools_scm.
+    import setuptools_scm
+
+    with chdir(".."):
+        release = setuptools_scm.get_version()
+
+
+# -- Project information -----------------------------------------------------
+
+project = "Weaviate Python Client"
+copyright = "2021-2024, Weaviate"
+author = "Weaviate"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosectionlabel",
+    "sphinxcontrib.autodoc_pydantic",
+]
+
+# Autodoc settings for pydantic
+autodoc_pydantic_model_show_json = False
+autodoc_pydantic_model_show_config_summary = False
+autodoc_pydantic_model_show_validator_summary = False
+autodoc_pydantic_model_show_validator_members = False
+autodoc_pydantic_model_show_field_summary = False
+autodoc_pydantic_model_undoc_members = False
+autodoc_pydantic_model_members = False
+
+
+# Make sure the target is unique
+autosectionlabel_prefix_document = True
+
+autoclass_content = "both"
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []
---
+++ packages/weaviate-client/weaviate_client-4.13.2.tar.gz/weaviate_client-4.13.2/docs/weaviate.rst
@@ -0,0 +1,86 @@
+Weaviate Packages
+=================
+
+.. automodule:: weaviate
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   weaviate.backup
+   weaviate.collections
+   weaviate.connect
+   weaviate.rbac
+
+Submodules
+----------
+
+weaviate.auth module
+--------------------
+
+.. automodule:: weaviate.auth
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+weaviate.classes module
+-----------------------
+
+.. automodule:: weaviate.classes
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+weaviate.client module
+----------------------
+
+.. automodule:: weaviate.client
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+weaviate.config module
+----------------------
+
+.. automodule:: weaviate.config
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+weaviate.embedded module
+------------------------
+
+.. automodule:: weaviate.embedded
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+weaviate.exceptions module
+--------------------------
+
+.. automodule:: weaviate.exceptions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+weaviate.types module
+---------------------
+
+.. automodule:: weaviate.types
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+weaviate.util module
+--------------------
+
+.. automodule:: weaviate.util
+   :members:
+   :undoc-members:
+   :show-inheritance:
