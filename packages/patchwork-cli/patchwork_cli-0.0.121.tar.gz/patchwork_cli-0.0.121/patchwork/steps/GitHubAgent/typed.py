9131791133728e74fe181187744082174d968244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:13 +0000

    Add package patchwork-cli/0.0.121/patchwork_cli-0.0.121.tar.gz

---
+++ packages/patchwork-cli/patchwork_cli-0.0.121.tar.gz/patchwork_cli-0.0.121/patchwork/steps/GitHubAgent/typed.py
@@ -0,0 +1,40 @@
+from typing_extensions import Annotated, Any, Dict, TypedDict
+
+from patchwork.common.utils.step_typing import StepTypeConfig
+
+
+class __GitHubAgentRequiredInputs(TypedDict):
+    task: str
+
+
+class GitHubAgentInputs(__GitHubAgentRequiredInputs, total=False):
+    base_path: str
+    prompt_value: Dict[str, Any]
+    max_llm_calls: Annotated[int, StepTypeConfig(is_config=True)]
+    openai_api_key: Annotated[
+        str,
+        StepTypeConfig(
+            is_config=True, or_op=["patched_api_key", "google_api_key", "client_is_gcp", "anthropic_api_key"]
+        ),
+    ]
+    anthropic_api_key: Annotated[
+        str,
+        StepTypeConfig(is_config=True, or_op=["patched_api_key", "google_api_key", "client_is_gcp", "openai_api_key"]),
+    ]
+    google_api_key: Annotated[
+        str,
+        StepTypeConfig(
+            is_config=True, or_op=["patched_api_key", "openai_api_key", "client_is_gcp", "anthropic_api_key"]
+        ),
+    ]
+    client_is_gcp: Annotated[
+        str,
+        StepTypeConfig(
+            is_config=True, or_op=["patched_api_key", "openai_api_key", "anthropic_api_key", "google_api_key"]
+        ),
+    ]
+
+
+class GitHubAgentOutputs(TypedDict):
+    request_tokens: int
+    response_tokens: int
