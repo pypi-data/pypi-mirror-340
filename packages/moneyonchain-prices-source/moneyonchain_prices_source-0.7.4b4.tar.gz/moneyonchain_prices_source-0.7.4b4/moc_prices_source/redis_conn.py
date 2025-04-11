192d2c6856a366053537729e8d9fd35e6e426907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 03:17:30 +0000

    Add package moneyonchain-prices-source/0.7.4b4/moneyonchain_prices_source-0.7.4b4.tar.gz

---
+++ packages/moneyonchain-prices-source/moneyonchain_prices_source-0.7.4b4.tar.gz/moneyonchain_prices_source-0.7.4b4/moc_prices_source/redis_conn.py
@@ -0,0 +1,71 @@
+import json
+from sys import stderr
+from os.path import basename, dirname, abspath, expanduser
+from json.decoder import JSONDecodeError
+from redis import Redis
+
+
+
+def get_redis_conf():
+    app_dir  = dirname(abspath(__file__))
+    app_name = basename(app_dir)
+    redis_conf_files = [expanduser("~") + '/.' + app_name + '/redis.json',
+                        expanduser("~") + '/.' + app_name + '/redis_default.json',
+                        app_dir + '/data/redis.json',
+                        app_dir + '/data/redis_default.json']
+    redis_conf = {}
+    for file_ in redis_conf_files:
+        try:
+            with open(file_, 'r') as f:
+                redis_conf = json.load(f)
+        except JSONDecodeError as e:
+            print(f'Error in "{file_}", {str(e)}', file=stderr)
+            exit(1)
+        except Exception as e:
+            redis_conf = {}
+        if redis_conf:
+            break
+    redis_conf['file'] = file_
+    connection_parameters = {}
+    for key, type_ in [('host', str),
+                       ('port', int),
+                       ('db', int),
+                       ('unix_socket_path', str)]:
+        if key in redis_conf:
+            try:
+                connection_parameters[key] = type_(redis_conf[key])
+            except Exception as e:
+                print(f'Error in "{file_}", {str(e)}',
+                      file=stderr)
+                exit(1)
+    redis_conf['connection_parameters'] = connection_parameters
+    return redis_conf
+
+redis_conf = get_redis_conf()
+
+use_redis = redis_conf.get('enable', False)
+
+redis_conf_file = redis_conf.get('file', 'unknown')
+
+redis_conn_parameters = redis_conf.get('connection_parameters', {})
+
+def get_redis():
+    if not use_redis:
+        return None
+    try:
+        redis = Redis(**redis_conn_parameters)
+        redis.ping()
+        return redis
+    except Exception as e:
+        print(f'Error in "{redis_conf_file}", {str(e)}', file=stderr)
+        exit(1)
+
+
+
+if __name__ == '__main__':
+    print("File: {}, Ok!".format(repr(__file__)))
+    print()
+    print(f"{redis_conf_file=}")
+    print(f"{use_redis=}")
+    print(f"{redis_conn_parameters=}")
+
