f90c625475d560dffeae4d48b6a9e90c997de779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 03:17:38 +0000

    Add package symbiosis-api-client/0.0.3/symbiosis_api_client-0.0.3-py3-none-any.whl

---
+++ packages/symbiosis-api-client/symbiosis_api_client-0.0.3-py3-none-any.whl/symbiosis_api_client/__init__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present Garuda <106117111+garootman@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+from .client import SymbiosisClient as SymbiosisClient
