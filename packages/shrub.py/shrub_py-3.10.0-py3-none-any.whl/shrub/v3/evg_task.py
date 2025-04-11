12f8fddc667e36a89e395afa43ebfef58d31f350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 06:05:13 +0000

    Add package shrub.py/3.10.0/shrub_py-3.10.0-py3-none-any.whl

---
+++ packages/shrub.py/shrub_py-3.10.0-py3-none-any.whl/shrub/v3/evg_task.py
@@ -0,0 +1,74 @@
+"""Evergreen models for tasks."""
+from typing import List, Optional, Union
+
+from pydantic import BaseModel
+
+from shrub.v3.evg_command import EvgCommand
+
+
+class EvgTaskDependency(BaseModel):
+    """
+    Specification of what a task depends on.
+
+    * name: Name of task to depend on.
+    * variant: Name of build variant depending task should run on.
+    """
+
+    name: str
+    variant: Optional[str] = None
+    status: Optional[str] = None
+    patch_optional: Optional[bool] = None
+    omit_generated_tasks: Optional[bool] = None
+
+
+class EvgTaskRef(BaseModel):
+    """
+    Reference to an evergreen task.
+
+    * name: Name of task.
+    * distros: Distro that task should be run on.
+    * depends_on: Other tasks that must be successful for this task to run.
+    * batchtime: How frequent this task should be run.
+    """
+
+    name: str
+    distros: Optional[List[str]] = None
+    depends_on: Optional[List[EvgTaskDependency]] = None
+    batchtime: Optional[int] = None
+
+
+class EvgTask(BaseModel):
+    """
+    Definition of a task.
+
+    * name: Name of task.
+    * commands: List of commands that make up task.
+    * depends_on: Other tasks that must be successful for this task to run.
+    * run_on: Distros this task should be run on.
+    * exec_timeout_secs: Time task can run before being considered timing out.
+    * tags: List of tags to attach to task.
+    * disable: Prevent this task from running at all.
+    * patchable: Whether this task can run in patch builds.
+    * batchtime: How frequent this task should be run.
+    * stepback: Whether this task should run stepback.
+    """
+
+    name: str
+    commands: Optional[List[EvgCommand]] = None
+    depends_on: Optional[List[EvgTaskDependency]] = None
+    run_on: Optional[Union[str, List[str]]] = None
+    exec_timeout_secs: Optional[int] = None
+    tags: Optional[List[str]] = None
+    disable: Optional[bool] = None
+    patchable: Optional[bool] = None
+    # batchtime: Optional[int] = None # DEVPROD-12032
+    stepback: Optional[bool] = None
+
+    def get_task_ref(self, distros: Optional[List[str]] = None) -> EvgTaskRef:
+        """
+        Get a reference to this task.
+
+        :param distros: List of distros this task should be run on.
+        :return: Reference to this task.
+        """
+        return EvgTaskRef(name=self.name, distros=distros)
