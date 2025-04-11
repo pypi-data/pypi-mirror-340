6b8ee92ec6c1bf71ba039ba46fe2d05b693c4e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 04:04:06 +0000

    Add package betlib/0.1.2.5/betlib-0.1.2.5.tar.gz

---
+++ packages/betlib/betlib-0.1.2.5.tar.gz/betlib-0.1.2.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="betlib",
+    version="0.1.2.5",
+    author="Pierre Peigné",
+    description="Minimalist Library for calling PRISM Eval BET API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.11",
+    install_requires=[
+        "requests",
+        "aiohttp",
+        "python-dotenv",
+    ],
+    extras_require={
+        "dev": [
+            "pytest"
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
