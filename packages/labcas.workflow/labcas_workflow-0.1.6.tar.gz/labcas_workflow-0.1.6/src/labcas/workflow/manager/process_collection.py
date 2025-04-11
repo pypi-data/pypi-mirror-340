08cba599ef1aa09bd705ee86615661dfe907e35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 04:04:53 +0000

    Add package labcas.workflow/0.1.6/labcas_workflow-0.1.6.tar.gz

---
+++ packages/labcas.workflow/labcas_workflow-0.1.6.tar.gz/labcas_workflow-0.1.6/src/labcas/workflow/manager/process_collection.py
@@ -0,0 +1,19 @@
+import os
+from labcas.workflow.manager import DataStore
+from labcas.workflow.steps.alphan.process import process_img
+
+
+def process_collection(bucket_name, in_prefix, out_prefix, fun, kwargs):
+    # Use a breakpoint in the code line below to debug your script.
+
+    datastore = DataStore(bucket_name, in_prefix, out_prefix)
+
+    for obj in datastore.get_inputs():
+        in_key = obj['Key']
+        print(in_key)
+        print(in_key)
+        fun(
+            datastore,
+            in_key,
+            **kwargs
+        )
