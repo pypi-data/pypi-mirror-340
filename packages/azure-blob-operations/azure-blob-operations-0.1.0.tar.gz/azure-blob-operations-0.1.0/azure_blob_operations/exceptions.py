71254512b0b1db3ce198192e6410f57ec24c1bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:06:29 +0000

    Add package azure-blob-operations/0.1.0/azure-blob-operations-0.1.0.tar.gz

---
+++ packages/azure-blob-operations/azure-blob-operations-0.1.0.tar.gz/azure-blob-operations-0.1.0/azure_blob_operations/exceptions.py
@@ -0,0 +1,6 @@
+class BlobOperationError(Exception):
+    """Base exception for blob operation errors."""
+    def __init__(self, message: str, status_code: int = 400):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(self.message)
---
+++ packages/azure-blob-operations/azure-blob-operations-0.1.0.tar.gz/azure-blob-operations-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="azure-blob-operations",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "azure-storage-blob>=12.19.0",
+    ],
+    author="Uday Pallati",
+    author_email="udaypallati123@gmail.com",
+    description="A utility package for generic Azure Blob Storage operations",
+    url="https://github.com/yourusername/azure-blob-operations",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11.5",
+)
