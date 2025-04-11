db83f5d94c08c05bfeaf74f312e73b3773318576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 06:04:56 +0000

    Add package sculblog/0.1.13/sculblog-0.1.13.tar.gz

---
+++ packages/sculblog/sculblog-0.1.13.tar.gz/sculblog-0.1.13/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sculblog",
+    version="0.1.13",
+    packages=find_packages(),
+    install_requires=[
+        "markdown",
+        "beautifulsoup4",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sculblog=sculblog.main:main",  # Changed from cli:main to main:main
+        ],
+    },
+    author="Diego Cabello",
+    description="Super Cool Utility Lightweight Blog - A minimalist blogging framework",
+    python_requires=">=3.6",
+)
