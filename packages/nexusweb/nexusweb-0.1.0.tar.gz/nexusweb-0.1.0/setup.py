7d276daf3566fe2e7dfddecaa4e3a5b090640204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:04:55 +0000

    Add package nexusweb/0.1.0/nexusweb-0.1.0.tar.gz

---
+++ packages/nexusweb/nexusweb-0.1.0.tar.gz/nexusweb-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nexusweb",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Faded",
+    author_email="nexusweb@gmail.com",
+    description="A flexible micro web framework with built-in blueprints, API docs, JSON support, and rate limiting.",
+    license="MIT",
+    keywords="web framework wsgi nexus",
+)
