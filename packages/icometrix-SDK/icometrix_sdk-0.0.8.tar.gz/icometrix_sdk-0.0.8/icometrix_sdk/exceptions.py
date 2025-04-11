65fe46a0e74bbc41fa869f9c12f3d657a801dc5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:11 +0000

    Add package icometrix-SDK/0.0.8/icometrix_sdk-0.0.8.tar.gz

---
+++ packages/icometrix-SDK/icometrix_sdk-0.0.8.tar.gz/icometrix_sdk-0.0.8/icometrix_sdk/exceptions.py
@@ -0,0 +1,34 @@
+class IcometrixException(Exception):
+    pass
+
+
+class IcometrixHttpException(IcometrixException):
+    fmt = "An HTTP Client raised an unhandled exception: {error}"
+
+
+class IcometrixParseException(IcometrixException):
+    fmt = "Failed to parse HTTP response body"
+
+
+class IcometrixConfigException(IcometrixException):
+    fmt = "Invalid configuration"
+
+
+class IcometrixAuthException(IcometrixException):
+    fmt = "Authentication failed"
+
+
+class IcometrixDataImportException(IcometrixException):
+    fmt = "Dicom import failed"
+
+
+class IcometrixInvalidInputDataException(IcometrixException):
+    fmt = "Data validation failed"
+
+
+class WaitTimeoutException(IcometrixException):
+    ...
+
+
+class IcometrixUnusableReportException(IcometrixException):
+    pass
