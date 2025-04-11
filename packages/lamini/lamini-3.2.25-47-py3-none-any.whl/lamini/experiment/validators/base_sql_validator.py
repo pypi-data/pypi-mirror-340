550f6788706e61c1ab61ecfcd3d1c33adfba3889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 07:04:54 +0000

    Add package lamini/3.2.25/lamini-3.2.25-47-py3-none-any.whl

---
+++ packages/lamini/lamini-3.2.25-47-py3-none-any.whl/lamini/experiment/validators/base_sql_validator.py
@@ -0,0 +1,48 @@
+# TODO: This change is added so that we can support GPT call part of BaseGenerator
+#        Please note that when this is pushed into SDK we need to have some filtering for external users
+
+from lamini.experiment.validators import BaseValidator
+from lamini.experiment.generators import BaseSQLGenerator
+
+
+class BaseSQLValidator(BaseSQLGenerator, BaseValidator):
+    """Base class for SQL validation that combines generator and validator capabilities."""
+
+    def __init__(
+        self,
+        model,
+        client=None,
+        db_type=None,
+        db_params=None,
+        name="BaseSQLValidator",
+        instruction=None,
+        output_type=None,
+        **kwargs,
+    ):
+        # Initialize BaseSQLGenerator first
+        BaseSQLGenerator.__init__(
+            self,
+            model=model,
+            client=client,
+            db_type=db_type,
+            db_params=db_params,
+            name=name,
+            instruction=instruction,
+            output_type=output_type,
+            **kwargs,
+        )
+
+        # Initialize BaseValidator
+        BaseValidator.__init__(
+            self,
+            model=model,
+            client=client,
+            name=name,
+            instruction=instruction,
+            output_type=output_type,
+            **kwargs,
+        )
+
+    def __call__(self, prompt_obj, debug=False):
+        """Base validation method to be implemented by subclasses."""
+        return BaseValidator.__call__(self, prompt_obj, debug)
