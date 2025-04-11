004e6c09fa9e98ee3847f8ea9a92a8fb4dd1ec27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:25 +0000

    Add package oarepo-model-builder-rdm/1.0.16/oarepo_model_builder_rdm-1.0.16.tar.gz

---
+++ packages/oarepo-model-builder-rdm/oarepo_model_builder_rdm-1.0.16.tar.gz/oarepo_model_builder_rdm-1.0.16/oarepo_model_builder_rdm/datatypes/components/__init__.py
@@ -0,0 +1,19 @@
+from .draft_record import RDMDraftParentComponent
+from .ext_resource import RDMExtResourceModelComponent
+from .links import RDMLinksComponent
+from .marshmallow import RDMMarshmallowModelComponent
+from .record import RDMRecordModelComponent
+from .search_options import RDMSearchOptionsModelComponent
+from .service import RDMServiceComponent
+from .ui_marshmallow import RDMUIMarshmallowModelComponent
+
+RDM_COMPONENTS = [
+    RDMServiceComponent,
+    RDMRecordModelComponent,
+    RDMExtResourceModelComponent,
+    RDMDraftParentComponent,
+    RDMMarshmallowModelComponent,
+    RDMSearchOptionsModelComponent,
+    RDMUIMarshmallowModelComponent,
+    RDMLinksComponent,
+]
---
+++ packages/oarepo-model-builder-rdm/oarepo_model_builder_rdm-1.0.16.tar.gz/oarepo_model_builder_rdm-1.0.16/oarepo_model_builder_rdm/invenio/__init__.py
@@ -0,0 +1,7 @@
+TEMPLATES = {
+    "rdm-record-metadata": "templates/rdm_record_metadata.py.jinja2",
+    "rdm-record": "templates/rdm_record.py.jinja2",
+    "rdm-draft-record": "templates/rdm_draft_record.py.jinja2",
+    "rdm-record-search-options": "templates/rdm_record_search_options.py.jinja2",
+    "rdm-ext": "templates/rdm_ext.py.jinja2",
+}
