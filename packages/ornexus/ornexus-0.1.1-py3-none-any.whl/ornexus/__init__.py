2ab348376fb1b0e9d39ce5ff385f352906a6c10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 06:05:13 +0000

    Add package ornexus/0.1.1/ornexus-0.1.1-py3-none-any.whl

---
+++ packages/ornexus/ornexus-0.1.1-py3-none-any.whl/ornexus/__init__.py
@@ -0,0 +1,10 @@
+"""
+OrNexus - Framework para criação de agentes com Agno
+"""
+
+__version__ = "0.1.0"
+
+from .utils import OrNexusConfig
+from .main import OrNexus, main
+
+__all__ = ["OrNexus", "OrNexusConfig", "main"] 
