fd9a08baf7c52fd18c93d067f809f02f5a38f72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:05 +0000

    Add package xplt/0.11.10/xplt-0.11.10.tar.gz

---
+++ packages/xplt/xplt-0.11.10.tar.gz/xplt-0.11.10/xplt/__init__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Philipp Niedermayer"
+__contact__ = "eltos@outlook.de"
+
+
+__version__ = "0.11.10"
+
+
+# expose the following in global namespace
+from .colors import *
+from .line import *
+from .particles import *
+from .phasespace import *
+from .properties import *
+from .timestructure import *
+from .twiss import *
+from .util import AUTO
+
+# allow usage of xplt.mpl.* without importing matplotlib
+import matplotlib as mpl
+
+
+# Deprecated, for backwards compatibility
+class TimeVariationPlot(SpillQualityPlot):
+    """
+    .. deprecated:: 0.8
+        Use :class:`xplt.SpillQualityPlot` instead.
+    """
+
+    pass
+
+
+class TimeVariationScalePlot(SpillQualityTimescalePlot):
+    """
+    .. deprecated:: 0.8
+        Use :class:`xplt.SpillQualityTimescalePlot` instead.
+    """
+
+    pass
+
+
+from . import hooks as _hooks
+
+_hooks.try_register_hooks()
+
+import matplotlib.style as _mpl_style
+
+
+def apply_style():
+    """Apply xplt's matplotlib style sheet and update rcParams"""
+    _mpl_style.use("xplt.xplt")
+    # import pint.formatting as _pint_formatting
+    # _pint_formatting.format_default = "X"  # use explicit format instead!
