89130597fa388c735696891b4e6722b785987d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:07:51 +0000

    Add package daily-funcs/0.1.1/daily_funcs-0.1.1.tar.gz

---
+++ packages/daily-funcs/daily_funcs-0.1.1.tar.gz/daily_funcs-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='daily_funcs',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'tqdm',
+        'datetime',
+        'math',
+        'json',
+    ],
+    author='gouqi',
+    author_email='1249224822@qq.com',
+    description='daily tools for data science employee',
+    license='MIT',
+    keywords='daily tools for data science employee',
+    url='https://github.com/gouqi/daily_tools'
+)
