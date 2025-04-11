d4c7aa9e471ef16425ad979a0bef74a0b70431af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:27 +0000

    Add package europa/0.1.2/europa-0.1.2.tar.gz

---
+++ packages/europa/europa-0.1.2.tar.gz/europa-0.1.2/src/europa/services/sample_single_queue_service.py
@@ -0,0 +1,60 @@
+import time
+import random
+
+from europa.framework.queues.queue_manager import QueueManager, QueueType, QueueItem
+from europa.framework.service import SingleQueueService
+from europa.framework.endpoint import expose
+
+
+class SampleSinglePostgresQueueService(SingleQueueService):
+
+    def custom_startup(self):
+        self.queue_manager = QueueManager(
+            queue_type=QueueType.POSTGRES,
+            conn_params={
+                "host": "localhost",
+                "port": 5557,
+                "user": "postgres",
+                "password": "example",
+            },
+            table_name="test_q_name",
+        )
+        item_id = self.queue_manager.enqueue({"count": 0})
+        self.log.info(f"queued starting item with id: {item_id}")
+
+    def calculate(self, queue_item: QueueItem):
+        # Custom calculation logic for this service
+
+        self.log.info(f"count = {queue_item.payload['count']}")
+        payload = queue_item.payload
+        payload["count"] += 1
+        self.queue_manager.enqueue(payload)
+
+        time.sleep(random.random())
+        return None
+
+    @expose.GET
+    def example_endpoint(self):
+        return {"message": "Hello World"}
+
+
+class SampleSingleQueueService(SingleQueueService):
+
+    def custom_startup(self):
+        self.queue_manager = QueueManager(QueueType.MEMORY)
+        self.queue_manager.enqueue({"count": 0})
+
+    def calculate(self, queue_item: QueueItem):
+        # Custom calculation logic for this service
+
+        self.log.info(f"count = {queue_item.payload['count']}")
+        payload = queue_item.payload
+        payload["count"] += 1
+        self.queue_manager.enqueue(payload)
+
+        time.sleep(random.random())
+        return None
+
+    @expose.GET
+    def example_endpoint(self):
+        return {"message": "Hello World"}
