c9d7a80b149040292924abd8e569b1e4615ad04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:05:14 +0000

    Add package varsim/1.0.8/varsim-1.0.8.tar.gz

---
+++ packages/varsim/varsim-1.0.8.tar.gz/varsim-1.0.8/src/varsim/__init__.py
@@ -0,0 +1,25 @@
+from ._core import (
+    cds,
+    utr5,
+    utr3,
+    splicing,
+    inframe_del,
+    inframe_dup,
+    frameshift_del,
+    frameshift_dup,
+    aa_sub,
+    missense,
+)
+
+__all__ = [
+    "cds",
+    "utr5",
+    "utr3",
+    "splicing",
+    "inframe_del",
+    "inframe_dup",
+    "frameshift_del",
+    "frameshift_dup",
+    "aa_sub",
+    "missense",
+]
