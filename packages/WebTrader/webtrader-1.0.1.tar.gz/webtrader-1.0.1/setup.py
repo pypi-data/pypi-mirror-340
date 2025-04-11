7942459a0ab44da2bca4047e167055327f8c7d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:02 +0000

    Add package WebTrader/1.0.1/webtrader-1.0.1.tar.gz

---
+++ packages/WebTrader/webtrader-1.0.1.tar.gz/webtrader-1.0.1/setup.py
@@ -0,0 +1,13 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="WebTrader",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requiress=["pyperclip", "requests"],
+    entry_points={
+         "console_scripts": [
+              "webtrader = WebTrader.py:webtrader" 
+        ]
+    }
+)
