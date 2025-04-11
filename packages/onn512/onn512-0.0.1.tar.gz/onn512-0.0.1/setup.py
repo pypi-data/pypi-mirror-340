d316e92b029873e4a28f85da344524b503352fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 14:04:43 +0000

    Add package onn512/0.0.1/onn512-0.0.1.tar.gz

---
+++ packages/onn512/onn512-0.0.1.tar.gz/onn512-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='onn512',
+    version='0.0.1',
+    author='onn512',
+    description='Name reservation 😎',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
