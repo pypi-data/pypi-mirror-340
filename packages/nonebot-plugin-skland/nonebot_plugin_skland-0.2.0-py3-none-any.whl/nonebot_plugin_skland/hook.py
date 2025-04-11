e20b13f9b7405d178622981f46803d54661b88da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 15:04:46 +0000

    Add package nonebot-plugin-skland/0.2.0/nonebot_plugin_skland-0.2.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-skland/nonebot_plugin_skland-0.2.0-py3-none-any.whl/nonebot_plugin_skland/hook.py
@@ -0,0 +1,28 @@
+from nonebot import logger, get_driver
+
+from .config import config
+from .exception import RequestException
+from .download import GameResourceDownloader
+
+driver = get_driver()
+
+RESOURCE_ROUTES = ["portrait", "avatar", "skill"]
+
+
+@driver.on_startup
+async def startup():
+    if config.check_res_update:
+        try:
+            if version := await GameResourceDownloader.check_update():
+                logger.info("开始下载游戏资源")
+                for route in RESOURCE_ROUTES:
+                    logger.info(f"正在下载: {route}")
+                    await GameResourceDownloader.download_all(
+                        owner="yuanyan3060", repo="ArknightsGameResource", route=route, branch="main"
+                    )
+        except RequestException as e:
+            logger.error(f"下载游戏资源失败: {e}")
+            return
+        if version:
+            GameResourceDownloader.update_version_file(version)
+            logger.success(f"游戏资源已更新到版本：{version}")
