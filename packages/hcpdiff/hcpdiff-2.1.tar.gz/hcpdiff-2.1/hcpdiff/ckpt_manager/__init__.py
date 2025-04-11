d9bd924de46de55465b69cd54e1c365f93c1f077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 15:04:31 +0000

    Add package hcpdiff/2.1/hcpdiff-2.1.tar.gz

---
+++ packages/hcpdiff/hcpdiff-2.1.tar.gz/hcpdiff-2.1/hcpdiff/ckpt_manager/__init__.py
@@ -0,0 +1,4 @@
+from .format import EmbFormat, DiffusersSD15Format, DiffusersModelFormat, DiffusersSDXLFormat, DiffusersPixArtFormat, OfficialSDXLFormat, \
+    OfficialSD15Format
+from .ckpt import EmbSaver, easy_emb_saver
+from .loader import HCPLoraLoader
