ff7746ebfa7b8da48ff1d62a7c34c686fca949b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:05:13 +0000

    Add package WebTrader/1.0.6/webtrader-1.0.6.tar.gz

---
+++ packages/WebTrader/webtrader-1.0.6.tar.gz/webtrader-1.0.6/setup.py
@@ -0,0 +1,29 @@
+﻿from setuptools import setup, find_packages
+
+with open("readme.md", "r", encoding="utf-8") as f:
+    description=f.read()
+setup(
+    name="WebTrader",
+    version="1.0.6",
+    packages=find_packages(),
+    install_requiress=["pyperclip", "requests"],
+    entry_points={
+         "console_scripts": [
+              "webtrader = WebTrader:webtrader" 
+        ]
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author="NecmeddinHD",
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Topic :: Communications :: Chat",
+    "Intended Audience :: Developers",
+    "Development Status :: 4 - Beta",
+    "Framework :: AsyncIO",
+    "Environment :: Console",
+],
+keywords="telegram bot share link device transfer",
+)
