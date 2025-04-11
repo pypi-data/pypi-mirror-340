1da1fede8db7da30b2f4bcc871dc68d0485469f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 03:18:26 +0000

    Add package django-cache-otp/0.1.7/django_cache_otp-0.1.7.tar.gz

---
+++ packages/django-cache-otp/django_cache_otp-0.1.7.tar.gz/django_cache_otp-0.1.7/django_cache_otp/otp.py
@@ -0,0 +1,23 @@
+from django.core.cache import cache
+
+from django_cache_otp.modules import generate_random_otp, encrypt_otp, decrypt_otp, check_cache
+
+
+def generate_otp(username: str, otp_length: int = 6, timeout: int = 60) -> int:
+    check_cache()
+    otp = generate_random_otp(otp_length)
+    encrypted_otp = encrypt_otp(otp)
+    cache.set(username, encrypted_otp, timeout=timeout)
+    return otp
+
+
+def validate_otp(username: str, otp: int) -> bool:
+    encrypted_otp = cache.get(username)
+    if not encrypted_otp:
+        return False
+    decrypted_otp = decrypt_otp(encrypted_otp)
+    if decrypted_otp != otp:
+        return False
+    else:
+        cache.delete(username)
+        return True
