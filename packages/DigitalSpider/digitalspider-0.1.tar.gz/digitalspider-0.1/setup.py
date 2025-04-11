4b28684ed93b982a198d2b9488b1df2951511411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 05:07:03 +0000

    Add package DigitalSpider/0.1/digitalspider-0.1.tar.gz

---
+++ packages/DigitalSpider/digitalspider-0.1.tar.gz/digitalspider-0.1/setup.py
@@ -0,0 +1,10 @@
+from spider import main
+
+from setuptools import setup, find_packages
+
+setup(
+    name='DigitalSpider',
+    version='0.1',
+    packages=find_packages(),
+    # ajoute d'autres arguments nécessaires ici
+)
