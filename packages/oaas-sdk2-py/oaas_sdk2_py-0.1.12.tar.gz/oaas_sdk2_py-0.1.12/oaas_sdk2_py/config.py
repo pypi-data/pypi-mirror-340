35556679b58d923e09eb2866d6860c2c695e9e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 08:04:17 +0000

    Add package oaas-sdk2-py/0.1.12/oaas_sdk2_py-0.1.12.tar.gz

---
+++ packages/oaas-sdk2-py/oaas_sdk2_py-0.1.12.tar.gz/oaas_sdk2_py-0.1.12/oaas_sdk2_py/config.py
@@ -0,0 +1,8 @@
+from pydantic import Field, HttpUrl
+from pydantic_settings import BaseSettings
+
+
+class OprcConfig(BaseSettings):
+    oprc_odgm_url: HttpUrl = Field(default="http://localhost:10000")
+    oprc_zenoh_peers: str|None = Field(default=None)
+    oprc_partition_default: int = Field(default=0)
