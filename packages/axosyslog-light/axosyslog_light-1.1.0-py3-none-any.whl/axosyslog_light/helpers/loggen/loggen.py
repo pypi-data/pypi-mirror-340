6bb3fe6b04e7346226776c056b4ea1bc857ace3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 17:09:51 +0000

    Add package axosyslog-light/1.1.0/axosyslog_light-1.1.0-py3-none-any.whl

---
+++ packages/axosyslog-light/axosyslog_light-1.1.0-py3-none-any.whl/axosyslog_light/helpers/loggen/loggen.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#############################################################################
+# Copyright (c) 2020 One Identity
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As an additional exemption you are allowed to compile & link against the
+# OpenSSL libraries as published by the OpenSSL project. See the file
+# COPYING for details.
+#
+#############################################################################
+import typing
+from pathlib import Path
+
+from axosyslog_light.common.session_data import get_session_data
+from axosyslog_light.helpers.loggen.loggen_executor import LoggenExecutor
+from axosyslog_light.helpers.loggen.loggen_executor import LoggenStartParams
+from psutil import Popen
+
+
+class Loggen(object):
+    @staticmethod
+    def __get_new_instance_index():
+        with get_session_data() as session_data:
+            last_index = session_data.get("loggen_instance_index", 0)
+            index = last_index + 1
+            session_data["loggen_instance_index"] = index
+        return index
+
+    def __init__(self, loggen_executor: typing.Optional[LoggenExecutor] = None):
+        if loggen_executor is None:
+            loggen_executor = LoggenExecutor.get_default_executor()
+
+        if not loggen_executor:
+            raise Exception("Loggen executor is not available")
+
+        self.executor = loggen_executor
+
+    def start(self, start_params: LoggenStartParams) -> Popen:
+        instance_index = Loggen.__get_new_instance_index()
+        self.loggen_stdout_path = Path("loggen_stdout_{}".format(instance_index))
+        self.loggen_stderr_path = Path("loggen_stderr_{}".format(instance_index))
+        return self.executor.start(start_params, self.loggen_stderr_path, self.loggen_stdout_path, instance_index)
+
+    def stop(self) -> None:
+        self.executor.stop()
+
+    def get_sent_message_count(self) -> int:
+        if not self.loggen_stderr_path.exists():
+            return 0
+
+        # loggen puts the count= messages to the stderr
+        f = open(self.loggen_stderr_path, "r")
+        content = f.read()
+        f.close()
+
+        start_pattern = "count="
+        if start_pattern not in content:
+            return 0
+        index_start = content.rindex(start_pattern) + len(start_pattern)
+
+        end_pattern = ", "
+        if end_pattern not in content:
+            return 0
+        index_end = content.find(end_pattern, index_start)
+
+        return int(content[index_start:index_end])
