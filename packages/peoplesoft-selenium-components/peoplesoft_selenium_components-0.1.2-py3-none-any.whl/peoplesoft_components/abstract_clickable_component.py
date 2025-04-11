ff01effef7f885db44867abbeb357aaa77cf0324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 04:04:05 +0000

    Add package peoplesoft-selenium-components/0.1.2/peoplesoft_selenium_components-0.1.2-py3-none-any.whl

---
+++ packages/peoplesoft-selenium-components/peoplesoft_selenium_components-0.1.2-py3-none-any.whl/peoplesoft_components/abstract_clickable_component.py
@@ -0,0 +1,14 @@
+from .abstract_input_component import AbstractInputComponent
+
+
+class AbstractClickableComponent(AbstractInputComponent):
+    def click(self):
+        self.root_element.click()
+
+    def click_with_spinner(self):
+        self.click()
+        self._wait_for_spinner()
+
+    def click_with_save_spinner(self):
+        self.click()
+        self._wait_for_saved_spinner()
---
+++ packages/peoplesoft-selenium-components/peoplesoft_selenium_components-0.1.2-py3-none-any.whl/peoplesoft_pages/base_fluid_page.py
@@ -0,0 +1,12 @@
+from peoplesoft_components import NavBar
+from .base_page import BasePage
+
+
+class BaseFluidPage(BasePage):
+    @property
+    def page_title(self) -> str:
+        ...
+
+    @property
+    def nav_bar(self):
+        return NavBar(self.driver)
