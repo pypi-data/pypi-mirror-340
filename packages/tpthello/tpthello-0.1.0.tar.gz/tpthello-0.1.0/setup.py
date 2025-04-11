523a74b4cd35118d1e813e7284959cf514263915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 15:04:26 +0000

    Add package tpthello/0.1.0/tpthello-0.1.0.tar.gz

---
+++ packages/tpthello/tpthello-0.1.0.tar.gz/tpthello-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tpthello',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        #add dependencies here.
+        # e.g. 'numpy>=1.11.1
+    ],
+)
