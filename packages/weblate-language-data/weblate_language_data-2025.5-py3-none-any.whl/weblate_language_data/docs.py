4b0752f27a68dc1636f5a0bf83e34cf7763ba0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 16:04:50 +0000

    Add package weblate-language-data/2025.5/weblate_language_data-2025.5-py3-none-any.whl

---
+++ packages/weblate-language-data/weblate_language_data-2025.5-py3-none-any.whl/weblate_language_data/docs.py
@@ -0,0 +1,25 @@
+# Copyright © Michal Čihař <michal@weblate.org>
+#
+# SPDX-License-Identifier: MIT
+
+# List of Weblate documentation languages
+DOCUMENTATION_LANGUAGES = {
+    "en": "en",
+    "zh-hans": "zh_CN",
+    "zh-hant": "zh_TW",
+    "pt-br": "pt_BR",
+    "uk": "uk",
+    "ru": "ru",
+    "es": "es",
+    "pt": "pt",
+    "nb": "no",
+    "ja": "ja",
+    "fr": "fr",
+    "pl": "pl",
+    "sq": "sq",
+    "tr": "tr",
+    "ro": "ro",
+    "de": "de",
+    "nl": "nl",
+    "ga": "ga",
+}
