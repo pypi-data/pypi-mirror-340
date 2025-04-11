b6c08b7528782c8978b288bc374f5bb324d0de5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 06:05:13 +0000

    Add package eth-wake/4.16.1/eth_wake-4.16.1.tar.gz

---
+++ packages/eth-wake/eth_wake-4.16.1.tar.gz/eth_wake-4.16.1/wake/core/enums.py
@@ -0,0 +1,18 @@
+from wake.utils import StrEnum
+
+
+class EvmVersionEnum(StrEnum):
+    HOMESTEAD = "homestead"
+    TANGERINE_WHISTLE = "tangerineWhistle"
+    SPURIOUS_DRAGON = "spuriousDragon"
+    BYZANTIUM = "byzantium"
+    CONSTANTINOPLE = "constantinople"
+    PETERSBURG = "petersburg"
+    ISTANBUL = "istanbul"
+    BERLIN = "berlin"
+    LONDON = "london"
+    PARIS = "paris"
+    SHANGHAI = "shanghai"
+    CANCUN = "cancun"
+    PRAGUE = "prague"
+    OSAKA = "osaka"
