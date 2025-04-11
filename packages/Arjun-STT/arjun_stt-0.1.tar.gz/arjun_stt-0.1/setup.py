e16dd426f942fde94de75e8c11f6d56e5cff9ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 14:04:51 +0000

    Add package Arjun-STT/0.1/arjun_stt-0.1.tar.gz

---
+++ packages/Arjun-STT/arjun_stt-0.1.tar.gz/arjun_stt-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Arjun_STT",
+    version="0.1",
+    author="Arjun Manish",
+    author_email="arjunmanish1234@gmail.com",
+    description="This is a speech-to-text package created by Arjun Manish",
+    
+    packages=find_packages(include=['Arjun_STT', 'Arjun_STT.*']),
+    
+    install_requires=[  # ✅ corrected spelling from install_requirement
+        'selenium',
+        'webdriver_manager'
+    ],
+    
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
