4817802e08cb41118b20ed2afb616f8921b0f52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 14:04:35 +0000

    Add package rf-humab/0.0.10/rf_humab-0.0.10.tar.gz

---
+++ packages/rf-humab/rf_humab-0.0.10.tar.gz/rf_humab-0.0.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.10'
+DESCRIPTION = 'Humanization tool for IgG heavy chain sequences'
+LONG_DESCRIPTION = 'Humanization tool for IgG heavy chain sequences based on the paper at https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8760955/.'
+
+# Setting up
+setup(
+        name="rf_humab", 
+        version=VERSION,
+        author="Lydia Stone",
+        author_email="<lstone@g.hmc.edu>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+        'numpy>=1.21.0',
+        'pandas>=1.3.0',
+        'scikit-learn>=1.0.0',
+        ],
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        include_package_data=True,
+        package_data={'rf_humab': ['sequence_data/*.csv', 'saved_models/*.sav']},
+)
