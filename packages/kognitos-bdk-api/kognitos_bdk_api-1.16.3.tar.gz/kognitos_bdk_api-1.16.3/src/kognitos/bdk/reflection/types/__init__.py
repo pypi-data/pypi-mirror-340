72685c329bc94c295f4a92e61bc7e412ccb3dd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 14:04:41 +0000

    Add package kognitos-bdk-api/1.16.3/kognitos_bdk_api-1.16.3.tar.gz

---
+++ packages/kognitos-bdk-api/kognitos_bdk_api-1.16.3.tar.gz/kognitos_bdk_api-1.16.3/src/kognitos/bdk/reflection/types/__init__.py
@@ -0,0 +1,12 @@
+from .any import ConceptAnyType
+from .base import ConceptType
+from .credential import CredentialScalarType, CredentialType
+from .dict import ConceptDictionaryType, ConceptDictionaryTypeField
+from .list import ConceptListType
+from .opaque import ConceptOpaqueType
+from .optional import ConceptOptionalType
+from .scalar import ConceptScalarType
+from .self import ConceptSelfType
+from .sensitive import ConceptSensitiveType
+from .table import ConceptTableType, ConceptTableTypeColumn
+from .union import ConceptUnionType
