86c640b99b25ed62f2d952f65f50a7cb7bf93f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 03:17:20 +0000

    Add package biomimetic/0.4.6/biomimetic-0.4.6.tar.gz

---
+++ packages/biomimetic/biomimetic-0.4.6.tar.gz/biomimetic-0.4.6/README.md
@@ -0,0 +1,5 @@
+# Biomimetic
+
+## A Python library for biomimetic cell models in neural networks
+
+Refer to the three papers: [Bio-mimetic approach in digital artificial neuro-science](https://ieeexplore.ieee.org/document/7560120) [Biomimetic Cells: A New Frontier in Brain Informatics](https://ieeexplore.ieee.org/abstract/document/8603044) [Advanced Biomimetic Cells Architecture for Parallel Operations in Artificial Intelligence](https://ieeexplore.ieee.org/abstract/document/8851116)
---
+++ packages/biomimetic/biomimetic-0.4.6.tar.gz/biomimetic-0.4.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='biomimetic',
+    version='0.4.6',
+    author='Sari Itani and Dr. Ziad Doughan',
+    author_email='sariitani101@gmail.com',
+    description='A Python library for biomimetic cell models in neural networks.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    keywords='biomimetic neural networks machine learning',
+    install_requires=[line.strip() for line in open("requirements.txt", "r")],
+    python_requires='>=3.6',
+    package_data={
+        'biomimetic': ['data/*.data'],
+    },
+    include_package_data=True,
+    zip_safe=False
+)
