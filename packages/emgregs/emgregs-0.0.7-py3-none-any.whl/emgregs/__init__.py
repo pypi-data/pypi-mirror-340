4cc2befd98ca16ef928811f76f63190176f535ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 04:04:51 +0000

    Add package emgregs/0.0.7/emgregs-0.0.7-py3-none-any.whl

---
+++ packages/emgregs/emgregs-0.0.7-py3-none-any.whl/emgregs/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.7"
+
+from emgregs.emg_reg import emg_reg, emg_reg_heterosked
+from emgregs.flare_reg import flare_reg
+from emgregs.simulation import sim_emg_reg, sim_emg_reg_heterosked, sim_flare_reg
