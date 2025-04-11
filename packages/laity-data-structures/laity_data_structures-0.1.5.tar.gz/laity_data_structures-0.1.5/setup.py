bfcc817c6cf7f7fdb9340b74000d4931f8c9ae55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 08:04:18 +0000

    Add package laity-data-structures/0.1.5/laity_data_structures-0.1.5.tar.gz

---
+++ packages/laity-data-structures/laity_data_structures-0.1.5.tar.gz/laity_data_structures-0.1.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages  # type: ignore
+
+
+setup(
+    name="laity_data_structures",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[],
+    description="Data Structure Package By Pappalaity",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Pappa laity",
+    author_email="pappalaity@gmail.com",
+    url="https://github.com/PappaLaity/pappa-laity-data-structures-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
