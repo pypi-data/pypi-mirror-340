605aba6ea559b8de808c04f63b26ae2bf3c47ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-11 04:04:00 +0000

    Add package draive/0.57.0/draive-0.57.0-py3-none-any.whl

---
+++ packages/draive/draive-0.57.0-py3-none-any.whl/draive/generation/__init__.py
@@ -0,0 +1,21 @@
+from draive.generation.image import ImageGeneration, ImageGenerator, generate_image
+from draive.generation.model import (
+    ModelGeneration,
+    ModelGenerator,
+    ModelGeneratorDecoder,
+    generate_model,
+)
+from draive.generation.text import TextGeneration, TextGenerator, generate_text
+
+__all__ = (
+    "ImageGeneration",
+    "ImageGenerator",
+    "ModelGeneration",
+    "ModelGenerator",
+    "ModelGeneratorDecoder",
+    "TextGeneration",
+    "TextGenerator",
+    "generate_image",
+    "generate_model",
+    "generate_text",
+)
---
+++ packages/draive/draive-0.57.0-py3-none-any.whl/draive/generation/text/types.py
@@ -0,0 +1,22 @@
+from collections.abc import Iterable
+from typing import Any, Protocol, runtime_checkable
+
+from draive.instructions import Instruction
+from draive.multimodal import Multimodal
+from draive.prompts import Prompt
+from draive.tools import Tool, Toolbox
+
+__all__ = ("TextGenerator",)
+
+
+@runtime_checkable
+class TextGenerator(Protocol):
+    async def __call__(
+        self,
+        *,
+        instruction: Instruction | str | None,
+        input: Prompt | Multimodal,  # noqa: A002
+        tools: Toolbox | Iterable[Tool] | None,
+        examples: Iterable[tuple[Multimodal, str]] | None,
+        **extra: Any,
+    ) -> str: ...
