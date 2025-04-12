c8bcdacd94df30ea85cba4fbb1f893b74ef8af0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:41 +0000

    Add package so-file-system/0.1.1/so_file_system-0.1.1.tar.gz

---
+++ packages/so-file-system/so_file_system-0.1.1.tar.gz/so_file_system-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="so-file-system",
+    version="0.1.1",
+    author="TuNombre",
+    author_email="tunombre@correo.com",
+    description="Un sistema de archivos versionado con Copy-on-Write en Python.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TuUsuario/so-file-system",  # Opcional, tu repo
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "tqdm",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
