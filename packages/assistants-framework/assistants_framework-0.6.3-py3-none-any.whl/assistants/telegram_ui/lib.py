2efb8bde2601936e2bf4ff12555a315fd3db32ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 20:05:05 +0000

    Add package assistants-framework/0.6.3/assistants_framework-0.6.3-py3-none-any.whl

---
+++ packages/assistants-framework/assistants_framework-0.6.3-py3-none-any.whl/assistants/telegram_ui/lib.py
@@ -0,0 +1,30 @@
+from functools import wraps
+
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from assistants.ai.openai import Assistant
+from assistants.config import environment
+
+
+def requires_reply_to_message(f):
+    @wraps(f)
+    async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        try:
+            return await f(update, context)
+        except AttributeError:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="You must reply to a message from the target user to use this command",
+            )
+
+    return wrapper
+
+
+assistant = Assistant(
+    name=environment.ASSISTANT_NAME,
+    model=environment.DEFAULT_MODEL,
+    instructions=environment.ASSISTANT_INSTRUCTIONS,
+    tools=[{"type": "code_interpreter"}],
+    api_key=environment.OPENAI_API_KEY,
+)
