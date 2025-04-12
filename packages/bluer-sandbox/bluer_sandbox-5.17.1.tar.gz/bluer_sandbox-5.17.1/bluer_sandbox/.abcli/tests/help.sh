4fb77aa25e040578c5ef26ca6d77bd269e58f85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 06:05:22 +0000

    Add package bluer-sandbox/5.17.1/bluer_sandbox-5.17.1.tar.gz

---
+++ packages/bluer-sandbox/bluer_sandbox-5.17.1.tar.gz/bluer_sandbox-5.17.1/bluer_sandbox/.abcli/tests/help.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+
+function test_bluer_sandbox_help() {
+    local options=$1
+
+    local module
+    for module in \
+        \
+        "@notebooks" \
+        "@notebooks open" \
+        "@notebooks build" \
+        "@notebooks code" \
+        "@notebooks connect" \
+        "@notebooks create" \
+        "@notebooks host" \
+        \
+        "@sandbox" \
+        \
+        "@sandbox pypi" \
+        "@sandbox pypi browse" \
+        "@sandbox pypi build" \
+        "@sandbox pypi install" \
+        \
+        "@sandbox pytest" \
+        \
+        "@sandbox test" \
+        "@sandbox test list" \
+        \
+        "@sandbox browse" \
+        \
+        "bluer_sandbox"; do
+        bluer_ai_eval ,$options \
+            bluer_ai_help $module
+        [[ $? -ne 0 ]] && return 1
+
+        bluer_ai_hr
+    done
+
+    return 0
+}
