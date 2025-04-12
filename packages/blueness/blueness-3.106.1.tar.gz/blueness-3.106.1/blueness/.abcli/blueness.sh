1b589bdaaafbc8b92e839e2560fbf787ee672488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:04:53 +0000

    Add package blueness/3.106.1/blueness-3.106.1.tar.gz

---
+++ packages/blueness/blueness-3.106.1.tar.gz/blueness-3.106.1/blueness/.abcli/blueness.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+function blueness() {
+    abcli_blueness "$@"
+}
+
+function abcli_blueness() {
+    local task=$1
+
+    if [ $task == "upgrade" ]; then
+        local module
+        for module in blueness \
+            blue_options \
+            blue_objects \
+            blue_geo \
+            blueflow \
+            abcli; do
+            pip install --upgrade $module
+        done
+        return
+    fi
+
+    abcli_generic_task \
+        plugin=blueness,task=$task \
+        "${@:2}"
+}
