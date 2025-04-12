540b6d014f75152e3ba5c793fd8c9ece3e47838b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 06:05:25 +0000

    Add package picovoice/3.0.4/picovoice-3.0.4.tar.gz

---
+++ packages/picovoice/picovoice-3.0.4.tar.gz/picovoice-3.0.4/setup.py
@@ -0,0 +1,51 @@
+#
+# Copyright 2020-2023 Picovoice Inc.
+#
+# You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+# file accompanying this source.
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+
+import os
+import shutil
+
+import setuptools
+
+os.system('git clean -dfx')
+
+package_folder = os.path.join(os.path.dirname(__file__), 'picovoice')
+os.mkdir(package_folder)
+
+shutil.copy(os.path.join(os.path.dirname(__file__), '../../LICENSE'), package_folder)
+shutil.copy(os.path.join(os.path.dirname(__file__), '__init__.py'), os.path.join(package_folder, '__init__.py'))
+shutil.copy(os.path.join(os.path.dirname(__file__), '_picovoice.py'), os.path.join(package_folder, '_picovoice.py'))
+
+with open(os.path.join(os.path.dirname(__file__), 'MANIFEST.in'), 'w') as f:
+    f.write('include picovoice/LICENSE\n')
+    f.write('include picovoice/__init__.py\n')
+    f.write('include picovoice/_picovoice.py\n')
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="picovoice",
+    version="3.0.4",
+    author="Picovoice Inc.",
+    author_email="hello@picovoice.ai",
+    description="⚠️ DEPRECATED: This package is no longer maintained. Use better-package instead.",
+    long_description="This package has been deprecated. Please use better-package.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Picovoice/picovoice",
+    packages=["picovoice"],
+    install_requires=["pvporcupine==3.0.3", "pvrhino==3.0.3"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 7 - Inactive"
+    ],
+    python_requires='>=3.8',
+    keywords="wake word, voice control, speech recognition, voice recognition, natural language understanding",
+)
