8b39d3a4ff951996dcb46bc141410031b814f167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:05:23 +0000

    Add package wagtail-enap-designsystem/1.1.3/wagtail_enap_designsystem-1.1.3.tar.gz

---
+++ packages/wagtail-enap-designsystem/wagtail_enap_designsystem-1.1.3.tar.gz/wagtail_enap_designsystem-1.1.3/enap_designsystem/templates/enap_designsystem/templatetags/breadcrumbs_tags.py
@@ -0,0 +1,42 @@
+# enap_designsystem/templatetags/breadcrumbs_tags.py
+from django import template
+from wagtail.models import Page
+
+register = template.Library()
+
+@register.inclusion_tag('enap_designsystem/blocks/breadcrumbs.html', takes_context=True)
+def breadcrumbs(context, theme='light'):
+    """
+    Renderiza as breadcrumbs para a página atual.
+    """
+    page = context.get('page')
+    if not page:
+        return {'breadcrumbs': [], 'theme': theme}
+    
+    breadcrumbs = []
+    # Adiciona a página inicial
+    home_page = Page.objects.filter(depth=2).first()
+    
+    if home_page:
+        breadcrumbs.append({
+            'title': 'Home',
+            'url': home_page.url,
+            'is_home': True
+        })
+    
+    # Adiciona as páginas ancestrais
+    for ancestor in page.get_ancestors()[1:]:  # Ignora a raiz do site
+        breadcrumbs.append({
+            'title': ancestor.specific.title,
+            'url': ancestor.specific.url,
+            'is_home': False
+        })
+    
+    # Adiciona a página atual
+    breadcrumbs.append({
+        'title': page.title,
+        'url': None,
+        'is_current': True
+    })
+    
+    return {'breadcrumbs': breadcrumbs, 'theme': theme}
