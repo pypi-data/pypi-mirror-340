dca22edd222c970f826eb1d6791729d043993e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:05:09 +0000

    Add package fundi/0.0.8/fundi-0.0.8-py3-none-any.whl

---
+++ packages/fundi/fundi-0.0.8-py3-none-any.whl/fundi/from_.pyi
@@ -0,0 +1,16 @@
+import typing
+from typing import overload
+
+T = typing.TypeVar("T", bound=type)
+R = typing.TypeVar("R")
+
+@overload
+def from_(dependency: T) -> T: ...
+@overload
+def from_(dependency: typing.Callable[..., typing.Generator[R, None, None]]) -> R: ...
+@overload
+def from_(dependency: typing.Callable[..., typing.AsyncGenerator[R, None]]) -> R: ...
+@overload
+def from_(dependency: typing.Callable[..., typing.Awaitable[R]]) -> R: ...
+@overload
+def from_(dependency: typing.Callable[..., R]) -> R: ...
