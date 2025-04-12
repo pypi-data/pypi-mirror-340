ff3a1db5cc9bf4d9dd7ceb518f90424b8c8ffa52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:05:11 +0000

    Add package ifireflylib/0.0.3/ifireflylib-0.0.3.tar.gz

---
+++ packages/ifireflylib/ifireflylib-0.0.3.tar.gz/ifireflylib-0.0.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+# Try to read the README.md file from the correct location
+try:
+    with open("README.md", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    # Fallback to a simple description if README.md is not found
+    long_description = "A client package for FireFlyDB"
+
+setup(
+    name="ifireflylib",
+    version="0.0.3",
+    description="A client package for FireFlyDB",
+    author="IDSolutions",
+    package_dir={"": "."},
+    packages=find_packages(where="ifireflylib"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitea.innovativedevsolutions.org/IDSolutions/firefly",
+    # license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires=">=3.13",
+    package_data={
+        'ifireflylib': [
+            'native/libFireflyClient.dll',
+            'native/libFireflyClient.so',
+            'native/libFireflyClient.dylib',
+            'ifireflylib/client',
+            'ifireflylib/api',
+            'examples',
+            'tests'
+        ]
+    },
+    include_package_data=True,
+)
