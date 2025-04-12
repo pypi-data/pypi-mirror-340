57904688a929bc9dc2a05396138ddb2576d52463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:54 +0000

    Add package broken-source/0.9.0.dev0/broken_source-0.9.0.dev0-py3-none-any.whl

---
+++ packages/broken-source/broken_source-0.9.0.dev0-py3-none-any.whl/Broken/Version.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Manual trigger count: 1
+__version__ = "0.9.0.dev0"
+__options__ = dict(
+    GHA_VERSION=__version__,
+)
+
+# Export options to environment
+if (__name__ == "__main__"):
+    for (key, value) in __options__.items():
+        if isinstance(value, bool):
+            value = int(value)
+        print(f"{key}={value}")
