00fe96ae892e64bff8ff5a236fbbe8a272b15c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:05:09 +0000

    Add package atopile/0.4.0.dev0/atopile-0.4.0.dev0-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/atopile/atopile-0.4.0.dev0-cp313-cp313-macosx_11_0_arm64.whl/faebryk/library/I2C.py
@@ -0,0 +1,48 @@
+# This file is part of the faebryk project
+# SPDX-License-Identifier: MIT
+import logging
+from enum import Enum
+
+import faebryk.library._F as F
+from faebryk.core.module import Module
+from faebryk.core.moduleinterface import ModuleInterface
+from faebryk.libs.library import L
+from faebryk.libs.units import P
+
+logger = logging.getLogger(__name__)
+
+
+class I2C(ModuleInterface):
+    scl: F.ElectricLogic
+    sda: F.ElectricLogic
+
+    frequency = L.p_field(
+        units=P.Hz,
+        likely_constrained=True,
+        soft_set=L.Range(10 * P.kHz, 3.4 * P.MHz),
+    )
+
+    @L.rt_field
+    def single_electric_reference(self):
+        return F.has_single_electric_reference_defined(
+            F.ElectricLogic.connect_all_module_references(self)
+        )
+
+    def terminate(self, owner: Module):
+        # TODO: https://www.ti.com/lit/an/slva689/slva689.pdf
+
+        self.pull_up_sda = self.sda.pulled.pull(up=True, owner=owner)
+        self.pull_up_scl = self.scl.pulled.pull(up=True, owner=owner)
+
+    class SpeedMode(Enum):
+        low_speed = 10 * P.khertz
+        standard_speed = 100 * P.khertz
+        fast_speed = 400 * P.khertz
+        high_speed = 3.4 * P.Mhertz
+
+    @staticmethod
+    def define_max_frequency_capability(mode: SpeedMode):
+        return L.Range(I2C.SpeedMode.low_speed.value, mode.value)
+
+    def __preinit__(self) -> None:
+        self.frequency.add(F.is_bus_parameter())
