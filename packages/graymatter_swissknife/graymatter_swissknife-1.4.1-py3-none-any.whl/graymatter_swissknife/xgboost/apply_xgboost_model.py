170b63e841d0e0b041fd9162d2cdf850243f4d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 06:04:57 +0000

    Add package graymatter_swissknife/1.4.1/graymatter_swissknife-1.4.1-py3-none-any.whl

---
+++ packages/graymatter_swissknife/graymatter_swissknife-1.4.1-py3-none-any.whl/graymatter_swissknife/xgboost/apply_xgboost_model.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def apply_xgboost_model(xgboost_model, signal, microstruct_model):
+    
+    # Application of the model on the signals
+    y_hat = xgboost_model.predict(signal)
+    y_hat = np.clip(y_hat, 0, 1)
+    n_params = microstruct_model.n_params if not microstruct_model.has_noise_correction else microstruct_model.n_params - 1
+    for param_index in range(n_params):
+        y_hat[:, param_index] = (
+            y_hat[:, param_index]
+            * (microstruct_model.param_lim[param_index][1] - microstruct_model.param_lim[param_index][0])
+            + microstruct_model.param_lim[param_index][0]
+        )
+    
+    return y_hat
