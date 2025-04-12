8cb4d096cea40a99a501d3a06fdef9e0d4aedd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 15:05:04 +0000

    Add package bluer-objects/6.66.1/bluer_objects-6.66.1-py3-none-any.whl

---
+++ packages/bluer-objects/bluer_objects-6.66.1-py3-none-any.whl/bluer_objects/.abcli/mlflow/browse.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+function bluer_objects_mlflow_browse() {
+    local options=$1
+    local browse_experiment=$(abcli_option_int "$options" experiment 0)
+
+    local url=$DATABRICKS_HOST/$ABCLI_MLFLOW_URL_SUBDOMAIN
+
+    if [ $(abcli_option_int "$options" databricks 0) == 1 ]; then
+        url="https://accounts.cloud.databricks.com/"
+    elif [ $(abcli_option_int "$options" host 0) == 1 ]; then
+        : # do nothing
+    elif [ $(abcli_option_int "$options" models 0) == 1 ]; then
+        url="$url/models"
+    else
+        local object_name=$(bluer_ai_clarify_object $2 .)
+
+        local experiment_id=$(bluer_objects_mlflow get_id $object_name)
+        if [ -z "$experiment_id" ]; then
+            bluer_ai_log_error "@mlflow: browse: $object_name: object not found."
+            return 1
+        fi
+        bluer_ai_log "experiment id: $experiment_id"
+
+        url="$url/experiments/$experiment_id"
+
+        if [[ "$browse_experiment" == 0 ]]; then
+            local last_run_id=$(bluer_objects_mlflow get_run_id $object_name --count 1)
+            bluer_ai_log "last run id: $last_run_id"
+
+            url="$url/runs/$last_run_id"
+        fi
+    fi
+
+    bluer_ai_browse $url
+}
