09629a7cad9036fa2634c9e932cd49fc342c5ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:05:07 +0000

    Add package lite-bootstrap/0.10.3/lite_bootstrap-0.10.3.tar.gz

---
+++ packages/lite-bootstrap/lite_bootstrap-0.10.3.tar.gz/lite_bootstrap-0.10.3/tests/test_free_bootstrap.py
@@ -0,0 +1,38 @@
+import pytest
+import structlog
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+
+from lite_bootstrap import FreeBootstrapper, FreeBootstrapperConfig
+from tests.conftest import CustomInstrumentor
+
+
+logger = structlog.getLogger(__name__)
+
+
+def test_free_bootstrap() -> None:
+    bootstrapper = FreeBootstrapper(
+        bootstrap_config=FreeBootstrapperConfig(
+            service_debug=False,
+            opentelemetry_endpoint="otl",
+            opentelemetry_instrumentors=[CustomInstrumentor()],
+            opentelemetry_span_exporter=ConsoleSpanExporter(),
+            sentry_dsn="https://testdsn@localhost/1",
+            logging_buffer_capacity=0,
+        ),
+    )
+    bootstrapper.bootstrap()
+    try:
+        logger.info("testing logging", key="value")
+    finally:
+        bootstrapper.teardown()
+
+
+def test_free_bootstrap_logging_not_ready() -> None:
+    with pytest.warns(UserWarning, match="service_debug is True or structlog is not installed"):
+        FreeBootstrapper(
+            bootstrap_config=FreeBootstrapperConfig(
+                service_debug=True,
+                opentelemetry_endpoint="otl",
+                sentry_dsn="https://testdsn@localhost/1",
+            ),
+        )
