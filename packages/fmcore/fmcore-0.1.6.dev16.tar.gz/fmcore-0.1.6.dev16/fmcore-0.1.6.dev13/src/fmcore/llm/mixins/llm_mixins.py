6d4f961ffd419c5b2f4e8987f00f2441767fe30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:05:34 +0000

    Add package fmcore/0.1.6.dev16/fmcore-0.1.6.dev16.tar.gz

---
+++ packages/fmcore/fmcore-0.1.6.dev16.tar.gz/fmcore-0.1.6.dev16/fmcore-0.1.6.dev13/src/fmcore/llm/mixins/llm_mixins.py
@@ -0,0 +1,16 @@
+from typing import Optional, Union
+
+from fmcore.llm.types.llm_types import LLMConfig, DistributedLLMConfig
+from fmcore.types.mixins_types import Mixin
+from fmcore.types.typed import MutableTyped
+
+
+class LLMConfigMixin(MutableTyped, Mixin):
+    """
+    Mixin for LLM configuration.
+
+    Attributes:
+        llm_config (Optional[LLMConfig]): The LLM configuration object.
+    """
+
+    llm_config: Union[LLMConfig, DistributedLLMConfig]
