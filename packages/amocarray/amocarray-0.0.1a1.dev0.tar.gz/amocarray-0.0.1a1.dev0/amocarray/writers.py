b8c03cf8e4d28cd6644addd0ebe7122083181094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 06:05:00 +0000

    Add package amocarray/0.0.1a1.dev0/amocarray-0.0.1a1.dev0.tar.gz

---
+++ packages/amocarray/amocarray-0.0.1a1.dev0.tar.gz/amocarray-0.0.1a1.dev0/amocarray/writers.py
@@ -0,0 +1,43 @@
+import numpy as np
+import xarray as xr
+from numbers import Number
+
+def save_dataset(ds, output_file='../test.nc'):
+    """
+    Attempts to save the dataset to a NetCDF file. If a TypeError occurs due to invalid attribute values,
+    it converts the invalid attributes to strings and retries the save operation.
+
+    Parameters
+    ----------
+    ds (xarray.Dataset): The dataset to be saved.
+    output_file (str): The path to the output NetCDF file. Defaults to 'test.nc'.
+
+    Returns
+    -------
+    bool: True if the dataset was saved successfully, False otherwise.
+
+    Based on: https://github.com/pydata/xarray/issues/3743
+    """
+    valid_types = (str, int, float, np.float32, np.float64, np.int32, np.int64)
+    # More general
+    valid_types = (str, Number, np.ndarray, np.number, list, tuple)
+    try:
+        ds.to_netcdf(output_file, format='NETCDF4_CLASSIC')
+        return True
+    except TypeError as e:
+        print(e.__class__.__name__, e)
+        for varname, variable in ds.variables.items():
+            for k, v in variable.attrs.items():
+                if not isinstance(v, valid_types) or isinstance(v, bool):
+                    print(f"variable '{varname}': Converting attribute '{k}' with value '{v}' to string.")
+                    variable.attrs[k] = str(v)
+        try:
+            ds.to_netcdf(output_file, format='NETCDF4_CLASSIC')
+            return True
+        except Exception as e:
+            print("Failed to save dataset:", e)
+            datetime_vars = [var for var in ds.variables if ds[var].dtype == 'datetime64[ns]']
+            print("Variables with dtype datetime64[ns]:", datetime_vars)
+            float_attrs = [attr for attr in ds.attrs if isinstance(ds.attrs[attr], float)]
+            print("Attributes with dtype float64:", float_attrs)
+            return False
---
+++ packages/amocarray/amocarray-0.0.1a1.dev0.tar.gz/amocarray-0.0.1a1.dev0/docs/source/installation.md
@@ -0,0 +1,19 @@
+# Installation
+
+Since the project in this template isn't really designed to be installed, instead these instructions describe how to get started: first, with a copy (clone) of this repository, and second (after renaming the repository to suit your purposes), how to work with the new repository in a collaborative way (i.e., enabling other people to contribute to your repository).
+
+## Option 1: Copy this repository as a template for your project
+To copy the repository for your own purposes (e.g., to rename "template-project" to your project) rather than to suggest edits to this template, follow these steps:
+
+1. Clone template-project to your local machine: 
+    With a command line usage of git, this looks like:
+    ```
+    git clone https://github.com/AMOCcommunity/amocarray
+    ```
+
+    Alternatively, in your web browser, navigate to https://github.com/AMOCcommunity/amocarray and click the green button "<> Code" for a dropdown menu of options.  Choose, e.g. "Open with GitHub Desktop".  This requires the application "GitHub Desktop" (https://github.com/apps/desktop).  See instructions [here](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository).
+2. Change to the parent directory of template-project
+
+## Option 2: Edit this repository to improve the template
+
+The steps are as above, but instead of saying that you'd like to work on this separately, choose the option to "contribute to the original repository".  In command line, this will be like setting the upstream to be the original repository at https://github.com/AMOCcommunity/amocarray.
