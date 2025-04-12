3b27f892c38791d25f5cdc7bdc56155d1c8616aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 18:05:18 +0000

    Add package personal-library-manager/0.1.0/personal_library_manager-0.1.0.tar.gz

---
+++ packages/personal-library-manager/personal_library_manager-0.1.0.tar.gz/personal_library_manager-0.1.0/README.md
@@ -0,0 +1,19 @@
+# Personal Library Manager
+
+A simple CLI tool written in Python to manage your personal book collection.
+
+## Features
+
+- Add a new book
+- Remove a book
+- Search for a book by title or author
+- Display all books in a formatted list
+- Display statistics (total books and percentage read)
+- Save library data to a file and load it on startup
+
+## Installation
+
+You can install this package via pip (after publishing on PyPI):
+
+```bash
+pip install personal-library-manager
---
+++ packages/personal-library-manager/personal_library_manager-0.1.0.tar.gz/personal_library_manager-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="personal-library-manager",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # List external dependencies if any
+    entry_points={
+        'console_scripts': [
+            'library-manager = personal_library_manager.cli:main_menu',
+        ],
+    },
+    author="Muhammad waheed",
+    author_email="wm0297567@gmail.com",
+    description="A CLI app to manage your personal book collection",
+    long_description=open('README.md', encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/muhammadwaheedairi/personal-library-manager.git",  # Change to your repo URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
