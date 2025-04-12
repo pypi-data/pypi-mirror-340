600ec82f2ecf7780bc4d6859cc4ee19696726ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 05:05:10 +0000

    Add package SimpleWindow/0.21/simplewindow-0.21.tar.gz

---
+++ packages/SimpleWindow/simplewindow-0.21.tar.gz/simplewindow-0.21/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SimpleWindow",
+    version="0.21",
+    description="A package to easily create windows in Python using GLFW, OpenCV-Python, NumPy, pywin32 and ctypes.",
+    long_description=open("README.md").read(),
+    author="OleFranz",
+    license="GPL-3.0",
+    packages=["SimpleWindow"],
+    python_requires=">=3.9",
+    install_requires=[
+        "glfw",
+        "opencv-python",
+        "numpy",
+        "pywin32",
+    ],
+)
