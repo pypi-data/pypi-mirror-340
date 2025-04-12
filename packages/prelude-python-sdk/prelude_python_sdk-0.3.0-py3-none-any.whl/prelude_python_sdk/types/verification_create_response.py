e3417b28729a9209bcf035b7c356ef5dbd947cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:05:00 +0000

    Add package prelude-python-sdk/0.3.0/prelude_python_sdk-0.3.0-py3-none-any.whl

---
+++ packages/prelude-python-sdk/prelude_python_sdk-0.3.0-py3-none-any.whl/prelude_python_sdk/types/verification_create_response.py
@@ -0,0 +1,28 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["VerificationCreateResponse", "Metadata"]
+
+
+class Metadata(BaseModel):
+    correlation_id: Optional[str] = None
+
+
+class VerificationCreateResponse(BaseModel):
+    id: str
+    """The verification identifier."""
+
+    method: Literal["message"]
+    """The method used for verifying this phone number."""
+
+    status: Literal["success", "retry", "blocked"]
+    """The status of the verification."""
+
+    metadata: Optional[Metadata] = None
+    """The metadata for this verification."""
+
+    request_id: Optional[str] = None
