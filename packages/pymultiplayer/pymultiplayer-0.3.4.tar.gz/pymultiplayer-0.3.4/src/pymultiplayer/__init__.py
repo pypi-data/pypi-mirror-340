fff8442235d20f7f998d1407bbaac77799d54fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 05:05:12 +0000

    Add package pymultiplayer/0.3.4/pymultiplayer-0.3.4.tar.gz

---
+++ packages/pymultiplayer/pymultiplayer-0.3.4.tar.gz/pymultiplayer-0.3.4/src/pymultiplayer/__init__.py
@@ -0,0 +1,6 @@
+from .TCPserver import TCPMultiplayerServer
+from .client import MultiplayerClient
+from .server_manager import ServerManager, get_servers, create_server
+print("pymultiplayer by iamdeedz")
+print("https://www.github.com/iamdeedz/pymultiplayer")
+print("This library is currently a work in progress.")
