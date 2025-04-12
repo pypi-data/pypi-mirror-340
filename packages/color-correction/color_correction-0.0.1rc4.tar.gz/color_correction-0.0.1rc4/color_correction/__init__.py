553a1e8a9d3de36642ff80a97fb5509f024e744d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 20:05:39 +0000

    Add package color-correction/0.0.1rc4/color_correction-0.0.1rc4.tar.gz

---
+++ packages/color-correction/color_correction-0.0.1rc4.tar.gz/color_correction-0.0.1rc4/color_correction/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.0.1-rc4"
+
+# fmt: off
+from .constant.color_checker import reference_color_d50_bgr as REFERENCE_COLOR_D50_BGR  # noqa: N812, I001
+from .constant.color_checker import reference_color_d50_rgb as REFERENCE_COLOR_D50_RGB  # noqa: N812, I001
+from .core.card_detection.det_yv8_onnx import YOLOv8CardDetector
+from .schemas.det_yv8 import DetectionResult as YOLOv8DetectionResult
+from .services.color_correction import ColorCorrection
+from .services.correction_analyzer import ColorCorrectionAnalyzer
+# fmt: on
+
+__all__ = [
+    "__version__",
+    "REFERENCE_COLOR_D50_BGR",
+    "REFERENCE_COLOR_D50_RGB",
+    "ColorCorrection",
+    "ColorCorrectionAnalyzer",
+    "YOLOv8CardDetector",
+    "YOLOv8DetectionResult",
+]
