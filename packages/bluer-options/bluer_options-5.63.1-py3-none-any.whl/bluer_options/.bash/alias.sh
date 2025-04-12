0ff80f8873267b46d6c49acbd8fff6dc98279dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 15:05:14 +0000

    Add package bluer-options/5.63.1/bluer_options-5.63.1-py3-none-any.whl

---
+++ packages/bluer-options/bluer_options-5.63.1-py3-none-any.whl/bluer_options/.bash/alias.sh
@@ -0,0 +1,57 @@
+#! /usr/bin/env bash
+
+alias @assert=bluer_ai_assert
+
+alias @badge=bluer_ai_badge
+
+alias @browse=bluer_ai_browse
+
+alias @code=bluer_ai_code
+
+alias @env=bluer_ai_env
+
+alias @eval=bluer_ai_eval
+
+alias @help=bluer_ai_help
+
+alias @hr=bluer_ai_hr
+
+alias @cat=bluer_ai_cat
+
+alias @list=bluer_ai_list
+
+alias @log=bluer_ai_log
+alias @error=bluer_ai_log_error
+alias @warn=bluer_ai_log_warning
+
+alias @log::list=bluer_ai_log_list
+
+alias @not=bluer_ai_not
+
+alias @open=bluer_ai_open
+
+alias @option=abcli_option
+alias @option::int=abcli_option_int
+alias @option::choice=abcli_option_choice
+
+alias @options=bluer_options
+
+alias @pause=bluer_ai_pause
+
+alias @pylint=bluer_ai_pylint
+alias @pytest=bluer_ai_pytest
+
+alias @repeat=bluer_ai_repeat
+
+alias @sleep=bluer_ai_sleep
+
+alias @test=bluer_ai_test
+
+alias @timestamp=bluer_ai_string_timestamp
+alias @@timestamp=bluer_ai_string_timestamp_short
+
+alias @today=bluer_ai_string_today
+
+alias @wait=bluer_ai_wait
+
+alias @watch=bluer_ai_watch
---
+++ packages/bluer-options/bluer_options-5.63.1-py3-none-any.whl/bluer_options/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+NAME = "bluer_options"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} options for Bash."
+
+VERSION = "5.63.1"
+
+REPO_NAME = "bluer-options"
+
+MARQUEE = "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
