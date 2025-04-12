29915cc08d2e8336d154da7018de1db941c1fe01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 15:05:21 +0000

    Add package bluer-sandbox/5.25.1/bluer_sandbox-5.25.1-py3-none-any.whl

---
+++ packages/bluer-sandbox/bluer_sandbox-5.25.1-py3-none-any.whl/bluer_sandbox/.abcli/browse.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+function bluer_sandbox_browse() {
+    local options=$1
+    local what=$(abcli_option_choice "$options" actions,repo repo)
+
+    local url="https://github.com/kamangir/bluer-sandbox"
+    [[ "$what" == "actions" ]] &&
+        url="$url/actions"
+
+    bluer_ai_browse $url
+}
+
