e18cfb784eee2eded1a0a63568af57bb050487a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:35 +0000

    Add package echosms/0.10.0/echosms-0.10.0-py3-none-any.whl

---
+++ packages/echosms/echosms-0.10.0-py3-none-any.whl/echosms/__init__.py
@@ -0,0 +1,29 @@
+"""Setup the public API for echoSMs."""
+from .utils import wavenumber, wavelength, Neumann, h1, prolate_swf, spherical_jnpp
+from .utils import pro_rad1, pro_rad2, pro_ang1
+from .utils import as_dataframe, as_dataarray, as_dict, split_dict, theoretical_Sa
+from .dwbautils import create_dwba_spheroid, create_dwba_cylinder, create_dwba_from_xyza, DWBAorganism, DWBAdata
+from .scattermodelbase import ScatterModelBase
+from .benchmarkdata import BenchmarkData
+from .jechetaldata import JechEtAlData
+from .referencemodels import ReferenceModels
+from .esmodel import ESModel
+from .mssmodel import MSSModel
+from .psmsmodel import PSMSModel
+from .dcmmodel import DCMModel
+from .ptdwbamodel import PTDWBAModel
+from .dwbamodel import DWBAModel
+from .kamodel import KAModel
+from .krmmodel import KRMModel
+from .hpmodel import HPModel
+from .krmdata import KRMdata, KRMorganism, KRMshape
+
+__all__ = ['ScatterModelBase', 'BenchmarkData', 'ReferenceModels',
+           'MSSModel', 'PSMSModel', 'DCMModel', 'ESModel', 'PTDWBAModel',
+           'DWBAModel', 'SDWBAModel', 'KAModel', 'KRMModel', 'HPModel',
+           'wavenumber', 'wavelength', 'Neumann', 'h1', 'spherical_jnpp', 'prolate_swf',
+           'theoretical_Sa', 'KRMdata', 'KRMorganism', 'KRMshape',
+           'DWBAorganism', 'DWBAdata', 'JechEtAlData',
+           'pro_rad1', 'pro_rad2', 'pro_ang1',
+           'as_dataframe', 'as_dataarray', 'as_dict', 'split_dict',
+           'create_dwba_spheroid', 'create_dwba_cylinder', 'create_dwba_from_xyza']
