999a7f350c0234c6b4f5838702fe1d8905b0d08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:20 +0000

    Add package regressionmadesimple/1.4.0/regressionmadesimple-1.4.0.tar.gz

---
+++ packages/regressionmadesimple/regressionmadesimple-1.4.0.tar.gz/regressionmadesimple-1.4.0/regressionmadesimple/__init__.py
@@ -0,0 +1,22 @@
+from .linear import Linear
+from .utils_preworks import Preworks
+from .options import options, save_options, load_options, reset_options
+from .wrapper import LinearRegressionModel
+from .quadratic import Quadratic
+from .cubic import Cubic
+from .curves import CustomCurve
+
+__version__ = "1.3.0"
+
+__all__ = [
+    "Linear",
+    "Preworks",
+    "options",
+    "save_options",
+    "load_options",
+    "reset_options",
+    "LinearRegressionModel",
+    "Quadratic",
+    "Cubic",
+    "CustomCurve",
+]
