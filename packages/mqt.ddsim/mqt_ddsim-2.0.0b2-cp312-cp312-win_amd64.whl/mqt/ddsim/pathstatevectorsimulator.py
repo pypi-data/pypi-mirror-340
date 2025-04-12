5c107b649198a0d70a6392fd3bfb5899aff2fe6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:54 +0000

    Add package mqt.ddsim/2.0.0b2/mqt_ddsim-2.0.0b2-cp312-cp312-win_amd64.whl

---
+++ packages/mqt.ddsim/mqt_ddsim-2.0.0b2-cp312-cp312-win_amd64.whl/mqt/ddsim/pathstatevectorsimulator.py
@@ -0,0 +1,29 @@
+"""Backend for DDSIM."""
+
+from __future__ import annotations
+
+from qiskit.transpiler import Target
+
+from .pathqasmsimulator import PathQasmSimulatorBackend
+
+
+class PathStatevectorSimulatorBackend(PathQasmSimulatorBackend):
+    """Python interface to MQT DDSIM Simulation Path Framework."""
+
+    _SHOW_STATE_VECTOR = True
+    _Path_SV_TARGET = Target(
+        description="MQT DDSIM Simulation Path Framework Statevector Target",
+        num_qubits=30,  # corresponds to 16GiB memory for storing the full statevector
+    )
+
+    def __init__(self) -> None:
+        """Constructor for the DDSIM Simulation Path Framework Statevector Simulator backend."""
+        super().__init__(
+            name="path_sim_statevector_simulator",
+            description="MQT DDSIM Simulation Path Framework Statevector Simulator",
+        )
+
+    @property
+    def target(self) -> Target:
+        """Return the target of the backend."""
+        return self._Path_SV_TARGET
