5798bf1adbc14396bb5a7fc1ec5fe4d014a8808f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:48 +0000

    Add package tracebloc-package/0.6.14/tracebloc_package-0.6.14.tar.gz

---
+++ packages/tracebloc-package/tracebloc_package-0.6.14.tar.gz/tracebloc_package-0.6.14/setup.py
@@ -0,0 +1,51 @@
+import sys
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+# package_name = "tracebloc_package_dev"
+# package_url = "https://gitlab.com/tracebloc/tracebloc-py-package/-/tree/dev"
+# package_author_email = "info@tracebloc.io"
+
+# if "--live" in sys.argv:
+package_name = "tracebloc_package"
+package_url = "https://gitlab.com/tracebloc/tracebloc-py-package"
+package_author_email = "lukas@tracebloc.io"
+
+setup(
+    name=package_name,
+    version="0.6.14",
+    description="Package required to run Tracebloc jupyter notebook to create experiment",
+    url=package_url,
+    license="MIT",
+    python_requires=">=3",
+    packages=find_packages(),
+    author="Tracebloc",
+    author_email=package_author_email,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "absl-py",
+        "dill",
+        "protobuf",
+        "requests",
+        "rich",
+        "silence-tensorflow",
+        "tensorflow",
+        "tensorflow-datasets",
+        "termcolor",
+        "timm",
+        "torch",
+        "torchlightning",
+        "torchmetrics",
+        "torchvision",
+        "tqdm",
+        "transformers",
+        "twine",
+    ],
+    zip_safe=False,
+)
