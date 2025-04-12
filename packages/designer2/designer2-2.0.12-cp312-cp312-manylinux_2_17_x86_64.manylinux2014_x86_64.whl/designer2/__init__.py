be6676409a5f0c16837089dc785f7078732efa56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 05:05:12 +0000

    Add package designer2/2.0.12/designer2-2.0.12-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/designer2/designer2-2.0.12-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/designer2/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "2.0.12"
+
+import lib.tensor as tensor
+import lib.mpcomplex  as mpcomplex
+import lib.smi as smi
+from lib.mpunits import vectorize
+from lib.designer_fit_wrappers import save_params
+import lib.rpg as rpg
