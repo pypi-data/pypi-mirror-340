efb87dfdc40ac5e838f49c34e3e9a31e9f1fb329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:04:55 +0000

    Add package MSG-Nbody/0.2.1/msg_nbody-0.2.1.tar.gz

---
+++ packages/MSG-Nbody/msg_nbody-0.2.1.tar.gz/msg_nbody-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MSG_Nbody',
+    version='0.2.1',
+    description='Nbody simulation code for galaxy interactions',
+    url='https://github.com/elkogerville/MSG_Nbody',
+    author='Elko Gerville-Reache',
+    author_email='elkogerville@gmail.com',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'numba',
+        'tqdm',
+        'matplotlib',
+        'scipy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
