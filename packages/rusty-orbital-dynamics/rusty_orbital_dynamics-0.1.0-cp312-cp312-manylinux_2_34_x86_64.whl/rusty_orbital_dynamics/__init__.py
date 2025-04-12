4577b1354a3b4c8a3c49cca332c2fab268999c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:05:20 +0000

    Add package rusty-orbital-dynamics/0.1.0/rusty_orbital_dynamics-0.1.0-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/rusty-orbital-dynamics/rusty_orbital_dynamics-0.1.0-cp312-cp312-manylinux_2_34_x86_64.whl/rusty_orbital_dynamics/__init__.py
@@ -0,0 +1,4 @@
+# rusty_orbital_dynamics/__init__.py
+
+from .rusty_orbital_dynamics import *  # <- This is the compiled Rust extension
+from .helpers import *                 # <- Optional pure Python additions
