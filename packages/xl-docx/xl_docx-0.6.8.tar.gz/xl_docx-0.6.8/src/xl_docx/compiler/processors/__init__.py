efdb6548c655763c44d35d34c349d59cce50d047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 20:04:59 +0000

    Add package xl-docx/0.6.8/xl_docx-0.6.8.tar.gz

---
+++ packages/xl-docx/xl_docx-0.6.8.tar.gz/xl_docx-0.6.8/src/xl_docx/compiler/processors/__init__.py
@@ -0,0 +1,6 @@
+from xl_word.compiler.processors.base import BaseProcessor
+from xl_word.compiler.processors.style import StyleProcessor
+from xl_word.compiler.processors.directive import DirectiveProcessor
+from xl_word.compiler.processors.table import TableProcessor
+from xl_word.compiler.processors.paragraph import ParagraphProcessor
+from xl_word.compiler.processors.pager import PagerProcessor
