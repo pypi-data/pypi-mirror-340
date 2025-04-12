2fb7f1eaa9d671887137ea6e633b77e9b286960d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 20:05:29 +0000

    Add package tutorial-text2pdf-style/0.1.1/tutorial-text2pdf-style-0.1.1.tar.gz

---
+++ packages/tutorial-text2pdf-style/tutorial-text2pdf-style-0.1.1.tar.gz/tutorial-text2pdf-style-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tutorial-text2pdf-style',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'fpdf',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tutorial-txt2pdf-style = tutorial_text2pdf_style.__init__:main',
+        ],
+    },
+    author='Prakash Babu Adhikari',
+    author_email='your_email@example.com',
+    description='Convert cleanly formatted .txt documentation into color-styled PDF files.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/adhikaripb/coding-scripting-tutorial-pdf-generator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
