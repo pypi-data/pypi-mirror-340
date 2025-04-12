047788280fa498b80e25409b9b079bdf6beee197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:04:56 +0000

    Add package undate/0.4.0/undate-0.4.0-py3-none-any.whl

---
+++ packages/undate/undate-0.4.0-py3-none-any.whl/undate/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.4.0"
+
+from undate.date import DatePrecision
+from undate.undate import Undate, Calendar
+from undate.interval import UndateInterval
+
+__all__ = ["Undate", "UndateInterval", "Calendar", "DatePrecision", "__version__"]
