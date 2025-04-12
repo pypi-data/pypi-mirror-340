73aec800ce039906cdc42502225dc508998e34c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 16:04:50 +0000

    Add package zcc-helper/3.4.dev2/zcc_helper-3.4.dev2.tar.gz

---
+++ packages/zcc-helper/zcc_helper-3.4.dev2.tar.gz/zcc_helper-3.4.dev2/zcc/__init__.py
@@ -0,0 +1,10 @@
+'''ZCC Support Library to support ZIMI based home network elements'''
+import logging
+import sys
+
+from .controller import ControlPoint
+from .description import ControlPointDescription
+from .discovery import ControlPointDiscoveryService
+from .errors import ControlPointError
+
+logging.basicConfig(stream=sys.stderr)
