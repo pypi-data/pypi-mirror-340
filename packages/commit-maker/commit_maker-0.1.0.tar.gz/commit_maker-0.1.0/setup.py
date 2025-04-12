629e012348e30d6cdd7fbf28f73e90e8977adc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 06:05:24 +0000

    Add package commit-maker/0.1.0/commit_maker-0.1.0.tar.gz

---
+++ packages/commit-maker/commit_maker-0.1.0.tar.gz/commit_maker-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="commit_maker",
+    version="0.1.0",
+    author="Alex Bulgakov",
+    author_email="sashayerty@ya.ru",
+    description="CLI-утилита для генерации git-коммитов с помощью ИИ",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.8",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+            "commit_maker=commit_maker.main:main",
+        ],
+    },
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
