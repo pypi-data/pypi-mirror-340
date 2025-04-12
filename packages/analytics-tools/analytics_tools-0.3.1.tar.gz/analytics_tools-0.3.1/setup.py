eededa1847b80773397385152c844628ae510303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:42 +0000

    Add package analytics-tools/0.3.1/analytics_tools-0.3.1.tar.gz

---
+++ packages/analytics-tools/analytics_tools-0.3.1.tar.gz/analytics_tools-0.3.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = 'analytics_tools',
+        version = '0.3.1',
+        description = 'A package to help data analysts at cotidian tasks, mainly EDA and ETL tasks',
+        long_description = open('README.md').read(),
+        long_description_content_type = 'text/markdown',
+        author = 'Arnaldo Joao Bastos Junior',
+        author_email = 'arnaldo.jbj@gmail.com',
+        packages = find_packages(),
+        install_requires = [
+                            'pandas',
+                            'numpy',
+                            'IPython',
+                           ],
+        classifiers = [
+                        'Programming Language :: Python :: 3',
+                        'License :: OSI Approved :: MIT License',
+                        'Operating System :: OS Independent',
+                      ],
+        python_requires = '>=3.6',
+)
