079802c7d70d68872258122498d4827c91aea6d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 05:04:47 +0000

    Add package CG23016UNO/0.1.5/cg23016uno-0.1.5-py3-none-any.whl

---
+++ packages/CG23016UNO/cg23016uno-0.1.5-py3-none-any.whl/CG23016UNO/no_lineales.py
@@ -0,0 +1,14 @@
+'''Metodo de Biseccion'''
+
+def bisection(f, a, b, tol=1e-10, max_iter=100):
+    if f(a) * f(b) > 0:
+        raise ValueError("La funcion no cambia de signo en el intervalo")
+    for _ in range(max_iter):
+        c = (a + b) / 2
+        if abs(f(c)) < tol or (b - a) / 2 < tol:
+            return c
+        if f(a) * f(c) < 0:
+            b = c
+        else:
+            a = c
+    return (a + b) / 2
