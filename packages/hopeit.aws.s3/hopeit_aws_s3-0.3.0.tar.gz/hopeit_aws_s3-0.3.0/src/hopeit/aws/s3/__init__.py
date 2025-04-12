80079a438f024ecea5fe47ea56b9ed572bb5e98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 05:05:12 +0000

    Add package hopeit.aws.s3/0.3.0/hopeit_aws_s3-0.3.0.tar.gz

---
+++ packages/hopeit.aws.s3/hopeit_aws_s3-0.3.0.tar.gz/hopeit_aws_s3-0.3.0/src/hopeit/aws/s3/__init__.py
@@ -0,0 +1,14 @@
+"""
+Hopeit AWS S3 ObjectStorage API
+"""
+
+__version__ = "0.3.0"
+
+from hopeit.aws.s3.object_storage import (
+    ConnectionConfig,
+    ItemLocator,
+    ObjectStorage,
+    ObjectStorageSettings,
+)
+
+__all__ = ["ConnectionConfig", "ItemLocator", "ObjectStorage", "ObjectStorageSettings"]
