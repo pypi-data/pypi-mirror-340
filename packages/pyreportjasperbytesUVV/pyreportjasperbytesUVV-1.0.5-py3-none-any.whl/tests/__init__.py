e3035dc1d0b4a56197ef78a99eb20f558366fbc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:05:33 +0000

    Add package pyreportjasperbytesUVV/1.0.5/pyreportjasperbytesUVV-1.0.5-py3-none-any.whl

---
+++ packages/pyreportjasperbytesUVV/pyreportjasperbytesUVV-1.0.5-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# GNU GENERAL PUBLIC LICENSE
+#
+# 2023 Jadson Bonfim Ribeiro <contato@jadsonbr.com.br>
+# Versão alterada 2024 Eduardo Soares <edufrancoreis@hotmail.com>
+#
