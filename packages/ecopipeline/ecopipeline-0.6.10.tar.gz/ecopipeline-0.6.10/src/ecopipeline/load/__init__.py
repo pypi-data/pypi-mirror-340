22a2266eca68c50b6847d6718a527273071268f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:05:11 +0000

    Add package ecopipeline/0.6.10/ecopipeline-0.6.10.tar.gz

---
+++ packages/ecopipeline/ecopipeline-0.6.10.tar.gz/ecopipeline-0.6.10/src/ecopipeline/load/__init__.py
@@ -0,0 +1,2 @@
+from .load import check_table_exists, create_new_table, load_overwrite_database, load_event_table, report_data_loss
+__all__ = ["check_table_exists", "create_new_table", "load_overwrite_database", "load_event_table", "report_data_loss"]
