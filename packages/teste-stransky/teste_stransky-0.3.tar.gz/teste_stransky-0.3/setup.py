fdec7d46af5a16d9439f6c7a354451f2705ac415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:05:22 +0000

    Add package teste-stransky/0.3/teste_stransky-0.3.tar.gz

---
+++ packages/teste-stransky/teste_stransky-0.3.tar.gz/teste_stransky-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name='teste_stransky',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[],
+    author='Marcelo Stransky',
+    author_email='marcelostransky@gmail.com',
+    description='Uma biblioteca para cálculos de investimentos. Simulador de investimentos para estudos',
+    url='https://github.com/marcelostransky/meu_investimento',
+    classifiers=['Programming Language :: Python :: 3','License :: OSI Approved :: MIT License','Operating System :: OS Independent',],
+    python_requires='>=3.6',
+    )
