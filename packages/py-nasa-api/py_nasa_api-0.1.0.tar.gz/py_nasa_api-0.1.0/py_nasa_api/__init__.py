2afff9799f3690c7687df1f512e310e0b662a435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:16:00 +0000

    Add package py-nasa-api/0.1.0/py_nasa_api-0.1.0.tar.gz

---
+++ packages/py-nasa-api/py_nasa_api-0.1.0.tar.gz/py_nasa_api-0.1.0/py_nasa_api/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+__description__ = "NASA API wrapper written in Python"
+__license__ = "MIT"
+__authors__ = ["Keenan Noack <AlbertUnruh@pm.me>"]
+__repository__ = "https://github.com/AlbertUnruh/py-nasa-api/"
+
+
+__all__ = ()
