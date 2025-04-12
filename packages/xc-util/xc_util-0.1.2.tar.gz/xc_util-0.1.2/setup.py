013f5cbdd87259bf5290e1b0f41e1a193d91fc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:05:14 +0000

    Add package xc-util/0.1.2/xc_util-0.1.2.tar.gz

---
+++ packages/xc-util/xc_util-0.1.2.tar.gz/xc_util-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xc-util",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography>=36.0.0",
+    ],
+    python_requires=">=3.7",
+)
