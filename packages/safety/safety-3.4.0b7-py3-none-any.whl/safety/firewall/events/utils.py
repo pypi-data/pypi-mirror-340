6d4b9564c97f18725140d3e68d2e904bb8012db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:04:56 +0000

    Add package safety/3.4.0b7/safety-3.4.0b7-py3-none-any.whl

---
+++ packages/safety/safety-3.4.0b7-py3-none-any.whl/safety/firewall/events/utils.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING
+
+from safety_schemas.models.events import EventType
+from safety.events.event_bus import EventBus
+from safety.events.types import InternalEventType
+
+from .handlers import HeartbeatInspectionEventHandler
+
+if TYPE_CHECKING:
+    from safety.models import SafetyCLI
+
+
+def register_event_handlers(event_bus: "EventBus", obj: "SafetyCLI") -> None:
+    """
+    Subscribes to the firewall events that are relevant to the current context.
+    """
+    handle_inspection = HeartbeatInspectionEventHandler(event_bus=event_bus)
+    event_bus.subscribe([InternalEventType.EVENT_BUS_READY], handle_inspection)
+
+    if sec_events_handler := obj.security_events_handler:
+        event_bus.subscribe(
+            [
+                EventType.FIREWALL_CONFIGURED,
+                EventType.FIREWALL_HEARTBEAT,
+                EventType.FIREWALL_DISABLED,
+                EventType.PACKAGE_INSTALLED,
+                EventType.PACKAGE_UNINSTALLED,
+                EventType.PACKAGE_UPDATED,
+                EventType.TOOL_COMMAND_EXECUTED,
+            ],
+            sec_events_handler,
+        )
