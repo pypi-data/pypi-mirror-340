3972c4f138058b86aa7ede87305be5ce9f321e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:04:52 +0000

    Add package blueness/3.104.1/blueness-3.104.1-py3-none-any.whl

---
+++ packages/blueness/blueness-3.104.1-py3-none-any.whl/blueness/__init__.py
@@ -0,0 +1,9 @@
+NAME = "blueness"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} the blueness of ai."
+
+VERSION = "3.104.1"
+
+REPO_NAME = "blueness"
---
+++ packages/blueness/blueness-3.104.1-py3-none-any.whl/blueness/pypi.py
@@ -0,0 +1,80 @@
+import os
+from setuptools import setup as setup_real
+
+from blueness import ICON, VERSION
+
+
+def find_file(path: str, filename: str) -> str:
+    safety_counter = 0
+    while True:
+        full_filename = os.path.join(path, filename)
+        if os.path.exists(full_filename):
+            return full_filename
+
+        path = os.path.dirname(path)
+
+        safety_counter += 1
+
+        if path == "/" or safety_counter > 20:
+            raise FileNotFoundError(filename)
+
+
+def get_long_description(
+    filename: str,
+    repo_name: str,
+) -> str:
+    filename = find_file(os.path.dirname(filename), "README.md")
+
+    with open(filename) as f:
+        output = f.read()
+
+    output = output.replace(
+        "./",
+        "https://github.com/kamangir/{}/blob/{}/".format(
+            repo_name,
+            "current" if repo_name == "awesome-bash-cli" else "main",
+        ),
+    )
+
+    if "```mermaid" in output:
+        slices = output.split("```mermaid", 1)
+
+        assert "```" in slices[1], "open mermaid found."
+        slices[1] = slices[1].split("```", 1)[1]
+        output = "".join(slices)
+
+    output = f"{output}\nbuilt by {ICON} [`blueness-{VERSION}`](https://github.com/kamangir/blueness)."
+
+    return output
+
+
+def get_requirements(filename: str) -> list:
+    filename = find_file(os.path.dirname(filename), "requirements.txt")
+
+    with open(filename) as f:
+        requirements = f.read().strip().split("\n")
+
+    return [item for item in requirements if "git+https:" not in item]
+
+
+def setup(
+    filename: str,
+    repo_name: str,
+    **kwargs,
+):
+    setup_real(
+        author="Arash Abadpour (Kamangir)",
+        author_email="arash@kamangir.net",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Unix Shell",
+            "License :: Public Domain",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=get_requirements(filename),
+        license="Public Domain",
+        long_description=get_long_description(filename, repo_name),
+        long_description_content_type="text/markdown",
+        url=f"https://github.com/kamangir/{repo_name}",
+        **kwargs,
+    )
