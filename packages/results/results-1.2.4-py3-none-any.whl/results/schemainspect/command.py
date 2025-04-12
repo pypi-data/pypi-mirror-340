285fd72e5144d60ff613947c8e07836811389a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:53 +0000

    Add package results/1.2.4/results-1.2.4-py3-none-any.whl

---
+++ packages/results/results-1.2.4-py3-none-any.whl/results/schemainspect/command.py
@@ -0,0 +1,65 @@
+import argparse
+
+from .get import get_inspector
+from .misc import quoted_identifier
+from .tableformat import t
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description="Inspect a schema")
+
+    subparsers = parser.add_subparsers(help="sub-command help", dest="command")
+
+    parser_deps = subparsers.add_parser("deps", help="Show inspected dependencies")
+    parser_deps.add_argument("db_url", help="URL")
+
+    parser_deps2 = subparsers.add_parser(
+        "yaml", help="Export schema definition as YAML"
+    )
+    parser_deps2.add_argument("db_url", help="URL")
+
+    return parser.parse_args(args)
+
+
+def do_deps(db_url):
+    with S(db_url) as s:
+        i = get_inspector(s)
+        deps = i.deps
+
+    def process_row(dep):
+        depends_on = quoted_identifier(dep.name, dep.schema, dep.identity_arguments)
+        thing = quoted_identifier(
+            dep.name_dependent_on,
+            dep.schema_dependent_on,
+            dep.identity_arguments_dependent_on,
+        )
+
+        return dict(
+            thing="{}: {}".format(dep.kind_dependent_on, thing),
+            depends_on="{}: {}".format(dep.kind, depends_on),
+        )
+
+    deps = [process_row(_) for _ in deps]
+
+    rows = t(deps)
+
+    if rows:
+        print(rows)
+    else:
+        print("No dependencies found.")
+
+
+def do_yaml(db_url):
+    with S(db_url) as s:
+        i = get_inspector(s)
+        defn = i.encodeable_definition()
+
+    from io import StringIO as sio
+
+    import yaml
+
+    x = sio()
+
+    yaml.safe_dump(defn, x)
+
+    print(x.getvalue())
