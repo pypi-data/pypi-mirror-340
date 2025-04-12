0da0c780ca044e1a5c34e64313aacf5c9e96ee1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:29 +0000

    Add package robot-soccer-kit/2.4.1/robot_soccer_kit-2.4.1.tar.gz

---
+++ packages/robot-soccer-kit/robot_soccer_kit-2.4.1.tar.gz/robot_soccer_kit-2.4.1/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+import os, sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def package_files(directory):
+    paths = []
+    for path, directories, filenames in os.walk(directory):
+        for filename in filenames:
+            if "__pycache__" not in path:
+                paths.append(os.path.join("..", path, filename))
+    return paths
+
+setuptools.setup(
+    name="robot_soccer_kit",
+    version="2.4.1",
+    author="Rhoban team",
+    author_email="team@rhoban.com",
+    description="Robot Soccer Kit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rhoban/robot-soccer-kit/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+    ],
+    keywords="robot holonomic omniwheel ssl sct robocup junior soccer standard localized tracking",
+    install_requires=["numpy<2", "pyzmq", "pyyaml", "astar"],
+    extras_require={
+        "gc": ["pyserial", "flask", "flask-cors", "waitress", 
+            "opencv-contrib-python<4.7;platform_system=='Windows'",
+            "opencv-contrib-python;platform_system=='Linux'"
+            ],  # Game controller extra requirements
+    },
+    include_package_data=True,
+    package_data={"": package_files("rsk")},
+    python_requires=">=3.6",
+)
