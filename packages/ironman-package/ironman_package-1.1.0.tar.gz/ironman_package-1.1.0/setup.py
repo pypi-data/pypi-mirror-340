87ebfeed1c4aa430945970fe88db1a7c76c785e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 15:05:04 +0000

    Add package ironman-package/1.1.0/ironman_package-1.1.0.tar.gz

---
+++ packages/ironman-package/ironman_package-1.1.0.tar.gz/ironman_package-1.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='ironman-package',
+    version='1.1.0',
+    author='Juan Ignacio Espinoza-Retamal',
+    author_email='jiespinozar@uc.cl',
+    description='Joint fit Rossiter-McLaughlin data with photometry and out-of-transit radial velocities',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jiespinozar/ironman',
+    packages=find_packages(),
+    install_requires=['numpy','scipy','pandas','batman-package','dynesty','astropy','rmfit>=1.0.2','tqdm'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Astronomy',
+    ],
+    keywords='Astronomy, Obliquities, Radial Velocities',
+    license='MIT',
+    include_package_data=True,
+    python_requires='>=3.6',
+)
