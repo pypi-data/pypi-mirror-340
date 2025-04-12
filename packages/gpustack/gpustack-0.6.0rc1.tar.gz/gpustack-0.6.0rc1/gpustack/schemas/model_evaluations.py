43d4bc5ca6efa0a1969aff76b15ae3d97899616a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 15:05:07 +0000

    Add package gpustack/0.6.0rc1/gpustack-0.6.0rc1.tar.gz

---
+++ packages/gpustack/gpustack-0.6.0rc1.tar.gz/gpustack-0.6.0rc1/gpustack/schemas/model_evaluations.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+from pydantic import BaseModel, ConfigDict
+
+from gpustack.schemas.model_sets import ModelSpec
+
+
+class ResourceClaim(BaseModel):
+    ram: int  # in bytes
+    vram: int  # in bytes
+
+
+class ModelEvaluationRequest(BaseModel):
+    model_specs: Optional[List[ModelSpec]] = None
+
+    model_config = ConfigDict(protected_namespaces=())
+
+
+class ModelEvaluationResult(BaseModel):
+    compatible: bool = True
+    compatibility_messages: Optional[List[str]] = []
+    scheduling_messages: Optional[List[str]] = []
+    default_spec: Optional[ModelSpec] = None
+    resource_claim: Optional[ResourceClaim] = None
+
+    model_config = ConfigDict(protected_namespaces=())
+
+
+class ModelEvaluationResponse(BaseModel):
+    results: List[ModelEvaluationResult] = []
+
+    model_config = ConfigDict(protected_namespaces=())
