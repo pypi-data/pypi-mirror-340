4ecd3d1fba92877786e5c1363fbbc4e4a846bab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:26 +0000

    Add package winzy-visual-notify/0.0.4/winzy_visual_notify-0.0.4.tar.gz

---
+++ packages/winzy-visual-notify/winzy_visual_notify-0.0.4.tar.gz/winzy_visual_notify-0.0.4/tests/test_winzy_visual_notify.py
@@ -0,0 +1,30 @@
+import pytest
+import winzy_visual_notify as w
+from winzy_visual_notify.pyside6_speech_bubble import get_character_choices
+
+from argparse import ArgumentParser
+
+
+def test_create_parser():
+    subparser = ArgumentParser().add_subparsers()
+    parser = w.create_parser(subparser)
+
+    assert parser is not None
+
+    result = parser.parse_args(["--text", "hello"])
+    assert result.text == ["hello"]
+    assert result.character == "random"
+    assert result.duration == 300
+
+
+def test_plugin(capsys):
+    w.tell_plugin.hello(None)
+    captured = capsys.readouterr()
+    assert "Hello! This is an example ``winzy`` plugin." in captured.out
+
+
+def test_get_character_choices():
+    choices = get_character_choices()
+    assert len(choices) == 9
+    assert "dog2" in choices
+    assert "watermellon" in choices
