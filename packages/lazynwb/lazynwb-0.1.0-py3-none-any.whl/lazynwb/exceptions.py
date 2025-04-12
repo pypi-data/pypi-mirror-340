84221e8ad96250c3049d2076703923c2c5f7c235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:05:06 +0000

    Add package lazynwb/0.1.0/lazynwb-0.1.0-py3-none-any.whl

---
+++ packages/lazynwb/lazynwb-0.1.0-py3-none-any.whl/lazynwb/exceptions.py
@@ -0,0 +1,9 @@
+
+class ColumnError(KeyError):
+    """Requested column name is not in table group"""
+    pass
+
+
+class InternalPathError(KeyError):
+    """Requested internal path is not in file"""
+    pass
