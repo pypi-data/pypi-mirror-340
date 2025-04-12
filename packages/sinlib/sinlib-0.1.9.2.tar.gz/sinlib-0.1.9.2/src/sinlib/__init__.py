f09f9010717458745bc86b6c6bd9fadc9220e701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 20:05:24 +0000

    Add package sinlib/0.1.9.2/sinlib-0.1.9.2.tar.gz

---
+++ packages/sinlib/sinlib-0.1.9.2.tar.gz/sinlib-0.1.9.2/src/sinlib/__init__.py
@@ -0,0 +1,30 @@
+"""
+Sinlib: A comprehensive library for Sinhala text processing.
+
+This library provides tools for tokenization, romanization, and transliteration
+of Sinhala text, along with various preprocessing utilities.
+
+Available Classes:
+    - Tokenizer: For tokenizing Sinhala text
+    - Romanizer: For converting Sinhala text to Roman characters
+    - Transliterator: For transliterating between scripts
+"""
+
+from os import path
+from typing import List
+
+from sinlib.romanize import Romanizer
+from sinlib.tokenizer import Tokenizer
+from sinlib.transliterate import Transliterator
+from sinlib.utils import preprocessing
+from sinlib.spellcheck import TypoDetector
+
+__all__: List[str] = [
+    "Tokenizer",
+    "preprocessing",
+    "Romanizer",
+    "Transliterator",
+    "TypoDetector"
+]
+
+__version__ = "0.1.9.2"
