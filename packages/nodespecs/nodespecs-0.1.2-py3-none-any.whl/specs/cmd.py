d796c00cbd9089bf48abdf44a1374d2294a0593d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:05:15 +0000

    Add package nodespecs/0.1.2/nodespecs-0.1.2-py3-none-any.whl

---
+++ packages/nodespecs/nodespecs-0.1.2-py3-none-any.whl/specs/cmd.py
@@ -0,0 +1,26 @@
+keymap = {
+
+    "DiskUsage" : { 
+        "Linux"   :  "df -H",
+        "macOS"   :  "df -H",
+        "windows" :  "Get-PSDrive -PSProvider 'FileSystem'"
+     },
+
+     "OSrelease" : {
+        "Linux"   :  "cat /etc/os-release",
+        "macOS"   :  "",
+        "windows" :  "systeminfo | findstr /B /C:'OS'"
+     },
+     "SysmArch" : {
+        "Linux"   :  "uname -m",
+        "macOS"   :  "",
+        "windows" :  "systeminfo | findstr /B /C:'System Type'"
+     },
+
+     "CPUinfo" : {
+        "Linux"   :  ["cat /proc/cpuinfo","lscpu"],
+        "macOS"   :  "",
+        "windows" :  ""
+     },
+}
+
