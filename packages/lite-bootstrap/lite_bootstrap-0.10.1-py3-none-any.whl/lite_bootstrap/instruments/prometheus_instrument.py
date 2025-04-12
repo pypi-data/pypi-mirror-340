2c34af788b5636e3a2b65d31522ef2e8aa3fbda9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:04:54 +0000

    Add package lite-bootstrap/0.10.1/lite_bootstrap-0.10.1-py3-none-any.whl

---
+++ packages/lite-bootstrap/lite_bootstrap-0.10.1-py3-none-any.whl/lite_bootstrap/instruments/prometheus_instrument.py
@@ -0,0 +1,29 @@
+import dataclasses
+import re
+import typing
+
+from lite_bootstrap.instruments.base import BaseConfig, BaseInstrument
+
+
+VALID_PATH_PATTERN: typing.Final = re.compile(r"^(/[a-zA-Z0-9_-]+)+/?$")
+
+
+def _is_valid_path(maybe_path: str) -> bool:
+    return bool(re.fullmatch(VALID_PATH_PATTERN, maybe_path))
+
+
+@dataclasses.dataclass(kw_only=True, frozen=True)
+class PrometheusConfig(BaseConfig):
+    prometheus_metrics_path: str = "/metrics"
+    prometheus_metrics_include_in_schema: bool = False
+
+
+@dataclasses.dataclass(kw_only=True, slots=True, frozen=True)
+class PrometheusInstrument(BaseInstrument):
+    bootstrap_config: PrometheusConfig
+    not_ready_message = "prometheus_metrics_path is empty or not valid"
+
+    def is_ready(self) -> bool:
+        return bool(self.bootstrap_config.prometheus_metrics_path) and _is_valid_path(
+            self.bootstrap_config.prometheus_metrics_path
+        )
