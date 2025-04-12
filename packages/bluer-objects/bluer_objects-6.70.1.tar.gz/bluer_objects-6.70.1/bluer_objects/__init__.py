9047935fb32e2520b3ed1a2d49ae229cc32ade08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 15:05:23 +0000

    Add package bluer-objects/6.70.1/bluer_objects-6.70.1.tar.gz

---
+++ packages/bluer-objects/bluer_objects-6.70.1.tar.gz/bluer_objects-6.70.1/bluer_objects/__init__.py
@@ -0,0 +1,19 @@
+NAME = "bluer_objects"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} data objects for Bash."
+
+VERSION = "6.70.1"
+
+REPO_NAME = "bluer-objects"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/raw/main/blue-objects/marquee.png?raw=true"
+)
+
+ALIAS = "@objects"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
