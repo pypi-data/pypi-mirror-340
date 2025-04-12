51aecf49fdcbb7263101c454c7e59169f2c19e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 04:05:41 +0000

    Add package fmu-sumo/2.3.9/fmu_sumo-2.3.9.tar.gz

---
+++ packages/fmu-sumo/fmu_sumo-2.3.9.tar.gz/fmu_sumo-2.3.9/src/fmu/sumo/explorer/objects/cases.py
@@ -0,0 +1,23 @@
+"""Module for searchcontext for collection of cases."""
+
+from ._search_context import SearchContext
+
+
+class Cases(SearchContext):
+    def __init__(self, sc, uuids):
+        super().__init__(sc._sumo, must=[{"ids": {"values": uuids}}])
+        self._hits = uuids
+        return
+
+    def _maybe_prefetch(self, index):
+        return
+
+    async def _maybe_prefetch_async(self, index):
+        return
+
+    def filter(self, **kwargs):
+        sc = SearchContext(
+            self._sumo, must=[{"terms": {"fmu.case.uuid.keyword": self._hits}}]
+        ).filter(**kwargs)
+        uuids = sc.get_field_values("fmu.case.uuid.keyword")
+        return Cases(sc, uuids)
