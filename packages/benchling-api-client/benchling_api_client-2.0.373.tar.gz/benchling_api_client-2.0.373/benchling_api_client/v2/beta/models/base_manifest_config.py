baac5e946ec4e553de3b16cb1f74ef31fe485022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 05:04:43 +0000

    Add package benchling-api-client/2.0.373/benchling_api_client-2.0.373.tar.gz

---
+++ packages/benchling-api-client/benchling_api_client-2.0.373.tar.gz/benchling_api_client-2.0.373/benchling_api_client/v2/beta/models/base_manifest_config.py
@@ -0,0 +1,115 @@
+from typing import Any, cast, Dict, List, Optional, Type, TypeVar, Union
+
+import attr
+
+from ..extensions import NotPresentError
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="BaseManifestConfig")
+
+
+@attr.s(auto_attribs=True, repr=False)
+class BaseManifestConfig:
+    """  """
+
+    _name: str
+    _description: Union[Unset, None, str] = UNSET
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+    def __repr__(self):
+        fields = []
+        fields.append("name={}".format(repr(self._name)))
+        fields.append("description={}".format(repr(self._description)))
+        fields.append("additional_properties={}".format(repr(self.additional_properties)))
+        return "BaseManifestConfig({})".format(", ".join(fields))
+
+    def to_dict(self) -> Dict[str, Any]:
+        name = self._name
+        description = self._description
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        # Allow the model to serialize even if it was created outside of the constructor, circumventing validation
+        if name is not UNSET:
+            field_dict["name"] = name
+        if description is not UNSET:
+            field_dict["description"] = description
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any], strict: bool = False) -> T:
+        d = src_dict.copy()
+
+        def get_name() -> str:
+            name = d.pop("name")
+            return name
+
+        try:
+            name = get_name()
+        except KeyError:
+            if strict:
+                raise
+            name = cast(str, UNSET)
+
+        def get_description() -> Union[Unset, None, str]:
+            description = d.pop("description")
+            return description
+
+        try:
+            description = get_description()
+        except KeyError:
+            if strict:
+                raise
+            description = cast(Union[Unset, None, str], UNSET)
+
+        base_manifest_config = cls(
+            name=name,
+            description=description,
+        )
+
+        base_manifest_config.additional_properties = d
+        return base_manifest_config
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
+
+    def get(self, key, default=None) -> Optional[Any]:
+        return self.additional_properties.get(key, default)
+
+    @property
+    def name(self) -> str:
+        if isinstance(self._name, Unset):
+            raise NotPresentError(self, "name")
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        self._name = value
+
+    @property
+    def description(self) -> Optional[str]:
+        if isinstance(self._description, Unset):
+            raise NotPresentError(self, "description")
+        return self._description
+
+    @description.setter
+    def description(self, value: Optional[str]) -> None:
+        self._description = value
+
+    @description.deleter
+    def description(self) -> None:
+        self._description = UNSET
