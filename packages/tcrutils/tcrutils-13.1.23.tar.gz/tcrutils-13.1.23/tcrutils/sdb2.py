5470cd2a8bb4e9c2418a9f3d450702e654ac3cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 05:04:45 +0000

    Add package tcrutils/13.1.23/tcrutils-13.1.23.tar.gz

---
+++ packages/tcrutils/tcrutils-13.1.23.tar.gz/tcrutils-13.1.23/tcrutils/sdb2.py
@@ -0,0 +1,61 @@
+import pathlib as p
+import shelve
+from abc import ABC, abstractmethod
+
+
+class ShelfManager[T](ABC):
+	PATH: p.Path
+
+	key: str
+
+	def __init__(self, key: str):
+		self.key = str(key)
+
+	@abstractmethod
+	def default_factory(self) -> T: ...
+
+	@classmethod
+	def open_shelf(cls) -> shelve.Shelf[str, T]:
+		cls.PATH.parent.mkdir(exist_ok=True, parents=True)
+
+		return shelve.open(cls.PATH)  # noqa: SIM115
+
+	@classmethod
+	def contains(cls, key: str) -> bool:
+		with cls.open_shelf() as shelf:
+			return str(key) in shelf
+
+	@classmethod
+	def keys(cls) -> list[str]:
+		with cls.open_shelf() as shelf:
+			return list(shelf.keys())
+
+	@classmethod
+	def values(cls) -> list[T]:
+		with cls.open_shelf() as shelf:
+			return list(shelf.values())
+
+	@classmethod
+	def items(cls) -> list[tuple[str, T]]:
+		with cls.open_shelf() as shelf:
+			return list(shelf.items())
+
+	def __enter__(self) -> T:
+		self._shelf = self.open_shelf()
+
+		self._current_item = self._shelf[self.key] if self.key in self._shelf else self.default_factory()
+		return self._current_item
+
+	def __exit__(self, ty, e: BaseException | None, tb) -> None:
+		if e is None:
+			self._shelf[self.key] = self._current_item
+
+		del self._current_item
+
+		self._shelf.close()
+		del self._shelf
+
+
+class SingleShelfManager[T](ShelfManager[T]):
+	def __init__(self):
+		super().__init__(0)
