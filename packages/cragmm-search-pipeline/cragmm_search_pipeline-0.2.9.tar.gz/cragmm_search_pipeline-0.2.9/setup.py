d96ed0e6c5d672ad6101c1bfb1826d79da0b45ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 06:04:56 +0000

    Add package cragmm-search-pipeline/0.2.9/cragmm_search_pipeline-0.2.9.tar.gz

---
+++ packages/cragmm-search-pipeline/cragmm_search_pipeline-0.2.9.tar.gz/cragmm_search_pipeline-0.2.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cragmm_search_pipeline",
+    version="0.2.9",
+    description="CragMM unified search pipeline for text and image",
+    author="Haidar Khan",
+    author_email="haidark@gmail.com",
+    url="https://github.com/xiaoyangfb/crag-mm",
+    packages=find_packages(),
+    install_requires=[
+        "transformers>=4.20.0",
+        "pillow",
+        "requests",
+        "chromadb",
+        "bs4",
+        "torch",
+        "numpy",
+        "tqdm",
+        "huggingface_hub",
+        "pillow",
+        "pydantic",
+        "requests",
+    ],
+)
