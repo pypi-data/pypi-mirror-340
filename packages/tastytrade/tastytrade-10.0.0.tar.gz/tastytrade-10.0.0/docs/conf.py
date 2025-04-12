f2f9cc24440202ce156336dbfc127b70375e481e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 06:05:38 +0000

    Add package tastytrade/10.0.0/tastytrade-10.0.0.tar.gz

---
+++ packages/tastytrade/tastytrade-10.0.0.tar.gz/tastytrade-10.0.0/docs/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+from tastytrade import VERSION
+
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "tastytrade"
+copyright = "2024, Graeme Holliday"
+author = "Graeme Holliday"
+release = VERSION
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "enum_tools.autoenum",
+    "sphinxcontrib.autodoc_pydantic",
+]
+
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+# -- Options for pydantic -------------------------------------------------
+autodoc_pydantic_model_show_json = True
+autodoc_pydantic_settings_show_json = False
+autodoc_pydantic_show_field_summary = True
+autodoc_pydantic_model_undoc_members = False
+autodoc_pydantic_model_hide_paramlist = False
+autodoc_pydantic_model_show_config_summary = False
