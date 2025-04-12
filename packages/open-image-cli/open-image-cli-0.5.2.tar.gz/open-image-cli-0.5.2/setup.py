95bef48587ca39f090b0fe068ff8b4e0c99ad47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 20:05:19 +0000

    Add package open-image-cli/0.5.2/open-image-cli-0.5.2.tar.gz

---
+++ packages/open-image-cli/open-image-cli-0.5.2.tar.gz/open-image-cli-0.5.2/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read_requirements(name):
+    with open(os.path.join(here, name), encoding='utf-8') as f:
+        require_str = f.read()
+        return require_str.split()
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='open-image-cli',
+    version='0.5.2',
+    author='youngfreefjs',
+    url='https://github.com/youngfreeFJS/imagecli',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=read_requirements('requirements.txt'),
+    entry_points={
+        'console_scripts': [
+            'img=imagecli.cli:main',
+        ],
+    },
+)
