9bacce17d25417dfd2a7956206dfbaa543310b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 14:05:03 +0000

    Add package rustipy/0.1.2/rustipy-0.1.2-py3-none-any.whl

---
+++ packages/rustipy/rustipy-0.1.2-py3-none-any.whl/__init__.py
@@ -0,0 +1,11 @@
+from .rustipy.option import Option, Some, Nothing # type: ignore
+from .rustipy.result import Result, Ok, Err # type: ignore
+
+__all__ = [
+    "Option",
+    "Some",
+    "Nothing",
+    "Result",
+    "Ok",
+    "Err",
+]
