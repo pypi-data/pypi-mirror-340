fa51e4f45431ebb2f813f9db0d2a713d60878f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 15:05:24 +0000

    Add package ry/0.0.40/ry-0.0.40-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ry/ry-0.0.40-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ry/ryo3/_flate2.pyi
@@ -0,0 +1,19 @@
+"""ryo3-flate2 types"""
+
+from __future__ import annotations
+
+from ry import Bytes
+from ry._types import Buffer
+
+# =============================================================================
+# GZIP
+# =============================================================================
+def gzip_encode(input: Buffer, quality: int = 9) -> Bytes: ...
+def gzip_decode(input: Buffer) -> Bytes: ...
+def gzip(input: Buffer, quality: int = 9) -> Bytes:
+    """Alias for gzip_encode"""
+
+def gunzip(input: Buffer) -> Bytes:
+    """Alias for gzip_decode"""
+
+def is_gzipped(input: Buffer) -> bool: ...
