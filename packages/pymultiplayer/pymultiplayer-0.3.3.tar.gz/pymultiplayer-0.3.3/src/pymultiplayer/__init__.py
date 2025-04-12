91bd876ac76f8a93022bb591b5a85d30df13a68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 05:04:58 +0000

    Add package pymultiplayer/0.3.3/pymultiplayer-0.3.3.tar.gz

---
+++ packages/pymultiplayer/pymultiplayer-0.3.3.tar.gz/pymultiplayer-0.3.3/src/pymultiplayer/__init__.py
@@ -0,0 +1,7 @@
+from .TCPserver import TCPMultiplayerServer
+from .client import MultiplayerClient
+from .server_manager import ServerManager, get_servers
+print("This library is currently undergoing a major rewrite.")
+print("Not all features will work as intended.")
+print("pymultiplayer by iamdeedz")
+print("https://www.github.com/iamdeedz/pymultiplayer")
