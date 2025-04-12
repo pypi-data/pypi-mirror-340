a61ec463230b21496c89b2359e223fe4e65cd9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-12 03:15:52 +0000

    Add package django-model-suite/0.2.0/django_model_suite-0.2.0.tar.gz

---
+++ packages/django-model-suite/django_model_suite-0.2.0.tar.gz/django_model_suite-0.2.0/django_model_suite/generators/admin/change_view_generator.py
@@ -0,0 +1,15 @@
+# change_view_generator.py
+from ..base import BaseGenerator
+
+class ChangeViewGenerator(BaseGenerator):
+    def generate(self, fields: list) -> None:
+        model_name = self.model.__name__
+        content = f'''class {model_name}ChangeView:
+    filter_horizontal = ()
+    compressed_fields = True
+    autocomplete_fields = ()
+    fieldsets = (
+        ("Information", {{"fields": ()}}),
+    )
+'''
+        self.write_file('change_view.py', content)
