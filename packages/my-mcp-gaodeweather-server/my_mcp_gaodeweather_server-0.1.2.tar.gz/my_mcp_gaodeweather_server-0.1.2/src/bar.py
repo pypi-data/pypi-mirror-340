b9cae688f7f4582fe0cc94f4c553956cc0e401b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:19 +0000

    Add package my-mcp-gaodeweather-server/0.1.2/my_mcp_gaodeweather_server-0.1.2.tar.gz

---
+++ packages/my-mcp-gaodeweather-server/my_mcp_gaodeweather_server-0.1.2.tar.gz/my_mcp_gaodeweather_server-0.1.2/src/bar.py
@@ -0,0 +1,19 @@
+"""
+ * @file   : bar.py
+ * @time   : 16:23
+ * @date   : 2025/4/12
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: Less is more.Simple is best!
+"""
+
+
+class Bar(object):
+    @staticmethod
+    def echo(arg_name: str):
+        return "Hello, {}!".format(arg_name)
+
+    pass
+
+
+pass
