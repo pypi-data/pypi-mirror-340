eb8d09eb004bf705ce9080ba29fa9a700550f2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:05:20 +0000

    Add package maleo-core/0.3.97/maleo_core-0.3.97.tar.gz

---
+++ packages/maleo-core/maleo_core-0.3.97.tar.gz/maleo_core-0.3.97/maleo_core/models/maleo_suite/maleo_soapie/transfers/general/assessment.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from pydantic import BaseModel, Field
+from typing import Optional
+from maleo_core.models.maleo_suite.maleo_soapie.transfers.general.diagnosis import MaleoSOAPIEDiagnosisGeneralTransfers
+
+class MaleoSOAPIEAssessmentGeneralTransfers:
+    class AssessmentID(BaseModel):
+        assessment_id:int = Field(..., ge=1, description="Assessment's id")
+
+    class AssessmentIDs(BaseModel):
+        assessment_ids:Optional[list[int]] = Field(None, description="Specific Assessment IDs")
+
+    class Base(BaseModel):
+        overall:str = Field(..., description="Overall assessment")
+        diagnoses:list[MaleoSOAPIEDiagnosisGeneralTransfers.Base] = Field([], description="Diagnoses")
+        other_information:Optional[str] = Field(None, description="Other information")
