e0408ae0859141bdf8662c9e849eb078daf8a490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:23 +0000

    Add package pyhowto/0.0.1/pyhowto-0.0.1-py3-none-any.whl

---
+++ packages/pyhowto/pyhowto-0.0.1-py3-none-any.whl/pyhowto/providers.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import os
+
+import google.generativeai as genai
+from openai import OpenAI
+
+prompt = """
+how to {}
+
+
+Give concise and short solution/answer, to the point.
+If the answer is technical, just give the technical solution
+"""
+
+
+def ask_deepseek(howto: str) -> str:
+    assert 'DEEPSEEK_API_KEY' in os.environ.keys(), "missing env variable DEEPSEEK_API_KEY"
+    client = OpenAI(api_key=os.environ['DEEPSEEK_API_KEY'], base_url="https://api.deepseek.com")
+    message = [{"role": "user", "content": prompt.format(howto)}]
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=message
+    )
+    return response.choices[0].message.content
+
+
+def ask_gemini(howto: str) -> str:
+    assert 'GEMINI_API_KEY' in os.environ.keys(), "missing env variable GEMINI_API_KEY"
+    genai.configure(api_key=os.environ['GEMINI_API_KEY'])
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt.format(howto))
+    return response.text
+
+
+def ask_openai(howto: str) -> str:
+    assert 'OPENAI_API_KEY' in os.environ.keys(), "missing env variable OPENAI_API_KEY"
+    client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+    response = client.responses.create(
+        model="gpt-4o",
+        input=prompt.format(howto)
+    )
+    return response.output_text
+
+
+if __name__ == '__main__':
+    question = 'substitute string in bash'
+    print(ask_gemini(question))
