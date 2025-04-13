727ee92c5b464b1a4506d5b00e90fc8f13e7a906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:18 +0000

    Add package hike/0.11.1/hike-0.11.1.tar.gz

---
+++ packages/hike/hike-0.11.1.tar.gz/hike-0.11.1/src/hike/support/__init__.py
@@ -0,0 +1,13 @@
+"""Support code and classes."""
+
+##############################################################################
+# Local imports.
+from .history import History
+from .mouse import is_copy_request_click
+from .view_in_browser import view_in_browser
+
+##############################################################################
+# Exports.
+__all__ = ["History", "is_copy_request_click", "view_in_browser"]
+
+### __init__.py ends here
