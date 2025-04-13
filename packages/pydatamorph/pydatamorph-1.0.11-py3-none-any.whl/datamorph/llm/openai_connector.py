cfc645dcb918683092af9e34571ca93c41e1d32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:46 +0000

    Add package pydatamorph/1.0.11/pydatamorph-1.0.11-py3-none-any.whl

---
+++ packages/pydatamorph/pydatamorph-1.0.11-py3-none-any.whl/datamorph/llm/openai_connector.py
@@ -0,0 +1,31 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+#load_dotenv() <<-- use this for local development
+
+def getapikey:
+    with open('/run/secrets/my_secret') as f:
+        api_key = f.read().strip()
+    return api_key
+
+#api_key=os.getenv("OPENAI_API_KEY") <<-- use this for local development
+
+# Initialize OpenAI client
+client = OpenAI(api_key=getapikey())
+
+def call_llm(prompt, model="gpt-3.5-turbo"):
+    try:
+        response = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": "You are a helpful pipeline planner."},
+                {"role": "user", "content": prompt}
+            ],
+            temperature=0.3
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"[❌] LLM call failed: {e}")
+        return "error"
