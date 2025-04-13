93cc4dda8517e608acdb4da207acdf7f22ba3e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:06:59 +0000

    Add package maleo-core/0.3.93/maleo_core-0.3.93.tar.gz

---
+++ packages/maleo-core/maleo_core-0.3.93.tar.gz/maleo_core-0.3.93/maleo_core/models/maleo_suite/maleo_soapie/transfers/results/service/__init__.py
@@ -0,0 +1,9 @@
+# This file serves all MaleoSOAPIE Services Results
+
+from __future__ import annotations
+
+class MaleoSOAPIEServiceResults: pass
+
+__all__ = [
+    "MaleoSOAPIEServiceResults",
+]
