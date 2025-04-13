88cc886bd06645d77231f46710a26ef0502d51e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:40 +0000

    Add package blueness/3.118.1/blueness-3.118.1-py3-none-any.whl

---
+++ packages/blueness/blueness-3.118.1-py3-none-any.whl/blueness/__init__.py
@@ -0,0 +1,13 @@
+NAME = "blueness"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} Blueness for Bluer AI."
+
+VERSION = "3.118.1"
+
+REPO_NAME = "blueness"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/raw/main/blue-objects/marquee.png?raw=true"
+)
