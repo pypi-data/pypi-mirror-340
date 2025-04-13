1f83cfc143be1050521d0b7c6b9199b86e7a113f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:05:47 +0000

    Add package ntsulib/1.2.168/ntsulib-1.2.168-py3-none-any.whl

---
+++ packages/ntsulib/ntsulib-1.2.168-py3-none-any.whl/ntsulib/npyqt5/__init__.py
@@ -0,0 +1,8 @@
+from .npyqt5 import *
+
+# npyqt5.py
+import ctypes
+from typing import Union
+from PyQt5.QtCore import QRect
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtWidgets import QWidget, QMainWindow
