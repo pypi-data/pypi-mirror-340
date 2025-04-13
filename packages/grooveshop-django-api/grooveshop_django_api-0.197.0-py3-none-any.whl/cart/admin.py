7ba64c9a0a514bc202d1c0e52ec35309c29ebaf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:57:08 +0000

    Add package grooveshop-django-api/0.197.0/grooveshop_django_api-0.197.0-py3-none-any.whl

---
+++ packages/grooveshop-django-api/grooveshop_django_api-0.197.0-py3-none-any.whl/cart/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from cart.models import Cart, CartItem
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_filter = ["id"]
+    search_fields = ["id"]
+    date_hierarchy = "updated_at"
+    inlines = [CartItemInline]
+
+
+admin.site.register(CartItem)
---
+++ packages/grooveshop-django-api/grooveshop_django_api-0.197.0-py3-none-any.whl/country/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from parler.admin import TranslatableAdmin
+
+from country.models import Country
+from region.admin import RegionInline
+
+
+@admin.register(Country)
+class CountryAdmin(TranslatableAdmin):
+    search_fields = (
+        "translations__name",
+        "alpha_2",
+        "alpha_3",
+        "iso_cc",
+        "phone_code",
+    )
+    inlines = [RegionInline]
