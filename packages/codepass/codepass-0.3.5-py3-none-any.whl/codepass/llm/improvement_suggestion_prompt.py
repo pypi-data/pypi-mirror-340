4235385bb215b02234c7b26d8c875cc6c5b95da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:56:24 +0000

    Add package codepass/0.3.5/codepass-0.3.5-py3-none-any.whl

---
+++ packages/codepass/codepass-0.3.5-py3-none-any.whl/codepass/llm/improvement_suggestion_prompt.py
@@ -0,0 +1,23 @@
+improvement_suggestion_prompt = """
+Analyze the given code and provide suggestion to reduce its cognitive complexity. 
+
+Prioritize factors of cognitive complexity in the following order:
+
+1. Advanced coding techniques (like functional programming paradigms) that are not essential for solving the task but reflect the developer’s preference.
+2. Deep technical domain knowledge required, such as advanced algorithms, parallel programming, signal processing, or low-level optimizations.
+3. Project-specific knowledge is required, such as the use of third-party libraries or specific business rules
+4. Control structures and expressions
+5. Readable the code is based on factors like naming conventions, formatting, and non-runtime characteristics
+
+Provide only one the most important suggestion. Skip introduction and motivation behind you suggestion, keep it short and focus on explaining the action. 2 sentence maximum.
+
+{format_instructions}
+
+Please note that JSON does not support comments.
+
+{error_recovery_instructions}
+
+Code to evaluate: 
+
+{code}
+"""
