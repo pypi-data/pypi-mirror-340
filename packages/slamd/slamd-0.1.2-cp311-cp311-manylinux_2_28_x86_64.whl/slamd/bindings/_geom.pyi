3713c6b9ad52c9b0f6fd2ca59ec1561eaeccbca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:56:35 +0000

    Add package slamd/0.1.2/slamd-0.1.2-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/slamd/slamd-0.1.2-cp311-cp311-manylinux_2_28_x86_64.whl/slamd/bindings/_geom.pyi
@@ -0,0 +1,26 @@
+from __future__ import annotations
+__all__ = ['Arrows', 'Box', 'CameraFrustum', 'Geometry', 'Image', 'MonoMesh', 'PointCloud', 'Points2D', 'PolyLine', 'SimpleMesh', 'Sphere', 'Triad']
+class Arrows(Geometry):
+    pass
+class Box(Geometry):
+    pass
+class CameraFrustum(Geometry):
+    pass
+class Geometry:
+    pass
+class Image(Geometry):
+    pass
+class MonoMesh(Geometry):
+    pass
+class PointCloud(Geometry):
+    pass
+class Points2D(Geometry):
+    pass
+class PolyLine(Geometry):
+    pass
+class SimpleMesh(Geometry):
+    pass
+class Sphere(Geometry):
+    pass
+class Triad(Geometry):
+    pass
