b1952e8ce696ca5951ca3d4a20799d5e0576ef66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:57:03 +0000

    Add package rc-toolkit/0.2.0/rc_toolkit-0.2.0-py3-none-any.whl

---
+++ packages/rc-toolkit/rc_toolkit-0.2.0-py3-none-any.whl/RCTK/core/io/file.py
@@ -0,0 +1,23 @@
+from os import path, makedirs
+from pathlib import Path
+
+from ..env import is_debug
+
+from typing import NoReturn
+
+def mkdir(file_path: str) -> NoReturn:
+    """
+    make file dirs
+
+    Args:
+        file_path (str): file path
+    """
+
+    dir_path = Path(file_path).parent
+    if not path.isdir(dir_path):
+        try:
+            makedirs(dir_path)
+        except Exception:  # pylint: disable=broad-exception-caught
+            if not is_debug(): return  
+            raise
+
