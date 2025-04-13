9ef638edf51356f51271e33cef63ded6d362c4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:35 +0000

    Add package yta-ai-utils/0.0.8/yta_ai_utils-0.0.8.tar.gz

---
+++ packages/yta-ai-utils/yta_ai_utils-0.0.8.tar.gz/yta_ai_utils-0.0.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.8'
+DESCRIPTION = 'Youtube Autónomo AI utils are here.'
+LONG_DESCRIPTION = 'These are the AI utils we need in the Youtube Autónomo project to work in a better way.'
+
+setup(
+    name = "yta_ai_utils", 
+    version = VERSION,
+    author = "Daniel Alcalá",
+    author_email = "<danielalcalavalera@gmail.com>",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    packages = find_packages(),
+    install_requires = [
+        'yta-general-utils',
+        'google-generativeai',
+    ],
+    
+    keywords = [
+        'youtube autonomo ai utils'
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
