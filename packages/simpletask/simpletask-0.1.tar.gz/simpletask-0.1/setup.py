f2e511b934fd04cd3af3b89bf539bb087b44a3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:45 +0000

    Add package simpletask/0.1/simpletask-0.1.tar.gz

---
+++ packages/simpletask/simpletask-0.1.tar.gz/simpletask-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simpletask",
+    version="0.1",
+    packages=find_packages(),
+    description="Librería para tareas programadas en Python",
+    author="Nicolás Castro (nicocastronc)",
+    install_requires=[],  # Cero dependencias.
+    python_requires=">=3.6",
+)
