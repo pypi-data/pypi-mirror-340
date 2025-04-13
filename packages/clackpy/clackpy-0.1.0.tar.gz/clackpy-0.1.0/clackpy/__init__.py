726b63bdf5e465d74e2300c9de88e559dcc6530c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:40 +0000

    Add package clackpy/0.1.0/clackpy-0.1.0.tar.gz

---
+++ packages/clackpy/clackpy-0.1.0.tar.gz/clackpy-0.1.0/clackpy/__init__.py
@@ -0,0 +1,12 @@
+from .prompts import (
+    text,
+    password,
+    confirm,
+    select,
+    multiselect,
+    is_cancel,
+    Color,
+    CURSOR_UP_ONE,
+    CARRIAGE_RETURN,
+    ERASE_LINE,
+)
