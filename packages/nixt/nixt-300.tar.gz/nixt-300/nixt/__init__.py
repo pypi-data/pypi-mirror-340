3cd0d8478cae0f421fbc5c891cdb5b68df689e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:56:23 +0000

    Add package nixt/300/nixt-300.tar.gz

---
+++ packages/nixt/nixt-300.tar.gz/nixt-300/nixt/__init__.py
@@ -0,0 +1,10 @@
+# This file is placed in the Public Domain.
+
+
+"NIXT"
+
+
+from nixt        import object, json
+from nixt.disk   import read,write
+from nixt.object import *
+from nixt.json   import *
