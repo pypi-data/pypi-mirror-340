af9f4bdd21932b5f143afccdc4cc84a439e45de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:06:14 +0000

    Add package equations-solver-system/0.1.0/equations_solver_system-0.1.0.tar.gz

---
+++ packages/equations-solver-system/equations_solver_system-0.1.0.tar.gz/equations_solver_system-0.1.0/README.md
@@ -0,0 +1,13 @@
+# equations_solver_system
+
+Librería sencilla para resolver sistemas de ecuaciones lineales y no lineales.
+
+## Métodos implementados
+
+- Eliminación de Gauss
+- Gauss-Jordan
+- Cramer
+- LU
+- Jacobi
+- Gauss-Seidel
+- Bisección
---
+++ packages/equations-solver-system/equations_solver_system-0.1.0.tar.gz/equations_solver_system-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="equations_solver_system",
+    version="0.1.0",
+    description="Librería sencilla para resolver sistemas de ecuaciones lineales y no lineales",
+    author="Luis Eduardo Salamanca Garcia",
+    packages=find_packages(),
+)
