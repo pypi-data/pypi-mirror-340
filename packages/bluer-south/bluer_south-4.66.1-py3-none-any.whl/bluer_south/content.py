397693ed57a620c55e13b6dd117505e9a25d95cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:44 +0000

    Add package bluer-south/4.66.1/bluer_south-4.66.1-py3-none-any.whl

---
+++ packages/bluer-south/bluer_south-4.66.1-py3-none-any.whl/bluer_south/content.py
@@ -0,0 +1,38 @@
+import blueness
+import bluer_ai
+import bluer_options
+import bluer_objects
+import bluer_plugin
+import bluer_sandbox
+import bluer_sbc
+from bluer_objects import README
+import gizai
+
+
+items = README.Items(
+    [
+        {
+            "name": module.NAME,
+            "marquee": module.MARQUEE,
+            "description": " ".join(
+                [
+                    module.DESCRIPTION.replace(module.ICON, "").strip(),
+                    " [![PyPI version](https://img.shields.io/pypi/v/{}.svg)](https://pypi.org/project/{}/)".format(
+                        module.NAME, module.NAME
+                    ),
+                ]
+            ),
+            "url": f"https://github.com/kamangir/{module.REPO_NAME}",
+        }
+        for module in [
+            bluer_ai,
+            bluer_sbc,
+            blueness,
+            bluer_options,
+            bluer_objects,
+            bluer_plugin,
+            bluer_sandbox,
+            gizai,
+        ]
+    ]
+)
