d791dda00d1bfb13abfd0c7420b67d4b4c430b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:57:36 +0000

    Add package pyweber/0.9.56/pyweber-0.9.56-py3-none-any.whl

---
+++ packages/pyweber/pyweber-0.9.56-py3-none-any.whl/pyweber/core/__init__.py
@@ -0,0 +1,17 @@
+from .element import Element
+from .template import Template
+from .window import Window
+from .events import (
+    EventHandler,
+    WindowEvents,
+    TemplateEvents
+)
+
+__all__ = [
+    'Element',
+    'Template',
+    'Window',
+    'EventHandler',
+    'WindowEvents',
+    'TemplateEvents'
+]
