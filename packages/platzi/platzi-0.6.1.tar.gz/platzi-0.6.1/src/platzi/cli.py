f9ba7127aee3f1aa9bed95901c3630c33b5822ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:32 +0000

    Add package platzi/0.6.1/platzi-0.6.1.tar.gz

---
+++ packages/platzi/platzi-0.6.1.tar.gz/platzi-0.6.1/src/platzi/cli.py
@@ -0,0 +1,92 @@
+import asyncio
+
+import typer
+from rich import print
+from typing_extensions import Annotated
+
+from platzi import AsyncPlatzi, Cache
+
+app = typer.Typer(rich_markup_mode="rich")
+
+
+@app.command()
+def login():
+    """
+    Open a browser window to Login to Platzi.
+
+    Usage:
+        platzi login
+    """
+    asyncio.run(_login())
+
+
+@app.command()
+def logout():
+    """
+    Delete the Platzi session from the local storage.
+
+    Usage:
+        platzi logout
+    """
+    asyncio.run(_logout())
+
+
+@app.command()
+def download(
+    url: Annotated[
+        str,
+        typer.Argument(
+            help="The URL of the course to download",
+            show_default=False,
+        ),
+    ],
+    overwrite: Annotated[
+        bool,
+        typer.Option(
+            "--overwrite",
+            "-w",
+            help="Overwrite files if exist.",
+            show_default=True,
+        ),
+    ] = False,
+):
+    """
+    Download a Platzi course from the given URL.
+
+    Arguments:
+        url: str - The URL of the course to download.
+
+    Usage:
+        platzi download <url>
+
+    Example:
+        platzi download https://platzi.com/cursos/fastapi-2023/
+    """
+    asyncio.run(_download(url, overwrite=overwrite))
+
+
+@app.command()
+def clear_cache():
+    """
+    Clear the Platzi CLI cache.
+
+    Usage:
+        platzi clear-cache
+    """
+    Cache.clear()
+    print("[green]Cache cleared successfully 🗑️[/green]")
+
+
+async def _login():
+    async with AsyncPlatzi() as platzi:
+        await platzi.login()
+
+
+async def _logout():
+    async with AsyncPlatzi() as platzi:
+        await platzi.logout()
+
+
+async def _download(url: str, **kwargs):
+    async with AsyncPlatzi() as platzi:
+        await platzi.download(url, **kwargs)
---
+++ packages/platzi/platzi-0.6.1.tar.gz/platzi-0.6.1/src/platzi/utils.py
@@ -0,0 +1,123 @@
+import asyncio
+import re
+from pathlib import Path
+
+import aiofiles
+import rnet
+from playwright.async_api import Page
+from unidecode import unidecode
+
+from .helpers import retry
+
+
+async def progressive_scroll(
+    page: Page, time: float = 3, delay: float = 0.1, steps: int = 250
+):
+    await asyncio.sleep(3)  # delay to avoid rate limiting
+    delta, total_time = 0.0, 0.0
+    while total_time < time:
+        await asyncio.sleep(delay)
+        await page.mouse.wheel(0, steps)
+        delta += steps
+        total_time += delay
+
+
+def get_course_slug(url: str) -> str:
+    """
+    Extracts the course slug from a Platzi course URL.
+
+    :param url(str): The Platzi course URL.
+    :return str: The course slug.
+    :raises Exception: If the URL is not a valid Platzi course URL.
+
+    Example
+    -------
+    >>> get_course_slug("https://platzi.com/cursos/fastapi-2023/")
+    "fastapi-2023"
+    """
+    pattern = r"https://platzi\.com/cursos/([^/]+)/?"
+    match = re.search(pattern, url)
+    if not match:
+        raise Exception("Invalid course url")
+    return match.group(1)
+
+
+def clean_string(text: str) -> str:
+    """
+    Remove special characters from a string and strip it.
+
+    :param text(str): string to clean
+    :return str: cleaned string
+
+    Example
+    -------
+    >>> clean_string("   Hi:;<>?{}|"")
+    "Hi"
+    """
+    result = re.sub(r"[ºª\n\r]|[^\w\s]", "", text)
+    return re.sub(r"\s+", " ", result).strip()
+
+
+def slugify(text: str) -> str:
+    """
+    Slugify a string, removing special characters and replacing
+    spaces with hyphens.
+
+    :param text(str): string to convert
+    :return str: slugified string
+
+    Example
+    -------
+    >>> slugify(""Café! Frío?"")
+    "cafe-frio"
+    """
+    return unidecode(clean_string(text)).lower().replace(" ", "-")
+
+
+def get_m3u8_url(content: str) -> str:
+    pattern = r"https?://[^\s\"'}]+\.m3u8"
+    matches = re.findall(pattern, content)
+
+    if not matches:
+        raise Exception("No m3u8 urls found")
+
+    return matches[0]
+
+
+def get_subtitles_url(content: str) -> str | None:
+    pattern = r"https?://[^\s\"'}]+\.vtt"
+    matches = re.findall(pattern, content)
+
+    if not matches:
+        return None
+
+    return matches[0]
+
+
+@retry()
+async def download(url: str, path: Path, **kwargs):
+    overwrite = kwargs.get("overwrite", False)
+
+    if not overwrite and path.exists():
+        return
+
+    path.unlink(missing_ok=True)
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    client = rnet.Client(impersonate=rnet.Impersonate.Firefox135)
+    response: rnet.Response = await client.get(url, **kwargs)
+
+    try:
+        if not response.ok:
+            raise Exception("[Bad Response]")
+
+        async with aiofiles.open(path, "wb") as file:
+            async with response.stream() as streamer:
+                async for chunk in streamer:
+                    await file.write(chunk)
+
+    except Exception as e:
+        raise Exception(f"Error downloading file: [{path.name}]") from e
+
+    finally:
+        response.close()
