69716b6d68c2646819b78468d3b3b0fe853fbaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:05:19 +0000

    Add package maleo-core/0.3.95/maleo_core-0.3.95-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.3.95-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_soapie/transfers/results/client/http/services/__init__.py
@@ -0,0 +1,9 @@
+# This file serves all MaleoSOAPIE's HTTP Client Services Results
+
+from __future__ import annotations
+
+class MaleoSOAPIEHTTPClientServicesResults: pass
+
+__all__ = [
+    "MaleoSOAPIEHTTPClientServicesResults",
+]
