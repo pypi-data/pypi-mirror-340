1cfc7a354d94a403748d693763ee264bd6ffd824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:56:46 +0000

    Add package cicpy/0.1.8/cicpy-0.1.8-py3-none-any.whl

---
+++ packages/cicpy/cicpy-0.1.8-py3-none-any.whl/cicpy/core/layoutcell.py
@@ -0,0 +1,96 @@
+######################################################################
+##        Copyright (c) 2020 Carsten Wulff Software, Norway 
+## ###################################################################
+## Created       : wulff at 2020-3-14
+## ###################################################################
+##  The MIT License (MIT)
+## 
+##  Permission is hereby granted, free of charge, to any person obtaining a copy
+##  of this software and associated documentation files (the "Software"), to deal
+##  in the Software without restriction, including without limitation the rights
+##  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+##  copies of the Software, and to permit persons to whom the Software is
+##  furnished to do so, subject to the following conditions:
+## 
+##  The above copyright notice and this permission notice shall be included in all
+##  copies or substantial portions of the Software.
+## 
+##  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+##  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+##  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+##  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+##  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+##  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+##  SOFTWARE.
+##  
+######################################################################
+
+from .rect import Rect
+from .cell import Cell
+from .port import Port
+from .instance import Instance
+from .text import Text
+import cicspi as spi
+
+class LayoutCell(Cell):
+
+    def __init__(self):
+        super().__init__()
+        self.altenateGroup = False
+        self.boundaryIgnoreRouting = False
+        self.useHalfHeight = False
+        self.graph = None
+
+
+    def toJson(self):
+        o = super().toJson()
+        return o
+
+
+    def place():
+
+        pass
+
+    def route():
+
+        pass
+
+    def fromJson(self,o):
+        super().fromJson(o)
+
+        if("alternateGroup" in o):
+            self.alternateGroup = o["alternateGroup"]
+
+        if("useHalfHeight" in o):
+            self.useHalfHeight = o["useHalfHeight"]
+
+        if("boundarIgnoreRouting" in o):
+            self.boundaryIgnoreRouting = o["boundaryIgnoreRouting"]
+
+        if("meta" in o):
+            self.meta = o["meta"]
+
+        if("graph" in o):
+            self.graph = o["graph"]
+
+        for child in o["children"]:
+
+            c = None
+            cl = child["class"]
+            if(cl == "Rect"):
+                c = Rect()
+            elif(cl == "Port"):
+                c  = Port()
+            elif(cl == "Text"):
+                c  = Text()
+            elif(cl == "Instance"):
+                c  = Instance()
+            elif(cl == "Cell" or cl== "cIcCore::Route" or cl == "cIcCore::RouteRing" or cl == "cIcCore::Guard" or cl == "cIcCore::Cell" or cl == "cIcCore::LayoutCell"):
+                c = LayoutCell()
+            else:
+                print(f"Unkown class {cl}")
+
+            if(c is not None):
+                c.design = self.design
+                c.fromJson(child)
+                self.add(c)
