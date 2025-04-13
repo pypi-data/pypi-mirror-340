f79e86d423d3e7af033ce5f64269874e8d61091f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:48 +0000

    Add package ndtools/0.1.1/ndtools-0.1.1-py3-none-any.whl

---
+++ packages/ndtools/ndtools-0.1.1-py3-none-any.whl/ndtools/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ["comparison", "operators", "total_equality", "total_ordering"]
+__version__ = "0.1.1"
+
+
+# dependencies
+from . import comparison
+from . import operators
+from .comparison import *
