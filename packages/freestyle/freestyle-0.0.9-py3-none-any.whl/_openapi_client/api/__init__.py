924d4a3ab5e80eef2764a5b114831b25e857f520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:15 +0000

    Add package freestyle/0.0.9/freestyle-0.0.9-py3-none-any.whl

---
+++ packages/freestyle/freestyle-0.0.9-py3-none-any.whl/_openapi_client/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from _openapi_client.api.certs_api import CertsApi
+from _openapi_client.api.cloudstate_api import CloudstateApi
+from _openapi_client.api.dns_api import DNSApi
+from _openapi_client.api.domains_api import DomainsApi
+from _openapi_client.api.execute_api import ExecuteApi
+from _openapi_client.api.git_api import GitApi
+from _openapi_client.api.observability_api import ObservabilityApi
+from _openapi_client.api.web_api import WebApi
+
