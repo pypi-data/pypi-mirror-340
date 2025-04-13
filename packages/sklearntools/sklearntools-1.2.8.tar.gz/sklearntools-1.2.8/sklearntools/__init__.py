1a334ca5bdc210153c32c3d86af526c9d10205e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:05:39 +0000

    Add package sklearntools/1.2.8/sklearntools-1.2.8.tar.gz

---
+++ packages/sklearntools/sklearntools-1.2.8.tar.gz/sklearntools-1.2.8/sklearntools/__init__.py
@@ -0,0 +1,13 @@
+from ._tools import train_evaluate, train_evaluate_split, search_model_params, search_model_params_split, \
+	search_test_size, search_random_state, multi_round_evaluate, one_round_evaluate
+
+__all__ = [
+	'train_evaluate',
+	'train_evaluate_split',
+	'search_model_params',
+	'search_model_params_split',
+	'search_test_size',
+	'search_random_state',
+	'multi_round_evaluate',
+	'one_round_evaluate'
+]
