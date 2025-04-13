076d912af2af2ab4792fd9d4602da6ea5997fdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:56:30 +0000

    Add package brave-bookmarks/1.1.1/brave-bookmarks-1.1.1.tar.gz

---
+++ packages/brave-bookmarks/brave-bookmarks-1.1.1.tar.gz/brave-bookmarks-1.1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import distutils.core
+
+setuptools.setup(
+    name='brave-bookmarks',
+    version="1.1.1",
+    author='@readwithai',
+    long_description_content_type='text/markdown',
+    author_email='talwrii@gmail.com',
+    description='',
+    license='MIT',
+    keywords='bookmarks, brave, command-line',
+    url='https://github.com/talwrii/brave-bookmarks',
+    packages=["brave_bookmarks"],
+    long_description=open('README.md').read(),
+    entry_points={
+        'console_scripts': [
+            'bravemarks=brave_bookmarks.main:bookmarks',
+            'bravemark=brave_bookmarks.main:bookmark',
+            'brave-bookmarks=brave_bookmarks.main:documentation',
+            ]
+        }
+)
