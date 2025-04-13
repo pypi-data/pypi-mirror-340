39217feb2993a77cf39bc1da0d04ca4f8ab23ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:05:33 +0000

    Add package PySCHARE/0.0.5.7/pyschare-0.0.5.7-py2.py3-none-any.whl

---
+++ packages/PySCHARE/pyschare-0.0.5.7-py2.py3-none-any.whl/pyschare/__init__.py
@@ -0,0 +1,9 @@
+from pyschare.labels import labels
+from pyschare.search import search
+from pyschare.visuals import visuals
+from pyschare.explore import explore
+from pyschare.gemini_chat import get_gemini_chat
+
+
+__all__=["labels", "search", "visuals", "explore", "gemini_chat"]
+
