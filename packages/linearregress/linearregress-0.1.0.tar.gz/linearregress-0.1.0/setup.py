4c2db671c4e6a683f8c6694259e41efc3a5a251e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:41 +0000

    Add package linearregress/0.1.0/linearregress-0.1.0.tar.gz

---
+++ packages/linearregress/linearregress-0.1.0.tar.gz/linearregress-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='linearregress',                     # Your package name
+    version='0.1.0',                    # Version
+    description='Practical of Simple/Multiple Linear Regression.',
+    author='Aryan Bhagwat',
+    author_email='legendasur531@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
