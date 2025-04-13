f9c5b885065636e79c98d3939dd0ebddcb582ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:56:23 +0000

    Add package jaraco.mongodb/12.1.3/jaraco_mongodb-12.1.3-py3-none-any.whl

---
+++ packages/jaraco.mongodb/jaraco_mongodb-12.1.3-py3-none-any.whl/jaraco/mongodb/check-gridfs.py
@@ -0,0 +1,56 @@
+"""
+Script to check a GridFS instance for corrupted records.
+"""
+
+import logging
+import sys
+
+import autocommand
+import pymongo
+from more_itertools.recipes import consume
+
+from jaraco.context import ExceptionTrap
+from jaraco.itertools import Counter
+from jaraco.mongodb import helper
+from jaraco.ui import progress
+
+log = logging.getLogger()
+
+
+class FileChecker:
+    def __init__(self, gfs, depth):
+        self.gfs = gfs
+        self.depth = depth
+
+    def run(self):
+        files = self.gfs.list()
+        bar = progress.TargetProgressBar(len(files))
+        processed_files = map(self.process, bar.iterate(files))
+        errors = filter(None, processed_files)
+        counter = Counter(errors)
+        consume(map(self.handle_trap, counter))
+        return counter
+
+    def process(self, filename):
+        file = self.gfs.get_last_version(filename)
+        with ExceptionTrap(pymongo.errors.PyMongoError) as trap:
+            file.read(self.depth)
+        trap.filename = filename
+        return trap
+
+    def handle_trap(self, trap):
+        cls, exc, tb = trap.exc_info
+        log.error("Failed to read %s (%s)", trap.filename, exc)
+
+
+@autocommand.autocommand(__name__)
+def run(
+    db: helper.connect_gridfs,
+    depth: (int, 'Bytes to read into each file during check') = 1024,  # noqa: F722
+):
+    logging.basicConfig(stream=sys.stderr)
+
+    checker = FileChecker(db, depth)
+    counter = checker.run()
+
+    print("Encountered", counter.count, "errors")
---
+++ packages/jaraco.mongodb/jaraco_mongodb-12.1.3-py3-none-any.whl/jaraco/mongodb/monitor-index-creation.py
@@ -0,0 +1,29 @@
+import re
+import time
+
+import autocommand
+
+from jaraco.mongodb import helper
+
+from .compat import query_or_command
+
+
+def is_index_op(op):
+    cmd = query_or_command(op) or {}
+    return 'createIndexes' in cmd
+
+
+@autocommand.autocommand(__name__)
+def run(db: helper.connect_db):
+    while True:
+        ops = db.current_op()['inprog']  # type: ignore[attr-defined]
+        index_op = next(filter(is_index_op, ops), None)
+        if not index_op:
+            print("No index operations in progress")
+            break
+        msg = index_op['msg']
+        name = query_or_command(index_op)['indexes'][0]['name']
+        pat = re.compile(r'Index Build( \(background\))?')
+        msg = pat.sub(name, msg, count=1)
+        print(msg, end='\r')
+        time.sleep(5)
