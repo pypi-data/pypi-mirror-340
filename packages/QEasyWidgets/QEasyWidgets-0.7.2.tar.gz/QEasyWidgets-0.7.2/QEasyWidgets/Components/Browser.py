c1e9349de75524778cebd491599211d44f65511c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:39 +0000

    Add package QEasyWidgets/0.7.2/QEasyWidgets-0.7.2.tar.gz

---
+++ packages/QEasyWidgets/QEasyWidgets-0.7.2.tar.gz/QEasyWidgets-0.7.2/QEasyWidgets/Components/Browser.py
@@ -0,0 +1,29 @@
+from typing import Optional, overload
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+
+from ..Common.StyleSheet import *
+from ..Common.QFunctions import *
+from ..Resources.Sources import *
+from .Menu import MenuBase
+
+##############################################################################################################################
+
+class TextBrowserBase(QTextBrowser):
+    """
+    Base class for textBrowser components
+    """
+
+    def __init__(self, parent: Optional[QWidget] = None):
+        super().__init__(parent)
+
+        StyleSheetBase.Browser.apply(self)
+
+    def setBorderless(self, borderless: bool) -> None:
+        self.setProperty("isBorderless", borderless)
+
+    def clearDefaultStyleSheet(self) -> None:
+        StyleSheetBase.Browser.deregistrate(self)
+
+##############################################################################################################################
