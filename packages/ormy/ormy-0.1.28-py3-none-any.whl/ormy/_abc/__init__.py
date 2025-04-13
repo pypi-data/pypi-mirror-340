831103ca692f4881d936ee4dd0849fb6ec0abaac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:03 +0000

    Add package ormy/0.1.28/ormy-0.1.28-py3-none-any.whl

---
+++ packages/ormy/ormy-0.1.28-py3-none-any.whl/ormy/_abc/__init__.py
@@ -0,0 +1,14 @@
+from .abstract import AbstractABC, AbstractMixinABC, SemiFrozenField
+from .config import ConfigABC, Mergeable
+from .typing import AbstractData
+
+# ----------------------- #
+
+__all__ = [
+    "ConfigABC",
+    "AbstractABC",
+    "AbstractMixinABC",
+    "SemiFrozenField",
+    "AbstractData",
+    "Mergeable",
+]
