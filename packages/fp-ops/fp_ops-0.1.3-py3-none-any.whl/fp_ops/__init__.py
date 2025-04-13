c9f3ade39651960d35fdb7d04727e391946a1126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:20 +0000

    Add package fp-ops/0.1.3/fp_ops-0.1.3-py3-none-any.whl

---
+++ packages/fp-ops/fp_ops-0.1.3-py3-none-any.whl/fp_ops/__init__.py
@@ -0,0 +1,15 @@
+from fp_ops.context import BaseContext
+from fp_ops.placeholder import _, Placeholder
+from fp_ops.decorators import operation
+
+
+
+__version__ = "0.1.3"
+
+
+__all__ = [
+    "BaseContext",
+    "Placeholder",
+    "_",
+    "operation",
+]
