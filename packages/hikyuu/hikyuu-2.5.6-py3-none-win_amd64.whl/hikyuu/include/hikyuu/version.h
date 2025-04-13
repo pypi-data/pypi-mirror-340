b3d3fdc8b873297916d7e7a5f072bfdd36eb8442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:06:23 +0000

    Add package hikyuu/2.5.6/hikyuu-2.5.6-py3-none-win_amd64.whl

---
+++ packages/hikyuu/hikyuu-2.5.6-py3-none-win_amd64.whl/hikyuu/include/hikyuu/version.h
@@ -0,0 +1,24 @@
+/*
+ * version.h
+ *
+ *  Copyright (c) 2020, hikyuu.org
+ *
+ *  Created on: 2020-4-19
+ *      Author: fasiondog
+ */
+
+#pragma once
+#ifndef HKU_VERSION_H
+#define HKU_VERSION_H
+
+// clang-format off
+#define HKU_VERSION "2.5.6"
+#define HKU_VERSION_MAJOR 2
+#define HKU_VERSION_MINOR 5
+#define HKU_VERSION_ALTER 6
+#define HKU_VERSION_BUILD 202504131513
+#define HKU_VERSION_MODE "RELEASE"
+#define HKU_VERSION_GIT "2.5.6 release.2e933ae9 (RELEASE)"
+// clang-format on
+
+#endif /* HKU_VERSION_H */
