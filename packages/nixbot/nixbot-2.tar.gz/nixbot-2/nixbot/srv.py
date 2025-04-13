dd46ac5299478ae389bff17cf1b8825d905741e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:57:13 +0000

    Add package nixbot/2/nixbot-2.tar.gz

---
+++ packages/nixbot/nixbot-2.tar.gz/nixbot-2/nixbot/srv.py
@@ -0,0 +1,27 @@
+# This file is placed in the Public Domain.
+
+
+"create service file"
+
+
+from . import Main
+
+
+TXT = """[Unit]
+Description=%s
+After=network-online.target
+
+[Service]
+Type=simple
+User=%s
+Group=%s
+ExecStart=/home/%s/.local/bin/%s -s
+
+[Install]
+WantedBy=multi-user.target"""
+
+
+def srv(event):
+    import getpass
+    name = getpass.getuser()
+    event.reply(TXT % (Main.name.upper(), name, name, name, Main.name))
