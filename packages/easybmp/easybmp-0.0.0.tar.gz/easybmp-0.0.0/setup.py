e5fad16a5f094ad30331b28b332ea0a133c973c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:57:18 +0000

    Add package easybmp/0.0.0/easybmp-0.0.0.tar.gz

---
+++ packages/easybmp/easybmp-0.0.0.tar.gz/easybmp-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easybmp',
+    version='0.0.0',
+    description='A Python library which can easily read / modify / compare / and other functions with .bmp files',
+    installed_requires=['struct'],
+)
