538379ecae8d0fbec541904ec9a12ccdeb7147ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:43 +0000

    Add package bluer-ai/12.87.1/bluer_ai-12.87.1.tar.gz

---
+++ packages/bluer-ai/bluer_ai-12.87.1.tar.gz/bluer_ai-12.87.1/README.md
@@ -0,0 +1,32 @@
+# 🌀 bluer-ai
+
+🌀 `bluer-ai` is an implementation of 🔻 [giza](https://github.com/kamangir/giza) and a language [to speak AI](https://github.com/kamangir/kamangir).
+
+![image](https://github.com/kamangir/assets/blob/main/awesome-bash-cli/marquee-2024-10-26.jpg?raw=true)
+
+# dev install
+
+- [Amazon EC2 instances](./bluer_ai/docs/ec2.md)
+- [Amazon SageMaker](./bluer_ai/docs/SageMaker.md)
+- [Jetson Nano](./bluer_ai/docs/Jetson-Nano.md)
+- [macOS](./bluer_ai/docs/macOS.md)
+- [Raspberry Pi](./bluer_ai/docs/RPi.md)
+
+# release install
+
+not recommended.
+
+```bash
+pip install bluer_ai
+```
+
+---
+
+> 🪄 [`abcli`](https://github.com/kamangir/awesome-bash-cli) for the [Global South](https://github.com/kamangir/bluer-south).
+
+---
+
+
+[![pylint](https://github.com/kamangir/bluer-ai/actions/workflows/pylint.yml/badge.svg)](https://github.com/kamangir/bluer-ai/actions/workflows/pylint.yml) [![pytest](https://github.com/kamangir/bluer-ai/actions/workflows/pytest.yml/badge.svg)](https://github.com/kamangir/bluer-ai/actions/workflows/pytest.yml) [![bashtest](https://github.com/kamangir/bluer-ai/actions/workflows/bashtest.yml/badge.svg)](https://github.com/kamangir/bluer-ai/actions/workflows/bashtest.yml) [![PyPI version](https://img.shields.io/pypi/v/bluer_ai.svg)](https://pypi.org/project/bluer_ai/) [![PyPI - Downloads](https://img.shields.io/pypi/dd/bluer_ai)](https://pypistats.org/packages/bluer_ai)
+
+built by 🌀 [`bluer README`](https://github.com/kamangir/bluer-objects/tree/main/bluer_objects/README), based on 🌀 [`bluer_ai-12.87.1`](https://github.com/kamangir/bluer-ai).
---
+++ packages/bluer-ai/bluer_ai-12.87.1.tar.gz/bluer_ai-12.87.1/bluer_ai/tests/test_plugins.py
@@ -0,0 +1,87 @@
+import pytest
+from bluer_ai import env
+from bluer_ai.plugins.functions import (
+    list_of_external,
+    list_of_installed,
+    get_plugin_name,
+    get_module_name,
+)
+
+
+@pytest.mark.parametrize(
+    ["repo_names"],
+    [
+        [True],
+        [False],
+    ],
+)
+def test_list_of_external(repo_names: bool):
+    assert isinstance(list_of_external(repo_names), list)
+
+
+@pytest.mark.parametrize(
+    ["return_path"],
+    [
+        [True],
+        [False],
+    ],
+)
+def test_list_of_installed(return_path: bool):
+    assert isinstance(list_of_installed(return_path), list)
+
+
+@pytest.mark.skipif(
+    env.abcli_is_github_workflow == "true",
+    reason="plugins are not present in the github workflow.",
+)
+@pytest.mark.parametrize(
+    [
+        "repo_name",
+        "plugin_name",
+        "module_name",
+    ],
+    [
+        [
+            "awesome-bash-cli",
+            "abcli",
+            "abcli",
+        ],
+        [
+            "abadpour",
+            "abadpour",
+            "abadpour",
+        ],
+        [
+            "roofai",
+            "roofai",
+            "roofai",
+        ],
+        [
+            "vancouver-watching",
+            "vancouver_watching",
+            "vancouver_watching",
+        ],
+        [
+            "giza",
+            "giza",
+            "gizai",
+        ],
+        [
+            "hubble",
+            "hubble",
+            "hubblescope",
+        ],
+        [
+            "aiart",
+            "aiart",
+            "articraft",
+        ],
+    ],
+)
+def test_get(
+    repo_name: str,
+    plugin_name: str,
+    module_name: str,
+):
+    assert get_plugin_name(repo_name) == plugin_name
+    assert get_module_name(repo_name) == module_name
