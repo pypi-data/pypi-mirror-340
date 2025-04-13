686bf5d5a787840e4734981d46a51fb41cf0f541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:09 +0000

    Add package ab23007uno/0.1.0/ab23007uno-0.1.0-py3-none-any.whl

---
+++ packages/ab23007uno/ab23007uno-0.1.0-py3-none-any.whl/ab23007uno/__init__.py
@@ -0,0 +1,7 @@
+from .bisection import bisection
+from .gauss_elimination import gauss_elimination
+from .gauss_jordan import gauss_jordan
+from .gauss_seidel import gauss_seidel
+from .jacobi import jacobi
+from .lu_decomposition import lu_decomposition
+
