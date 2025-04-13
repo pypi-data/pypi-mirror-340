a2cc13da564f809b9eb3b06998379c54179fafdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:57:21 +0000

    Add package pydatamorph/1.0.5/pydatamorph-1.0.5.tar.gz

---
+++ packages/pydatamorph/pydatamorph-1.0.5.tar.gz/pydatamorph-1.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pydatamorph',
+    version='1.0.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "streamlit>=1.0",
+        "pyyaml>=6.0"
+    ],
+    author='Chockalingam Subramanian',
+    description='Adaptive LLM-native data pipeline orchestrator',
+    entry_points={
+        'console_scripts': [
+            'datamorph=datamorph.core.runner:main'
+        ]
+    },
+    python_requires='>=3.7',
+)
+
