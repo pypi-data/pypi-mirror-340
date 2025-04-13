98bbbc750568851ad8bb6372023beaaaee4d0399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:44 +0000

    Add package LR20029UNO/1.0.0/lr20029uno-1.0.0.tar.gz

---
+++ packages/LR20029UNO/lr20029uno-1.0.0.tar.gz/lr20029uno-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LR20029UNO',  
+    version='1.0.0',  # 
+    description='Librería para resolver sistemas de ecuaciones lineales y no lineales',
+    long_description=open('README.md').read(),  # Lee la documentación desde el README
+    long_description_content_type='text/markdown',
+    author='Balmore',
+    author_email='LR20029@ues.edu.sv',
+    packages=find_packages(),  # Encuentra automáticamente los paquetes en la estructura
+    install_requires=[
+        'numpy',  # Dependencias necesarias
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Versión mínima de Python
+)
