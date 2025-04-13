84c7958dc0933a25804bd26d2ad6ecb09ee3f3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:15 +0000

    Add package hio/0.7.6/hio-0.7.6.tar.gz

---
+++ packages/hio/hio-0.7.6.tar.gz/hio-0.7.6/src/hio/base/hier/__init__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+hio.base.hier Package
+"""
+from .hiering import Context, WorkDom, actify, register, ActBase
+from .boxing import Box, Boxer, Maker
+from .acting import Act, Tract, EndAct
+from .needing import Need
+from .bagging import BagDom, Bag
