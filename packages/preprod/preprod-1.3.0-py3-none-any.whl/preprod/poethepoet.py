27ea101d8c59bf49b31b3b02d275649abe2a08d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:06:45 +0000

    Add package preprod/1.3.0/preprod-1.3.0-py3-none-any.whl

---
+++ packages/preprod/preprod-1.3.0-py3-none-any.whl/preprod/poethepoet.py
@@ -0,0 +1,69 @@
+from preprod import __version__
+from os import system
+
+def release():
+    print("""Nueva versión:
+  * Cambiar la version en pyproject.toml
+  * Cambiar la versión y la fecha en __init__.py
+  * Editar README.md to add CHANGELOG
+  * Ejecutar otra vez poe release
+  * git checkout -b preprod-{0}
+  * poe doc
+  * poe translate
+  * nano preprod/locale/es.po
+  * poe translate
+  * poe coverage
+  * git commit -a -m 'preprod-{0}'
+  * git push --set-upstream origin preprod-{0}
+  * Hacer un pull request con los cambios a main
+  * Hacer un nuevo tag en GitHub
+  * git checkout main
+  * git pull
+  * poetry build
+  * poetry publish
+  * Crea un nuevo ebuild de preprod en Gentoo con la nueva versión
+  * Subelo al repositorio myportage
+
+""".format(__version__))
+
+    
+def coverage():
+    system("coverage run --omit='*repository_commons.py' -m pytest && coverage report && coverage html")
+
+
+def translate():
+    #es
+    system("xgettext -L Python --no-wrap --no-location --from-code='UTF-8' -o preprod/locale/preprod.pot preprod/*.py")
+    system("msgmerge -N --no-wrap -U preprod/locale/es.po preprod/locale/preprod.pot")
+    system("msgfmt -cv -o preprod/locale/es/LC_MESSAGES/preprod.mo preprod/locale/es.po")
+    system("msgfmt -cv -o preprod/locale/en/LC_MESSAGES/preprod.mo preprod/locale/en.po")
+    
+def pytest():
+    system("pytest")
+
+def doc():
+    import io
+    import sys
+    import preprod.commons
+    # Replace 'module_name' with the actual module name you want to get help for
+
+    # Create a string buffer to capture the help output
+    buffer = io.StringIO()
+
+    # Redirect stdout to the buffer
+    sys.stdout = buffer
+
+    # Call the help function on the module
+    help(preprod.commons)
+
+    # Reset stdout to default
+    sys.stdout = sys.__stdout__
+
+    # Get the help output from the buffer
+    help_output = buffer.getvalue()
+
+    # Write the help output to a Markdown file
+    with open('doc/PREPROD_COMMANDS.md', 'w') as f:
+        f.write(f'```text\n{help_output}\n```')
+
+    print("Help output saved to doc/PREPROD_COMMANDS.md")
---
+++ packages/preprod/preprod-1.3.0-py3-none-any.whl/preprod/tests/test_commons.py
@@ -0,0 +1,218 @@
+from inspect import currentframe
+from preprod import commons, core
+from os import system, path
+from pytest import raises, fixture
+from shutil import which
+
+project_test_path=f"{commons.repository_path()}test"
+tmp_path="/tmp/preprod_tests"
+
+
+@fixture(scope='session', autouse=True)
+def setup_and_teardown():
+    # Code to run at the beginning
+    print("Creating preprod test project!")
+    system(f"rm -Rf {tmp_path}")
+    system(f"mkdir -p {project_test_path}")
+    # Anything you need to initialize
+    resource = "alltests"
+
+    yield resource  # This allows all tests to run with the resource available
+
+    # Code to run at the end
+    system(f"mv {project_test_path}/ {tmp_path}")
+    print("Removing preprod test project")
+    system(f"rm -Rf {project_test_path}")
+
+    
+def create_and_run_action(func_name,  code_):
+    """
+        Creates a action in repository called test/func_name
+        Creates a /tmp/ directory exclusive for this test
+        Chdir in this directory
+    """
+    tmp_test_path=f"{tmp_path}/{func_name}/"
+    with open(f"{project_test_path}/{func_name}", "w") as f:
+        f.write(f"""
+preprod_commons.makedirs("{tmp_test_path}")
+preprod_commons.chdir("{tmp_test_path}")
+{code_}
+""")
+    core.main(["test", func_name])
+    return tmp_test_path
+    
+def test_commons_create_python_virtual_env():
+    tmp_test_path=create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_python_virtual_env(python_version_name="python3.11", system_site_packages=False)
+preprod_commons.create_python_virtual_env(python_version_name="python3.12", system_site_packages=True)
+    """)
+    if which("python3.11"):
+        assert path.exists(f"{tmp_test_path}/.python3.11/bin/python3.11")
+        assert commons.file_contains_string(f"{tmp_test_path}/.python3.11/pyvenv.cfg",  "include-system-site-packages = false")
+    if which("python3.12"):
+        assert path.exists(f"{tmp_test_path}/.python3.12/bin/python3.12")
+        assert commons.file_contains_string(f"{tmp_test_path}/.python3.12/pyvenv.cfg",  "include-system-site-packages = true")
+    
+def test_commons_rmtree():
+    tmp_test_path=create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.makedirs("to_delete")
+preprod_commons.rmtree("to_delete")
+    """)
+    assert not path.exists(f"{tmp_test_path}/to_delete/")
+    
+def test_commons_nmcli_net_change():
+    pass
+#    create_and_run_action(currentframe().f_code.co_name,  """
+#preprod_commons.nmcli_net_change("Edfdthernet", "www.ddlaskdfla.com",80)
+#preprod_commons.nmcli_net_change("Ethernet", "www.gentoo.com",80)
+#preprod_commons.nmcli_net_change("Ethernet", "www.ddlaskdfla.com",80)
+#    """)
+#    assert False
+
+    
+    
+    
+    
+def test_commons_git_clone():
+    tmp_test_path=create_and_run_action(currentframe().f_code.co_name,  """
+    
+preprod_commons.git_clone("https://github.com/turulomio/preprod")
+preprod_commons.git_clone("https://github.com/turulomio/preprod", "preprod2")
+preprod_commons.git_clone("https://github.com/turulomio/preprod", "preprod3", "main")
+preprod_commons.git_clone("https://github.com/turulomio/pydicts", "", "main")
+    """)
+    assert path.exists(f"{tmp_test_path}/preprod/.git/")
+    assert path.exists(f"{tmp_test_path}/preprod2/.git/")
+    assert path.exists(f"{tmp_test_path}/preprod3/.git/")
+    assert path.exists(f"{tmp_test_path}/pydicts/.git/")
+    
+def test_commons_git_pull():
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.git_clone("https://github.com/turulomio/preprod")
+preprod_commons.chdir("preprod")
+preprod_commons.git_pull()
+    """)    
+    
+def test_commons_run_and_check():
+    
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.run_and_check("pwd")
+    """)
+    
+def test_commons_system():
+    
+    function_name=currentframe().f_code.co_name
+    create_and_run_action(function_name,  f"""
+preprod_commons.chdir("{tmp_path}")
+preprod_commons.system("pwd > {tmp_path}/{function_name}/system")
+    """)
+
+def test_commons_chown_recursive():
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_a_file("hello", "")
+preprod_commons.chown_recursive("hello")
+    """)
+    
+def test_commons_chmod_recursive():
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_a_file("hello", "")
+preprod_commons.chmod_recursive("hello")
+    """)
+    
+
+def test_commons_replace_in_file():   
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_a_file("hello.txt", "hello")
+preprod_commons.replace_in_file("hello.txt", "hello", "bye")
+    """)
+    assert commons.file_contains_string("hello.txt",  "bye")
+    
+def test_commons_lines_at_the_end_of_file():   
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_a_file("numbers.txt", "one\\n")
+preprod_commons.lines_at_the_end_of_file("numbers.txt", "two")
+    """)
+    assert commons.file_contains_string("numbers.txt",  "two")
+    
+def test_commons_insert_at_line():   
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_a_file("numbers.txt", "one\\nthree")
+preprod_commons.insert_at_line("numbers.txt", 2, "two")
+    """)
+    
+    with open("numbers.txt",  "r") as f:
+        line=f.readlines()
+        assert "two" in line[1]
+
+def test_commons_kill_from_ps_aux():   
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.system("sleep 4 &")
+preprod_commons.kill_from_ps_aux("sleep")
+preprod_commons.kill_from_ps_aux("sleeping")
+    """)
+
+def test_commons_delete_line_in_file():   
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_a_file("numbers.txt", "one\\ntwo\\nthree")
+preprod_commons.delete_line_in_file("numbers.txt", 2,)
+    """)
+    assert not commons.file_contains_string("numbers.txt",  "two")
+    
+def test_commons_copyfile():   
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_a_file("hello.txt", "")
+preprod_commons.copyfile("hello.txt", "bye.txt")
+    """)
+    assert path.exists("hello.txt")
+    assert path.exists("bye.txt")     
+    
+def test_commons_rm():   
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_a_file("hello.txt","")
+preprod_commons.rm("hello.txt")
+    """)
+    assert not path.exists("hello.txt")
+    
+    
+def test_commons_rsync():   
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.create_a_file("hello.txt", "")
+preprod_commons.rsync("hello.txt", "bye.txt")
+    """)
+    assert path.exists("hello.txt")
+    assert path.exists("bye.txt")
+        
+
+def test_commons_poetry_install():
+    create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.git_clone("https://github.com/turulomio/django_calories_tracker")
+preprod_commons.chdir("django_calories_tracker")
+preprod_commons.poetry_install()
+    """)
+
+def test_commons_npm_install():
+    tmp_test_path=create_and_run_action(currentframe().f_code.co_name,  """
+preprod_commons.git_clone("https://github.com/turulomio/calories_tracker")
+preprod_commons.chdir("calories_tracker")
+preprod_commons.npm_install()
+    """)
+    assert path.exists(f"{tmp_test_path}/calories_tracker/node_modules/")
+
+    
+def test_list():
+    with raises(SystemExit):
+        core.main([])
+    
+    with raises(SystemExit):
+        core.main(["test2"])
+    with raises(SystemExit):
+        core.main(["test", "test2"])
+        
+    core.main(['test', 'test_commons_run_and_check', '--pretend'])
+
+    
+def test_list_repository():
+    core.list_repository()
+
+
+    
