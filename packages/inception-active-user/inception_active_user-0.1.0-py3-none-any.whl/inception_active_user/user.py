1238f2dece6382471cea6f71b4b5da7995e7b5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:57:39 +0000

    Add package inception-active-user/0.1.0/inception_active_user-0.1.0-py3-none-any.whl

---
+++ packages/inception-active-user/inception_active_user-0.1.0-py3-none-any.whl/inception_active_user/user.py
@@ -0,0 +1,37 @@
+import aiohttp
+from inception_active_user.settings_usr import get_active_user_setting
+
+# Get settings
+user_setting = get_active_user_setting()
+
+async def fetch_user_info(authorization_data: str,url:str):
+    """
+    Helper function to fetch current active user info from the API.
+    """
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                url,
+                headers={"Authorization": authorization_data},
+            ) as response:
+                if response.ok:
+                    return await response.json()
+                else:
+                    error_detail = await response.json()
+                    raise Exception(
+                        f"Request failed with status {response.status}: {error_detail}"
+                    )
+    except Exception as e:
+        print(f"[ERROR] Failed to fetch user info: {e}")
+        raise Exception({
+            "type": "error",
+            "content": str(e),
+        })
+
+
+async def get_current_active_user(authorization_data: str):
+    return await fetch_user_info(authorization_data, f"{user_setting.url}/api/v1/users/me/minify")
+
+
+async def get_current_active_user_more_info(authorization_data: str):
+    return await fetch_user_info(authorization_data, f"{user_setting.url}/api/v1/users/me")
