610c88d77756716cc31ae288ac0c676095167d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:06:45 +0000

    Add package maleo-core/0.3.90/maleo_core-0.3.90-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.3.90-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_soapie/transfers/general/objective.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from pydantic import BaseModel, Field
+from typing import Optional
+from maleo_core.models.maleo_suite.maleo_soapie.transfers.general.vital_sign import MaleoSOAPIEVitalSignGeneralTransfers
+
+class MaleoSOAPIEObjectiveGeneralTransfers:
+    class ObjectiveIDs(BaseModel):
+        objective_ids:Optional[list[int]] = Field(None, description="Specific Objective IDs")
+
+    class Base(BaseModel):
+        soapie_id:int = Field(..., ge=1, description="SOAPIE's id")
+        overall:str = Field(..., description="Overall objective")
+        vital_sign:Optional[MaleoSOAPIEVitalSignGeneralTransfers.Base] = Field(None, description="Vital Sign")
+        other_information:Optional[str] = Field(None, description="Other information")
---
+++ packages/maleo-core/maleo_core-0.3.90-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_soapie/transfers/parameters/general/soapie.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from enum import StrEnum
+from pydantic import BaseModel, Field
+from maleo_core.models.base.transfers.parameters.general import BaseGeneralParameters
+from maleo_core.models.maleo_suite.maleo_soapie.transfers.general.soapie import MaleoSOAPIESOAPIEGeneralTransfers
+
+class MaleoSOAPIESOAPIEGeneralParameters:
+    class ExpandableFields(StrEnum):
+        SUBJECTIVE = "subjective"
+        OBJECTIVE = "objective"
+        VITAL_SIGN = "objective.vital_sign"
+        ASSESSMENT = "assessment"
+        DIAGNOSES = "assessment.diagnoses"
+        PLAN = "plan"
+        INTERVENTION = "intervention"
+        EVALUATION = "evaluation"
+
+    class Expand(BaseModel):
+        expand:list[MaleoSOAPIESOAPIEGeneralParameters.ExpandableFields] = Field([], description="Expanded field(s)")
+
+    class UniqueIdentifiers(StrEnum):
+        ID = "id"
+        UUID = "uuid"
+
+    class GetSingle(Expand, BaseGeneralParameters.GetSingle):
+        identifier:MaleoSOAPIESOAPIEGeneralParameters.UniqueIdentifiers = Field(..., description="Identifier")
+
+    class CreateOrUpdate(Expand, MaleoSOAPIESOAPIEGeneralTransfers.Base): pass
+
+MaleoSOAPIESOAPIEGeneralParameters.Expand.model_rebuild()
