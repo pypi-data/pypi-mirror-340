5fb527926e3312c82cae9478695a800b92438b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:56:49 +0000

    Add package myadder1/0.1.0/myadder1-0.1.0.tar.gz

---
+++ packages/myadder1/myadder1-0.1.0.tar.gz/myadder1-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myadder1',                     # Your package name
+    version='0.1.0',                    # Version
+    description='A simple addition module that sums a list of numbers',
+    author='Aryan Bhagwat',
+    author_email='legendasur531@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
