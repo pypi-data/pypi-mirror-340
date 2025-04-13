1be47d6dfcc46a1e8578f2a26da017e30a22878b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:06:21 +0000

    Add package MeUtils/2025.4.13.14.49.36/MeUtils-2025.4.13.14.49.36-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2025.4.13.14.49.36-py3-none-any.whl/examples/_openaisdk/openai_siliconflow.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : openai_siliconflow
+# @Time         : 2024/6/26 10:42
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.getenv("SILICONFLOW_API_KEY"),
+    # api_key="sk-zhvfnyebjcrylrwwzecxcbolwdsitqfwjqzxeysqhprdsjbm",
+    base_url="https://api.siliconflow.cn/v1",
+)
+
+# print(client.models.list())
+
+
+model = "01-ai/Yi-1.5-6B-Chat"
+# model = "THUDM/glm-4-9b-chat"
+# model = "google/gemma-2-9b-it"
+# model = "internlm/internlm2_5-7b-chat"
+# model = "google/gemma-2-27b-it"
+# model = 'chat-kolors'
+# model = "deepseek-ai/DeepSeek-V2-Chat"
+# model = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+# model = "meta-llama/Meta-Llama-3.1-70B-Instruct"
+# model = 'meta-llama/Meta-Llama-3.1-405B-Instruct'
+# model = "deepseek-ai/DeepSeek-V2.5"
+model = "deepseek-ai/DeepSeek-R1"
+
+
+# api black-forest-labs/FLUX.1-schnell
+messages = [
+    {'role': 'user', 'content': '详细说明 9.8 9.11那个大'}
+]
+response = client.chat.completions.create(
+    # model='alibaba/Qwen1.5-110B-Chat',
+    model=model,
+    # messages=[
+    #     {'role': 'user', 'content': "抛砖引玉是什么意思呀"}
+    # ],
+    messages=messages,
+    stream=False,
+    max_tokens=1,
+)
+print(response)
+for chunk in response:
+    print(chunk.choices[0].delta.content)
