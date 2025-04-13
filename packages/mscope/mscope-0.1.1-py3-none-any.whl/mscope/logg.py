2eacc0b48babf7e8f61aef929eaf6acefd2e7c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:06:39 +0000

    Add package mscope/0.1.1/mscope-0.1.1-py3-none-any.whl

---
+++ packages/mscope/mscope-0.1.1-py3-none-any.whl/mscope/logg.py
@@ -0,0 +1,11 @@
+def print_banner():
+    banner = """
+ _  _  ____   ___  __  ____  ____ 
+( \/ )/ ___) / __)/  \(  _ \(  __)
+/ \/ ||___ \( (__(  O )) __/ ) _) 
+\_)(_/(____/ \___)\__/(__)  (____)
+                    logic by: oakwide (git,tg)
+                    git: https://github.com/oakwide/mscope
+                    domains.txt / paterns.txt / hints.txt
+    """
+    print(banner)
