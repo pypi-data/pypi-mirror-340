07bf8501c5c30b5e7e4cf253f69ba27206a36d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:05:29 +0000

    Add package maleo-core/0.3.96/maleo_core-0.3.96.tar.gz

---
+++ packages/maleo-core/maleo_core-0.3.96.tar.gz/maleo_core-0.3.96/maleo_core/models/maleo_suite/maleo_soapie/transfers/general/objective.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from pydantic import BaseModel, Field
+from typing import Optional
+from maleo_core.models.maleo_suite.maleo_soapie.transfers.general.vital_sign import MaleoSOAPIEVitalSignGeneralTransfers
+
+class MaleoSOAPIEObjectiveGeneralTransfers:
+    class ObjectiveID(BaseModel):
+        objective_id:int = Field(..., ge=1, description="Objective's id")
+
+    class ObjectiveIDs(BaseModel):
+        objective_ids:Optional[list[int]] = Field(None, description="Specific Objective IDs")
+
+    class Base(BaseModel):
+        overall:str = Field(..., description="Overall objective")
+        vital_sign:Optional[MaleoSOAPIEVitalSignGeneralTransfers.Base] = Field(None, description="Vital Sign")
+        other_information:Optional[str] = Field(None, description="Other information")
