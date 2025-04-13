77d285e716cd527eb2fb135ce83e217a40d8ecf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:15 +0000

    Add package CM23137UNO/0.3/cm23137uno-0.3-py3-none-any.whl

---
+++ packages/CM23137UNO/cm23137uno-0.3-py3-none-any.whl/CM23137UNO/__init__.py
@@ -0,0 +1,12 @@
+from .lineales import (
+    gauss_elimination,
+    cramer,
+    gauss_jordan,
+    descomposition_lu,
+    jacobi,
+    gauss_seidel
+)
+
+from .no_lineales import (
+    bisection
+)
