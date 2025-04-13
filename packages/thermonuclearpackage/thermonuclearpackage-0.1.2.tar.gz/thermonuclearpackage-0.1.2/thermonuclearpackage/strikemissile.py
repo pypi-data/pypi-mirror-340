4f8effdb1214295b471873e1669a00206211f231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 03:56:46 +0000

    Add package thermonuclearpackage/0.1.2/thermonuclearpackage-0.1.2.tar.gz

---
+++ packages/thermonuclearpackage/thermonuclearpackage-0.1.2.tar.gz/thermonuclearpackage-0.1.2/thermonuclearpackage/strikemissile.py
@@ -0,0 +1,18 @@
+class nation:
+    def __init__(self, name, population, nukes):
+        self.name = name
+        self.population = population
+        self.nukes = nukes
+
+    def __str__(self):
+        return f'{self.name}, {self.population}, {self.nukes}'
+
+    def strike(self, target, number):
+        print(f'Initiating strike on {target.name}...')
+        print(f'{target.name} initial population: {target.population}')
+        print(f'{target.name} initial nukes: {target.nukes}')
+        self.nukes -= number
+        target.population *= 0.9**number
+        target.nukes += 5
+        print(f'{target.name} final population: {target.population}')
+        print(f'{target.name} final nukes: {target.nukes}')
