c77619ca401e1d3e3b85a34f47ca1d69802bf9a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:05:36 +0000

    Add package ezencode/0.5/ezencode-0.5-py3-none-any.whl

---
+++ packages/ezencode/ezencode-0.5-py3-none-any.whl/ezencode/decode.py
@@ -0,0 +1,45 @@
+"""EZ Encode decoder module."""
+
+from typing import Generator
+from . import maps
+
+
+class Decode(object):
+    """Translate encoded text to its corresponding ASCII and Extended ASCII representations."""
+    __slots__ = frozenset({})
+
+    def __new__(cls: type['Decode'], text: str, /) -> 'Decode':
+        assert isinstance(text, str), f"'{text}' must be of type 'str'"
+        if len(text) == 0: raise SystemExit(1)
+        cls.__text = text
+        return super(Decode, cls).__new__(cls)
+
+    @classmethod
+    def character_stream(cls: type['Decode'], /) -> Generator[str | int]:
+        start, size, length = 0, 2, len(cls.__text)
+        while start < length:
+            symbols = cls.__text[start: size]
+            yield maps.Maps.generate_map(5)[symbols]
+            start += 2
+            size += 2
+
+    @classmethod
+    def binary_stream(cls: type['Decode'], /) -> Generator[str | int]:
+        for characters in cls.character_stream():
+            try:
+                yield maps.Maps.generate_map(2)[characters]
+            except KeyError:
+                raise Exception(f"'{characters}' are not SEA encoded")
+
+    @classmethod
+    def ascii_stream(cls: type['Decode'], /) -> Generator[str | int]:
+        for binary_string in cls.binary_stream():
+            try:
+                yield maps.Maps.generate_map(1)[binary_string]
+            except KeyError:
+                raise Exception(f"Cannot encode character; '{binary_string}'")
+
+    @classmethod
+    def decoded(cls: type['Decode'], /) -> str:
+        decoded = ''.join([ chr(number) for number in cls.ascii_stream() ])
+        return decoded
---
+++ packages/ezencode/ezencode-0.5-py3-none-any.whl/ezencode/encode.py
@@ -0,0 +1,42 @@
+"""EZ Encode encoder module."""
+
+from typing import Generator
+from . import maps
+
+
+class Encode(object):
+    """Translate text to its corresponding EZ Encode code point."""
+    __slots__ = frozenset({})
+
+    def __new__(cls: type['Encode'], text: str, /) -> 'Encode':
+        assert isinstance(text, str), f"'{text}' must be of type 'str'"
+        if len(text) == 0: raise SystemExit(1)
+        cls.__text = text
+        return super(Encode, cls).__new__(cls)
+
+    @classmethod
+    def character_stream(cls: type['Encode'], /) -> Generator[str]:
+        for character in cls.__text:
+            yield character
+
+    @classmethod
+    def binary_stream(cls: type['Encode'], /) -> Generator[str | int]:
+        for character in cls.character_stream():
+            try:
+                yield maps.Maps.generate_map(0)[ord(character)]
+            except KeyError:
+                raise Exception(f"Cannot encode character; '{character}'")
+
+    @classmethod
+    def symbol_stream(cls: type['Encode'], /) -> Generator[str | int]:
+        for binary_string in cls.binary_stream():
+            try:
+                layer_1 = maps.Maps.generate_map(3)[binary_string]
+                yield maps.Maps.generate_map(4)[layer_1]
+            except KeyError:
+                raise Exception(f"Cannot encode character; '{binary_string}'")
+
+    @classmethod
+    def encoded(cls: type['Encode'], /) -> str:
+        encoded = ''.join([ symbol for symbol in cls.symbol_stream() ])
+        return encoded
