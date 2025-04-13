0537ac699f4bd39deb982c100af9ae7a12e20e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:54 +0000

    Add package json-explore/1.2.0/json_explore-1.2.0.tar.gz

---
+++ packages/json-explore/json_explore-1.2.0.tar.gz/json_explore-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="json-explore",
+    version="1.2.0",
+    author="Matthew Raburn",
+    description="A package for searching through JSONs via CLI",
+    packages=find_packages(),
+    py_modules=["json_cli"],  # The name of the Python file without .py
+    install_requires=[],  # Add dependencies here if needed
+    entry_points={
+        "console_scripts": [
+            "json-explore=json_cli:json_explore",  # CLI command
+        ],
+    },
+    python_requires='>=3.6',
+)
