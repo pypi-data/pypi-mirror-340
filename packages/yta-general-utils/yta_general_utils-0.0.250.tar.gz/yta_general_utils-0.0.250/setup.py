f89a085a5d26e7f23fdb905edf278dd129e89381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 14:05:21 +0000

    Add package yta-general-utils/0.0.250/yta_general_utils-0.0.250.tar.gz

---
+++ packages/yta-general-utils/yta_general_utils-0.0.250.tar.gz/yta_general_utils-0.0.250/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.250'
+DESCRIPTION = 'Youtube Autónomo general utils are here.'
+LONG_DESCRIPTION = 'These are the general utils we need in the Youtube Autónomo project to work in a better way.'
+
+setup(
+    name = "yta_general_utils", 
+    version = VERSION,
+    author = "Daniel Alcalá",
+    author_email = "<danielalcalavalera@gmail.com>",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    packages = find_packages(),
+    install_requires = [
+        'moviepy',
+        'pillow',
+        'backgroundremover',
+        'scikit-image',
+        'opencv-python',
+        'numpy',
+        'pyttsx3',
+        'gtts',
+        'pydub',
+        'torch',
+        'torchaudio',
+        'deepfilternet',
+        'exrex',
+        'validators',
+        'lxml', # 5.2.2
+        'selenium', # 4.18.1,
+        'python-dotenv'
+    ],
+    
+    keywords = [
+        'youtube autonomo general utils'
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
