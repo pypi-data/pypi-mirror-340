66fe14d343e119af2c988234ae96d8f04897f6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 04:32:42 +0000

    Add package aqgo/0.1.2/aqgo-0.1.2.tar.gz

---
+++ packages/aqgo/aqgo-0.1.2.tar.gz/aqgo-0.1.2/aletheia_quantum_genetic_optimizer/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.2"
+from aletheia_quantum_genetic_optimizer.individuals import Individual
+from aletheia_quantum_genetic_optimizers.bounds import BoundCreator
+from aletheia_quantum_genetic_optimizers.genetic_optimizer import GenethicOptimizer
