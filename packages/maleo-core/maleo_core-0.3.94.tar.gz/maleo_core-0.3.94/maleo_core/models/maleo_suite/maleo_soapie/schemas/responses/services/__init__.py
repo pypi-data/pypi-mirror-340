d0f4c48635e9e3b091b8612f7e6bfd9cc39f5e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-13 15:05:18 +0000

    Add package maleo-core/0.3.94/maleo_core-0.3.94.tar.gz

---
+++ packages/maleo-core/maleo_core-0.3.94.tar.gz/maleo_core-0.3.94/maleo_core/models/maleo_suite/maleo_soapie/schemas/responses/services/__init__.py
@@ -0,0 +1,9 @@
+# This file serves all MaleoSOAPIE's services responses schemas
+
+from __future__ import annotations
+
+class MaleoSOAPIEServicesResponsesSchemas: pass
+
+__all__ = [
+    "MaleoSOAPIEServicesResponsesSchemas",
+]
