a93845366e39f02c8984904f56fc794f18296182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 14:05:31 +0000

    Add package MM22025UNO/0.1.5/MM22025UNO-0.1.5-py3-none-any.whl

---
+++ packages/MM22025UNO/MM22025UNO-0.1.5-py3-none-any.whl/MM22025UNO/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.1.5"
+
+from .lineales import (
+    eliminacion_gauss,
+    gauss_jordan,
+    cramer,
+    descomposicion_lu,
+    jacobi,
+    gauss_seidel
+)
+
+from .no_lineales import resolver_biseccion
+
+__all__ = [
+    "eliminacion_gauss",
+    "gauss_jordan",
+    "cramer",
+    "descomposicion_lu",
+    "jacobi",
+    "gauss_seidel",
+    "resolver_biseccion"
+]
---
+++ packages/MM22025UNO/MM22025UNO-0.1.5-py3-none-any.whl/MM22025UNO/no_lineales.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy.optimize import fsolve
+
+def resolver_biseccion(f, a, b, tol=1e-10, max_iter=1000):
+    """
+    Encuentra una raíz de f en el intervalo [a, b] usando el método de bisección.
+    """
+    if f(a) * f(b) >= 0:
+        raise ValueError("f(a) y f(b) deben tener signos opuestos.")
+    for _ in range(max_iter):
+        c = (a + b) / 2
+        if abs(f(c)) < tol or (b - a) / 2 < tol:
+            return c
+        if f(c) * f(a) < 0:
+            b = c
+        else:
+            a = c
+    raise ValueError("El método de bisección no convergió.")
