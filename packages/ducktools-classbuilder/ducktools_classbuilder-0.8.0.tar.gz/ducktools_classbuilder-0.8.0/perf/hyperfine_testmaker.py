09fd828faf04bed3f768fb93b12bca75e294f7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:20 +0000

    Add package ducktools-classbuilder/0.8.0/ducktools_classbuilder-0.8.0.tar.gz

---
+++ packages/ducktools-classbuilder/ducktools_classbuilder-0.8.0.tar.gz/ducktools_classbuilder-0.8.0/perf/hyperfine_testmaker.py
@@ -0,0 +1,311 @@
+"""
+This script makes all of the hyperfine tests used in this project
+"""
+import sys
+from pathlib import Path
+from ducktools.classbuilder.prefab import prefab
+
+base_dir = Path(__file__).parent / 'hyperfine_testfiles'
+importer_dir = base_dir / 'hyperfine_importers'
+classdef_dir = importer_dir / 'class_definitions'
+results_dir = base_dir / 'hyperfine_results'
+
+# Template Body Files #
+
+standard_template = '''
+class C{n}:
+    def __init__(self, a, b, c, d, e):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.e = e
+
+    def __repr__(self):
+        return f'C{n}({{self.a!r}}, {{self.b!r}}, {{self.c!r}}, {{self.d!r}}, {{self.e!r}})'
+
+    def __eq__(self, other):
+        if self.__class__ is other.__class__:
+            return (self.a, self.b, self.c, self.d, self.e) == (other.a, other.b, other.c, other.d, other.e)
+        else:
+            return NotImplemented
+
+'''
+
+namedtuple_template = '''
+C{n} = namedtuple('C{n}', ['a', 'b', 'c', 'd', 'e'])
+'''
+
+NamedTuple_template = '''
+class C{n}(NamedTuple):
+    a : int
+    b : int
+    c : int
+    d : int
+    e : int
+'''
+
+dataclass_template = '''
+@dataclass
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+attr_noslots_template = '''
+@define(slots=False)
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+attr_slots_template = '''
+@define(slots=True)
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+pydantic_template = '''
+class C{n}(BaseModel):
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+cluegen_template = '''
+class C{n}(Datum):
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+# cluegen, but same default methods as dataclasses generated
+cluegen_eval_template = '''
+class C{n}(Datum):
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+dataklass_template = '''
+@dataklass
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+dataklass_eval_template = '''
+@dataklass
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+slotclass_template = '''
+@slotclass
+class C{n}:
+    __slots__ = SlotFields(
+        a=Field(),
+        b=Field(),
+        c=Field(),
+        d=Field(),
+        e=Field(),
+    )
+'''
+
+prefab_template = '''
+@prefab
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+prefab_attribute_template = '''
+@prefab
+class C{n}:
+    a = attribute()
+    b = attribute()
+    c = attribute()
+    d = attribute()
+    e = attribute()
+'''
+
+prefab_slots_template = '''
+@prefab
+class C{n}:
+    __slots__ = SlotFields(
+        a=attribute(),
+        b=attribute(),
+        c=attribute(),
+        d=attribute(),
+        e=attribute(),
+    )
+'''
+
+prefab_eval_template = '''
+@prefab
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+
+# Import Headings #
+
+namedtuple_header = "from collections import namedtuple"
+NamedTuple_header = "from typing import NamedTuple"
+dataclass_header = "from dataclasses import dataclass"
+attr_header = "from attrs import define"
+pydantic_header = "from pydantic import BaseModel"
+cluegen_header = "from cluegen import Datum"
+dataklass_header = "from dataklasses import dataklass"
+slotclass_header = "from ducktools.classbuilder import slotclass, SlotFields, Field"
+prefab_header = "from ducktools.classbuilder.prefab import prefab, attribute, SlotFields"
+
+
+def write_perf_file(outpath, count, template, setup):
+    with open(outpath, 'w') as f:
+        f.write(setup)
+        for n in range(count):
+            f.write(template.format(n=n))
+
+
+@prefab
+class TestData:
+    import_name: str
+    import_header: str
+    class_template: str
+
+    @property
+    def importer_file(self):
+        importer_name = f"{self.import_name}_timer.py"
+        return importer_dir / importer_name
+
+    @property
+    def def_file(self):
+        def_name = f"{self.import_name}_data.py"
+        return classdef_dir / def_name
+
+    def write_perf_importer(self):
+        data = f"import class_definitions.{self.import_name}_data\n"
+        self.importer_file.write_text(data)
+
+    def write_classdef_file(self, count=100):
+        with self.def_file.open(mode='w') as f:
+            f.write(self.import_header)
+            for n in range(count):
+                f.write(self.class_template.format(n=n))
+
+
+datasets = [
+    TestData('native_classes', '', standard_template),
+    TestData('slotclasses', slotclass_header, slotclass_template),
+    TestData('prefab', prefab_header, prefab_template),
+    TestData('prefab_slots', prefab_header, prefab_slots_template),
+    TestData('prefab_eval', prefab_header, prefab_eval_template),
+    TestData('namedtuples', namedtuple_header, namedtuple_template),
+    TestData('typed_namedtuples', NamedTuple_header, NamedTuple_template),
+    TestData('dataclasses', dataclass_header, dataclass_template),
+    TestData('attrs_noslots', attr_header, attr_noslots_template),
+    TestData('attrs_slots', attr_header, attr_slots_template),
+    TestData('pydantic', pydantic_header, pydantic_template),
+    # TestData('cluegen', cluegen_header, cluegen_template),
+    # TestData('cluegen_eval', cluegen_header, cluegen_eval_template),
+]
+
+
+def write_tests(*, runs=100, warmup=20, includes_pass=True):
+    base_dir.mkdir(exist_ok=True)
+    importer_dir.mkdir(exist_ok=True)
+    classdef_dir.mkdir(exist_ok=True)
+    results_dir.mkdir(exist_ok=True)
+
+    script_ext = "bat" if sys.platform == "win32" else "sh"
+
+    for data in datasets:
+        data.write_perf_importer()
+        data.write_classdef_file(count=100)
+
+    tests = " ".join(
+        f'"python hyperfine_importers/{data.import_name}_timer.py"'
+        for data in datasets
+    )
+
+    versions = (
+        "import attrs, pydantic, ducktools.classbuilder;"
+        "print(f'attrs {attrs.__version__}');"
+        "print(f'pydantic {pydantic.__version__}');"
+        "print(f'ducktools-classbuilder {ducktools.classbuilder.__version__}\\n')"
+    )
+
+    if includes_pass:
+        tests = f'"python -c \\"pass\\"" {tests}'
+
+    outpath = results_dir / 'hyperfine_result.md'
+
+    zsh_script = (
+        "python -VV\n"
+        f"python -c \"{versions}\"\n"
+        f"hyperfine --export-markdown {outpath} --shell=none --runs {runs} --warmup {warmup} {tests}"
+    )
+
+    shell_pth = base_dir / f"hyperfine_runner.{script_ext}"
+    shell_pth.write_text(zsh_script)
+
+    outpath = results_dir / 'hyperfine_importtimes.md'
+
+    tests = (
+        '''"python -c \\"pass\\"" '''
+        '''"python -c \\"from ducktools.classbuilder import slotclass\\"" '''
+        '''"python -c \\"from ducktools.classbuilder.prefab import prefab\\"" '''
+        '''"python -c \\"from collections import namedtuple\\"" '''
+        '''"python -c \\"from typing import NamedTuple\\"" '''
+        '''"python -c \\"from dataclasses import dataclass\\"" '''
+        '''"python -c \\"from attrs import define\\"" '''
+        '''"python -c \\"from pydantic import BaseModel\\"" '''
+    )
+
+    import_script = (
+        f"hyperfine --export-markdown {outpath} --shell=none --runs {runs} --warmup {warmup} {tests}"
+    )
+
+    shell_pth = base_dir / f'hyperfine_importtimes.{script_ext}'
+    shell_pth.write_text(import_script)
+
+
+if __name__ == '__main__':
+    write_tests(includes_pass=True, runs=30, warmup=10)
---
+++ packages/ducktools-classbuilder/ducktools_classbuilder-0.8.0.tar.gz/ducktools_classbuilder-0.8.0/perf/perf_profile.py
@@ -0,0 +1,291 @@
+# Modified from https://github.com/dabeaz/cluegen/blob/master/perf.py
+# A import performance test of:
+# standard classes, namedtuple, NamedTuple,
+# dataclasses, attrs,
+# cluegen, dataklasses,
+# prefab and compiled_prefab
+
+import os
+import sys
+import time
+import platform
+
+from collections import namedtuple
+from typing import NamedTuple
+
+import dataclasses
+import cluegen
+import dataklasses
+import ducktools.classbuilder as classbuilder
+
+standard_template = '''
+class C{n}:
+    def __init__(self, a, b, c, d, e):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.e = e
+
+    def __repr__(self):
+        return f'C{n}({{self.a!r}}, {{self.b!r}}, {{self.c!r}}, {{self.d!r}}, {{self.e!r}})'
+
+    def __eq__(self, other):
+        if self.__class__ is other.__class__:
+            return (self.a, self.b, self.c, self.d, self.e) == (other.a, other.b, other.c, other.d, other.e)
+        else:
+            return NotImplemented
+            
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+namedtuple_template = '''
+C{n} = namedtuple('C{n}', ['a', 'b', 'c', 'd', 'e'])
+'''
+
+NamedTuple_template = '''
+class C{n}(NamedTuple):
+    a : int
+    b : int
+    c : int
+    d : int
+    e : int
+    
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+dataclass_template = '''
+@dataclass
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+    
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+attr_template = '''
+@define
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+    
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+pydantic_template = '''
+class C{n}(BaseModel):
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+    
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+cluegen_template = '''
+class C{n}(Datum):
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+# cluegen, but same default methods as dataclasses generated
+cluegen_eval_template = '''
+class C{n}(Datum):
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+dataklass_template = '''
+@dataklass
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+
+dataklass_eval_template = '''
+@dataklass
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+    
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+slotclass_template = '''
+@slotclass(syntax_check=True)
+class C{n}:
+    __slots__ = SlotFields(
+        a=Field(),
+        b=Field(),
+        c=Field(),
+        d=Field(),
+        e=Field(),
+    )
+'''
+
+prefab_slots_template = '''
+@prefab
+class C{n}:
+    __slots__ = SlotFields(
+        a=attribute(),
+        b=attribute(),
+        c=attribute(),
+        d=attribute(),
+        e=attribute(),
+    )
+'''
+
+prefab_template = '''
+@prefab
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+'''
+
+prefab_attribute_template = '''
+@prefab
+class C{n}:
+    a = attribute()
+    b = attribute()
+    c = attribute()
+    d = attribute()
+    e = attribute()
+'''
+
+prefab_eval_template = '''
+@prefab
+class C{n}:
+    a: int
+    b: int
+    c: int
+    d: int
+    e: int
+
+C{n}.__init__, C{n}.__repr__, C{n}.__eq__
+'''
+
+
+def run_test(name, n):
+
+    import perftemp
+    del sys.modules["perftemp"]
+
+    start = time.time()
+    while n > 0:
+        import perftemp
+        del sys.modules['perftemp']
+        n -= 1
+    end = time.time()
+    print(f"| {name} | {end - start:.2f} |")
+
+
+def write_perftemp(count, template, setup):
+    with open('../perf/perftemp.py', 'w') as f:
+        f.write(setup)
+        for n in range(count):
+            f.write(template.format(n=n))
+
+
+def main(reps, test_everything=False):
+    """
+
+    :param reps: Number of repeat imports
+    :param test_everything: test against dataclasses/attrs/pydantic/cluegen/dataklasses
+    :return:
+    """
+    print(f"Python Version: {sys.version}")
+    print(f"Classbuilder version: {classbuilder.__version__}")
+    print(f"Platform: {platform.platform()}")
+
+    print(f"Time for {reps} imports of 100 classes defined with 5 basic attributes")
+    print("| Method | Total Time (seconds) |")
+    print("| --- | --- |")
+    write_perftemp(100, standard_template, '')
+    run_test('standard classes', reps)
+
+    if test_everything:
+        write_perftemp(100, namedtuple_template, 'from collections import namedtuple\n')
+        run_test('namedtuple', reps)
+
+        write_perftemp(100, NamedTuple_template, 'from typing import NamedTuple\n')
+        run_test('NamedTuple', reps)
+
+        write_perftemp(100, dataclass_template, 'from dataclasses import dataclass\n')
+        run_test('dataclasses', reps)
+
+        try:
+            import attrs
+            write_perftemp(100, attr_template, 'from attrs import define\n')
+            run_test(f'attrs {attrs.__version__}', reps)
+        except ImportError:
+            print("attrs not installed")
+
+        try:
+            import pydantic
+            write_perftemp(100, pydantic_template, 'from pydantic import BaseModel\n')
+            run_test(f'pydantic {pydantic.__version__}', reps)
+        except ImportError:
+            print("pydantic not installed")
+
+        write_perftemp(100, cluegen_template, 'from cluegen import Datum\n')
+        run_test('dabeaz/cluegen', reps)
+
+        write_perftemp(100, cluegen_eval_template, 'from cluegen import Datum\n')
+        run_test('dabeaz/cluegen_eval', reps)
+
+        write_perftemp(100, dataklass_template, 'from dataklasses import dataklass\n')
+        run_test('dabeaz/dataklasses', reps)
+
+        write_perftemp(100, dataklass_eval_template, 'from dataklasses import dataklass\n')
+        run_test('dabeaz/dataklasses_eval', reps)
+
+    slotclass_import = "from ducktools.classbuilder import slotclass, Field, SlotFields\n"
+    write_perftemp(100, slotclass_template, slotclass_import)
+    run_test(f"slotclass {classbuilder.__version__}", reps)
+
+    prefab_import = "from ducktools.classbuilder.prefab import prefab, attribute, SlotFields\n"
+
+    write_perftemp(100, prefab_slots_template, prefab_import)
+    run_test(f'prefab_slots {classbuilder.__version__}', reps)
+
+    write_perftemp(100, prefab_template, prefab_import)
+    run_test(f'prefab {classbuilder.__version__}', reps)
+
+    write_perftemp(100, prefab_attribute_template, prefab_import)
+    run_test(f'prefab_attributes {classbuilder.__version__}', reps)
+
+    write_perftemp(100, prefab_eval_template, prefab_import)
+    run_test(f'prefab_eval {classbuilder.__version__}', reps)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        reps = int(sys.argv[1])
+    else:
+        reps = 100
+    main(reps, test_everything=True)
