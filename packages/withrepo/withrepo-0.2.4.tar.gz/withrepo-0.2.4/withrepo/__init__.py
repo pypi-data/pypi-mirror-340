e12ca395cff8a81d9ae63fa9016be0042fcdc59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 14:05:42 +0000

    Add package withrepo/0.2.4/withrepo-0.2.4.tar.gz

---
+++ packages/withrepo/withrepo-0.2.4.tar.gz/withrepo-0.2.4/withrepo/__init__.py
@@ -0,0 +1,25 @@
+from withrepo.withrepo import (
+    repo,
+    RepoContext,
+    RepoFile,
+    File,
+)
+
+from withrepo.utils import (
+    RepoProvider,
+    RepoArguments,
+)
+
+from withrepo.download import (
+    copy_and_split_root_by_language_group,
+)
+
+__all__ = [
+    "repo",
+    "RepoContext",
+    "RepoFile",
+    "File",
+    "RepoArguments",
+    "RepoProvider",
+    "copy_and_split_root_by_language_group",
+]
