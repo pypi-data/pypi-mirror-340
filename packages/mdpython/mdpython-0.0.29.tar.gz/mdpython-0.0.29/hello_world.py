63ade43859dab49d1846d155d3833ed31beeaa24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:21:06 +0000

    Add package mdpython/0.0.29/mdpython-0.0.29.tar.gz

---
+++ packages/mdpython/mdpython-0.0.29.tar.gz/mdpython-0.0.29/hello_world.py
@@ -0,0 +1,4 @@
+from mdpython.docutils import testcase
+
+testcase.generate_test_cases(r"D:\data\mdpython\mdpython\packaging_tutorial"
+                             r"\src\mdpython", r"D:\data\output.xlsx", "phi", 2)
