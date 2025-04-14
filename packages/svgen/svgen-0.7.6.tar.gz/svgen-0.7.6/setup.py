5115d2a19fb3dadfb3aac7a1d578f3483eda299a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 14:06:03 +0000

    Add package svgen/0.7.6/svgen-0.7.6.tar.gz

---
+++ packages/svgen/svgen-0.7.6.tar.gz/svgen-0.7.6/setup.py
@@ -0,0 +1,38 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=2c50dc726c2b9ac8def8ad8b6a415bc8
+# =====================================
+
+"""
+svgen - Package definition for distribution.
+"""
+
+# third-party
+try:
+    from setuptools_wrapper.setup import setup
+except (ImportError, ModuleNotFoundError):
+    from svgen_bootstrap.setup import setup  # type: ignore
+
+# internal
+from svgen import DESCRIPTION, PKG_NAME, VERSION
+
+author_info = {
+    "name": "Vaughn Kottler",
+    "email": "vaughn@libre-embedded.com",
+    "username": "vkottler",
+}
+pkg_info = {
+    "name": PKG_NAME,
+    "slug": PKG_NAME.replace("-", "_"),
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "versions": [
+        "3.12",
+        "3.13",
+    ],
+}
+setup(
+    pkg_info,
+    author_info,
+)
