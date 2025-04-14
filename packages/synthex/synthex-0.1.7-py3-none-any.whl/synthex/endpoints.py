085eb7b41de0332bd77dbb7f9f549a3f1a089b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 04:06:15 +0000

    Add package synthex/0.1.7/synthex-0.1.7-py3-none-any.whl

---
+++ packages/synthex/synthex-0.1.7-py3-none-any.whl/synthex/endpoints.py
@@ -0,0 +1,13 @@
+# General
+API_BASE_URL: str = "https://compute.tanaos.com"
+PING_ENDPOINT = "/"
+
+# User
+GET_CURRENT_USER_ENDPOINT = "user"
+
+# Credits
+GET_PROMOTIONAL_CREDITS_ENDPOINT = "credits/promotional"
+
+# Jobs
+LIST_JOBS_ENDPOINT = "jobs"
+CREATE_JOB_WITH_SAMPLES_ENDPOINT = "jobs/with-samples"
