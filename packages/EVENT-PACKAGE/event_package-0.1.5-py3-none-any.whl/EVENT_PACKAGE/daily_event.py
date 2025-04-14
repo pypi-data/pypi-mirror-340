0ef184334b5ef2a3e94d9beaffc0c82f263ed1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:21:12 +0000

    Add package EVENT-PACKAGE/0.1.5/event_package-0.1.5-py3-none-any.whl

---
+++ packages/EVENT-PACKAGE/event_package-0.1.5-py3-none-any.whl/EVENT_PACKAGE/daily_event.py
@@ -0,0 +1,433 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def daily_events(Discharge, Dates):
+    
+    a = open(Discharge, 'r')
+    a1 = open(Dates, 'r')
+    # you can put event data (actual dates copies from excel) or numbers in the event date txt file
+    b = []
+    b1 = []
+    for lines in a:
+        #c = we strip the lines with c, making every line an item
+        c = lines.strip()
+        d = lines.split()
+        b.append(d)
+    a.close()
+
+    for lines in a1:
+        #c = we strip the lines with c, making every line an item
+        c = lines.strip()
+        d = lines.split()
+        b1.append(d)
+    a1.close()
+
+
+#     print(b)
+#     print(len(b))
+#     print('\n')
+#     print(b1)
+#     print(len(b1))
+#     print('\n')
+
+    Discharge = []
+    Dates = []
+    for elements in b:
+        if len(elements) < 1:
+            elements = '0'
+            Discharge.append(float(elements))
+        else:
+            Discharge.append(float(elements[0]))
+
+    for elements in b1:
+        Dates.append((elements[0]))
+        
+    print('\n')        
+#     print(f'''Discharge:
+# {Discharge}''')
+#     print(len(Discharge))
+#     print('\n')
+#     print(f'''Dates:
+# {Dates}''')
+#     print(len(Dates))
+#     print('\n')
+
+    percentage_discharge_change = ['event']
+    for elements in range(1, len(Discharge)):
+        y = Discharge[elements]
+        change = y - Discharge[elements - 1]
+        if Discharge[elements - 1 ] == 0:
+            percentage_discharge_change.append('no flow')
+        else:
+            percent_change = (change/Discharge[elements - 1]) * 100
+            percentage_discharge_change.append(percent_change)
+        
+    # print(f'''Discharge Percentage change
+    # {percentage_discharge_change}''')
+#     print(len(percentage_discharge_change))
+#     print('\n')
+    
+#     this part of the code prints out the changes in the flow and the serial number
+    for elements in range(0, len(percentage_discharge_change)):
+        c = f'{elements + 1} - {percentage_discharge_change[elements]}'
+#         print(c)
+    high_compound_flow = []
+    low_compound_flow = []
+    combine_compound_flow = []
+    events = []
+    event_dates = []
+    baseflow = []
+    baseflow_dates = []
+    event_endpoint = 0
+    event_bunch = []
+    catch_no = 0
+    packer = 0
+    caught_events = {}
+    event_occuring = False
+    switch = False
+    event_point = float(input('What is the value of the event start point: '))
+    if Discharge[0] > event_point:        
+        event_occuring = True
+        ## this part supplies the whole data for the analysis
+    event_ends = 0
+    base_compound = 0
+    high_compound = 0
+    for elements in range(0, len(Discharge)):
+#         event_ends = 0
+#         base_compound = 0
+#         high_compound = 0
+        last_slope = -1
+        if event_occuring and elements >= len(events):
+            truncated_list = Discharge[elements:]
+            section_dates = Dates[elements:]
+            changing_flow = percentage_discharge_change[elements:]
+            ## this part sections the whole data at event point and works on it until the event ends
+            for elements1 in range(0, len(truncated_list)):
+#                 the part logs the event
+                if event_occuring:
+                    if truncated_list[elements1] > event_point:
+                        event_ends = 0
+                        if elements1 > 0:
+                            if changing_flow[elements1] == 'no flow':
+                                changing_flow[elements1] = 0.0000000001
+                            if changing_flow[elements1] < 0:
+                                events.append(truncated_list[elements1])
+                                event_dates.append(section_dates[elements1])
+                                last_slope = changing_flow[elements1]
+                                baseflow.append('')
+                            else:
+                                if last_slope < 0:
+                                    high_compound += 1
+                                    events.append(truncated_list[elements1])
+                                    event_dates.append(section_dates[elements1])
+                                    last_slope = changing_flow[elements1]
+                                    baseflow.append('')
+                                else:
+                                    events.append(truncated_list[elements1])
+                                    event_dates.append(section_dates[elements1])
+                                    last_slope = changing_flow[elements1]
+                                    baseflow.append('')
+                        else:
+                            event_ends = 0
+                            events.append(truncated_list[elements1])
+                            event_dates.append(section_dates[elements1])
+                            baseflow.append('')
+#                         the part chech if the event is ended and if base flow as begun
+                    else:                        
+                        event_ends += 1
+                        if event_ends < 2:
+                            events.append(truncated_list[elements1])
+                            event_dates.append(section_dates[elements1])
+                            drop_1 = truncated_list[elements1]
+                            baseflow.append('')
+                        #this part checks if a compount event begins after reaching baseflow treshold
+                        else:
+                            if truncated_list[elements1] <= drop_1:
+                                baseflow.append(truncated_list[elements1])
+                                baseflow_dates.append(section_dates[elements1])
+                                event_occuring = False
+                                switch = True
+                                event_endpoint = elements1
+                                events.append('')
+                            else:
+#                             This part check if it is a slight bump, is significant to creat compound event
+                                if truncated_list[elements1] <= event_point:
+                                    baseflow.append(truncated_list[elements1])
+                                    baseflow_dates.append(section_dates[elements1])
+                                    events.append('')
+                                    event_occuring = False
+                                    switch = True
+                                    event_endpoint = elements1
+                                else:
+                                    base_compound += 1
+                                    events.append(truncated_list[elements1])
+                                    event_dates.append(section_dates[elements1])
+                                    baseflow.append('')
+        else:
+            if len(events) > 0 and len(events) >= packer and switch == True:
+                catch_no += 1
+                event_bunch.append(events)
+                packer = len(events)
+                caught_events[f'{catch_no} event '] = f'{event_dates[0]} - {event_dates[-1]}'
+                switch = False
+                if high_compound > 1:
+                    if base_compound > 1:
+                        combine_compound_flow.append(f'{event_dates[0]} - {event_dates[-1]}')
+                    else:
+                        high_compound_flow.append(f'{event_dates[0]} - {event_dates[-1]}')
+                elif base_compound > 1:
+                    low_compound_flow.append(f'{event_dates[0]} - {event_dates[-1]}')
+                    
+            if elements >= len(events):
+                if Discharge[elements] >= event_point:
+                    event_ends = 0
+                    event_dates = []
+                    event_occuring = True
+                    if event_occuring and elements >= len(events):
+                        truncated_list = Discharge[elements:]
+                        section_dates = Dates[elements:]
+                        changing_flow = percentage_discharge_change[elements:]
+                        ## this part sections the whole data at event point and works on it until the event ends
+                        for elements1 in range(0, len(truncated_list)):
+            #                 the part logs the event
+                            if event_occuring:
+                                if truncated_list[elements1] > event_point:
+                                    event_ends = 0
+                                    if elements1 > 0:
+                                        if changing_flow[elements1] == 'no flow':
+                                            changing_flow[elements1] = 0.0000000001
+                                        if changing_flow[elements1] < 0:
+                                            events.append(truncated_list[elements1])
+                                            event_dates.append(section_dates[elements1])
+                                            last_slope = changing_flow[elements1]
+                                            baseflow.append('')
+                                        else:
+                                            if last_slope < 0:
+                                                high_compound += 1
+                                                events.append(truncated_list[elements1])
+                                                event_dates.append(section_dates[elements1])
+                                                last_slope = changing_flow[elements1]
+                                                baseflow.append('')
+                                            else:
+                                                events.append(truncated_list[elements1])
+                                                event_dates.append(section_dates[elements1])
+                                                last_slope = changing_flow[elements1]
+                                                baseflow.append('')
+                                    else:
+                                        event_ends = 0
+                                        events.append(truncated_list[elements1])
+                                        event_dates.append(section_dates[elements1])
+                                        baseflow.append('')
+            #                         the part chech if the event is ended and if base flow as begun
+                                else:                        
+                                    event_ends += 1
+                                    if event_ends < 2:
+                                        events.append(truncated_list[elements1])
+                                        event_dates.append(section_dates[elements1])
+                                        drop_1 = truncated_list[elements1]
+                                        baseflow.append('')
+                                    #this part checks if a compount event begins after reaching baseflow treshold
+                                    else:
+                                        if truncated_list[elements1] <= drop_1:
+                                            baseflow.append(truncated_list[elements1])
+                                            baseflow_dates.append(section_dates[elements1])
+                                            event_occuring = False
+                                            switch = True
+                                            event_endpoint = elements1
+                                            events.append('')
+                                        else:
+            #                             This part check if it is a slight bump, is significant to creat compound event
+                                            if truncated_list[elements1] <= event_point:
+                                                baseflow.append(truncated_list[elements1])
+                                                baseflow_dates.append(section_dates[elements1])
+                                                events.append('')
+                                                event_occuring = False
+                                                switch = True
+                                                event_endpoint = elements1
+                                            else:
+                                                base_compound += 1
+                                                events.append(truncated_list[elements1])
+                                                event_dates.append(section_dates[elements1])
+                                                baseflow.append('')
+                    else:
+                        if len(events) > 0 and len(events) >= packer and switch == True:
+                            catch_no += 1
+                            event_bunch.append(events)
+                            packer = len(events)
+                            caught_events[f'{catch_no} event '] = f'{event_dates[0]} - {event_dates[-1]}'
+                            switch = False
+                            if high_compound > 1:
+                                if base_compound > 1:
+                                    combine_compound_flow.append(f'{event_dates[0]} - {event_dates[-1]}')
+                                else:
+                                    high_compound_flow.append(f'{event_dates[0]} - {event_dates[-1]}')
+                            elif base_compound > 1:
+                                low_compound_flow.append(f'{event_dates[0]} - {event_dates[-1]}')
+         
+                else:
+                    baseflow.append(Discharge[elements])
+                    baseflow_dates.append(Dates[elements])
+                    events.append('')                        
+    
+    # print(event_endpoint)            
+    # print(f'''events -
+    # {events}''')
+    # print(len(events))
+    # print('\n')
+    
+    # print(f'''event_dates -
+    # {event_dates}''')
+    # print(len(event_dates))
+    # print('\n')
+    
+    # print(f'''Baseflow -
+    # {baseflow}''')
+    # print(len(baseflow))
+    # print('\n')
+    # print(f'''Baseflow Dates -
+    # {baseflow_dates}''')
+    # print(len(baseflow_dates))
+    # print('\n')
+    # print(f'''Event Bunch -
+    # {event_bunch}''')
+    # print(len(event_bunch))
+    # print('\n')
+    
+    
+#     print(f'''Caught_event -
+#     {caught_events}''')
+#     print(len(caught_events))
+#     print('\n')
+#     print(f'There are {len(caught_events)} events')
+#     print('\n')
+    
+    
+    # print(f'''Combine Compound Flows - 
+    # {combine_compound_flow}''')
+    # print(f'You have {len(combine_compound_flow)} combine compound flows')
+    # print('\n')
+    # print(f'''High Compound flows - 
+    # {high_compound_flow}''')
+    # print(f'You have {len(high_compound_flow)} high compound flows')
+    # print('\n')
+    # print(f'''Low Compound Flows -
+    # {low_compound_flow}''')
+    # print(f'You have {len(low_compound_flow)} low compound flows')
+    # print('\n')
+
+#     print('EVENTS')
+#     for elements in events:
+#         print(elements)
+#     print('end')
+    
+    valid_drop = event_point
+    pack = 0
+    blocks = 0
+    pairs = 1
+    event_kickout = []
+    for elements in range(0,len(events)):
+        if type(events[elements]) == float:
+            pack += 1
+        elif elements > pack - 1 and pack > 0 and pack < 3:
+            look_max = []
+            blocks += 1
+            reductions = 0
+            for members in range (0, pack):
+                look_max.append(events[elements - pack])
+                pairs += 1
+#             print(look_max)
+            max_look = max(look_max)
+#             print(max_look)
+#             print(look_max)
+#             print(type(max_look))
+            if max_look < 1.25 * event_point:
+                event_kickout.append(f'{blocks} event ')
+                for kit in range (0, pack):
+                    events[elements - 1 - pack + reductions] = ''
+                    reductions += 1
+            pack = 0
+        else:
+            if pack > 0:
+                blocks += 1
+            pack = 0
+            
+    print(f'blocks (representing the amount of events left) = {blocks}')
+    print(f'These are the event kickout - {event_kickout}')
+    for elements in range(0, len(event_kickout)):
+        del caught_events[event_kickout[elements]]
+    
+    
+    print('\n')
+    print('These are the modified events')
+    print(caught_events)
+    print(len(caught_events))
+    print('\n')
+    print(f'There are {len(caught_events)} events')
+    print('\n')
+    
+    for elements in range (0, len(events)):
+        if type(events[elements]) == float:
+            if elements > 0 and events[elements] > valid_drop:
+                events[elements - 1] = Discharge[elements - 1]
+                
+    file_name = "Daily_flow_event_data.txt"
+        # Write to file (small predicted data)
+    with open(file_name, "w") as file:
+        for number in events:
+            file.write(f"{number}\n")
+    print(f"Numbers successfully written to {file_name}")
+    # print('BaseFlow')
+    # for elements in baseflow:
+    #     if elements == 0.0:
+    #         print('')
+    #     else:
+    #         print(elements)
+#     print('end')
+    
+    
+    y2 = events
+    y1 = Discharge
+    
+    x = []
+    for elements in range(0, len(y1)):
+        x.append(Dates[elements])
+
+    # Convert empty strings to np.nan and the rest to float for y2
+    y2_clean = [float(val) if val != '' else np.nan for val in y2]
+
+    # Create stacked subplots with shared x-axis
+    fig, axs = plt.subplots(2, 1, figsize=(6, 6), sharex=True)
+
+    # First plot (y1)
+    axs[0].plot(x, y1, marker='', color='green')
+    axs[0].set_title('Plot of Discharge Data')
+    axs[0].set_ylabel('Drainage (cm3/day)')
+    axs[0].grid(False)
+
+    # Second plot (y2 with missing)
+    axs[1].plot(x, y2_clean, marker='', linestyle='-', color='blue')
+    axs[1].set_title('Plot of Events')
+    axs[1].set_xlabel('Dates')
+    axs[1].set_ylabel('Drainage (cm3/day)')
+    axs[1].grid(False)
+    points = 30
+    if len(x) < points:
+        points = len(x)
+    axs[1].xaxis.set_major_locator(plt.MaxNLocator(nbins=points))
+    fig.autofmt_xdate(rotation=45)
+
+    # Optional: Set same y-axis limits (uncomment if needed)
+    y_min = min(min(y1, default=np.nan), min(y2_clean, default=np.nan))
+    y_max = max(max(y1, default=np.nan), max(y2_clean, default=np.nan))
+    if not np.isnan(y_min) and not np.isnan(y_max):
+        axs[0].set_ylim(y_min, y_max)
+        axs[1].set_ylim(y_min, y_max)
+
+    # Adjust layout and show
+    plt.tight_layout()
+    plt.show()
+
+Discharge = 'event_data.txt'
+Dates = 'Daily_dates.txt'
+
+daily_events(Discharge, Dates)
