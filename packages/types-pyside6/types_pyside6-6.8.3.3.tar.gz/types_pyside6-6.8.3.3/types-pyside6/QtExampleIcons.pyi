7c47ec1edfb30a772c36f278b0023439b7a64efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:22:30 +0000

    Add package types-pyside6/6.8.3.3/types_pyside6-6.8.3.3.tar.gz

---
+++ packages/types-pyside6/types_pyside6-6.8.3.3.tar.gz/types_pyside6-6.8.3.3/types-pyside6/QtExampleIcons.pyi
@@ -0,0 +1,16 @@
+# Copyright (C) 2022 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+from __future__ import annotations
+"""
+This file contains the exact signatures for all functions in module
+PySide6.QtExampleIcons, except for defaults which are replaced by "...".
+
+# mypy: disable-error-code="override, overload-overlap"
+"""
+
+# Module `PySide6.QtExampleIcons`
+
+import PySide6.QtExampleIcons
+
+
+# eof
