42f58bac2b8ad3ad3cad22bc2d0a8009bde9064a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 04:06:15 +0000

    Add package bot-engine/0.1.14/bot_engine-0.1.14.tar.gz

---
+++ packages/bot-engine/bot_engine-0.1.14.tar.gz/bot_engine-0.1.14/bot_engine/dialogs/BotDialogs.py
@@ -0,0 +1,27 @@
+from os import getenv
+
+# if getenv("ENVIRONMENT") == "testing":
+#     from bot.Bot import Bot
+
+# else:
+#     from bot_engine.bot.Bot import Bot
+
+
+class BotDialogs:
+    def __init__(self, bot):
+        """
+            Зависимости, которые было у меня ранее: 
+            1. Бот
+            2. Генератор диалогов
+            3. И тексты
+            
+            self.bot = Bot()
+            self.dialog_generator = DialogGenerator()
+            self.messages = Language().messages    
+        """
+        pass
+
+
+
+    def set_dialogs(self):
+        pass
