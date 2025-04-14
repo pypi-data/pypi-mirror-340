81703fddbd2d8981b44c78bd10e92f06214a5978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 16:05:56 +0000

    Add package maxminconquer/0.1.0/maxminconquer-0.1.0-py3-none-any.whl

---
+++ packages/maxminconquer/maxminconquer-0.1.0-py3-none-any.whl/maxminconquer/__init__.py
@@ -0,0 +1,2 @@
+from .find_max import find_max
+from .find_min import find_min
---
+++ packages/maxminconquer/maxminconquer-0.1.0-py3-none-any.whl/maxminconquer/find_min.py
@@ -0,0 +1,20 @@
+# maxminconquer/find_min.py
+
+def find_min(arr, left, right):
+    """Finds the minimum value in the array using Divide and Conquer."""
+    # Base case: If there is only one element
+    if left == right:
+        print(arr[left])
+        return arr[left]
+    
+    # Find the middle index
+    mid = (left + right) // 2
+    print("middle: ", mid)
+    print(arr[left:mid+1], arr[mid+1:right+1])
+    
+    # Recursively find the minimum in both halves
+    min_left = find_min(arr, left, mid)
+    min_right = find_min(arr, mid + 1, right)
+    
+    # Return the minimum of the two halves
+    return min(min_left, min_right)
