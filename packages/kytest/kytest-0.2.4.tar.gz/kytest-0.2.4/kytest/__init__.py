28a08df035e75785a71409503d598f66ffda9413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 16:05:29 +0000

    Add package kytest/0.2.4/kytest-0.2.4.tar.gz

---
+++ packages/kytest/kytest-0.2.4.tar.gz/kytest-0.2.4/kytest/__init__.py
@@ -0,0 +1,16 @@
+from .running.runner import main, ApiConfig, WebConfig, AppConfig, SonicConfig
+from .running.conf import App
+from .utils.config import FileConfig
+from .utils.pytest_util import *
+from .utils.allure_util import *
+from .utils.log import logger
+from .core.api import HttpReq, TC
+from .core.adr import AdrTC, AdrElem
+from .core.ios import IosTC, IosElem
+from .core.web import WebTC, WebElem
+from .core.hm import HmTC, HmElem
+from .core.ocr.element import OcrElem
+from .page import Page
+
+__version__ = "0.2.4"
+__description__ = "API/安卓/IOS/WEB/鸿蒙Next平台自动化测试框架"
