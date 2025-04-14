a497ff427038f8f2de89b5205a52ae2eb32db3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 04:06:20 +0000

    Add package MD23005UNO/0.4/md23005uno-0.4.tar.gz

---
+++ packages/MD23005UNO/md23005uno-0.4.tar.gz/md23005uno-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(
+    name='MD23005UNO',
+    version='0.4',
+    packages=['MD23005UNO'],
+    description='Librería que contiene diferentes métodos para resolver sistemas de ecuaciones lineales',
+    long_description=open('README.md',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Ronald Manzano',
+    author_email='md23005@ues.edu.sv',
+    url='https://github.com/MD23005/MD23005UNO.git',
+    install_requires=[
+        'numpy',
+        'typing'
+    ]
+    
+)
