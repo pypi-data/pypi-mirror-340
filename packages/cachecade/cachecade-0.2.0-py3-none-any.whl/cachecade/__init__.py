458d565feaf48b4e58c8345adb935864f0d113ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 14:06:13 +0000

    Add package cachecade/0.2.0/cachecade-0.2.0-py3-none-any.whl

---
+++ packages/cachecade/cachecade-0.2.0-py3-none-any.whl/cachecade/__init__.py
@@ -0,0 +1,3 @@
+from .caching import cachecaded, init_cache, generate_cache_key, get_cache_entry, set_cache_entry
+
+__all__ = ['cachecaded', 'init_cache', 'generate_cache_key', 'get_cache_entry', 'set_cache_entry']
