0160e416f1abd12744f559fa3f59e437505d2430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 04:05:55 +0000

    Add package ft-package-bgursoyy/0.0.1/ft_package_bgursoyy-0.0.1.tar.gz

---
+++ packages/ft-package-bgursoyy/ft_package_bgursoyy-0.0.1.tar.gz/ft_package_bgursoyy-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ft_package_bgursoyy",
+    version="0.0.1",
+    description="A sample test package",
+    author="bgursoy",
+    author_email="bestegursoy52@gmail.com",
+    url="https://github.com/bestegrsoy/Piscine-for-data-science/ft_package",
+    packages=find_packages(),
+    license="MIT",
+    classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    ]
+)
