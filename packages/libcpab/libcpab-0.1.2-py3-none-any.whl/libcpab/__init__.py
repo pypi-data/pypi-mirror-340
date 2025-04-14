631bc5c4740c421c881d50d1e2a91eda341e2434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:22:48 +0000

    Add package libcpab/0.1.2/libcpab-0.1.2-py3-none-any.whl

---
+++ packages/libcpab/libcpab-0.1.2-py3-none-any.whl/libcpab/__init__.py
@@ -0,0 +1,9 @@
+"""
+This code is a modified version of the original by SkafteNicki.
+See https://github.com/SkafteNicki/libcpab for original code.
+See LICENSE for more details.
+"""
+
+#%%
+from .cpab import Cpab
+
