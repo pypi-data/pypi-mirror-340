f4bde9e61b86fa07d4a73fda3894dbdd94a66fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:51 +0000

    Add package z3c.caching/4.0/z3c_caching-4.0.tar.gz

---
+++ packages/z3c.caching/z3c_caching-4.0.tar.gz/z3c_caching-4.0/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+
+version = '4.0'
+
+
+def read(name):
+    """Read a file in cwd."""
+    with open(name) as f:
+        return f.read()
+
+
+setup(
+    name="z3c.caching",
+    version=version,
+    description="Caching infrastructure for web apps",
+    long_description="\n\n".join([
+        read("README.rst"),
+        read("CHANGES.rst")]),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="zope caching",
+    author="Wichert Akkerman",
+    author_email="zope-dev@zope.dev",
+    url="https://github.com/zopefoundation/z3c.caching",
+    license="ZPL",
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.9',
+    install_requires=[
+        "setuptools",
+        "zope.browser",
+        "zope.component",
+        "zope.event",
+        "zope.interface>=3.8.0",
+        "zope.schema",
+        "zope.lifecycleevent",
+    ],
+    extras_require={
+        "zcml": ("zope.configuration", ),
+        "test": ("zope.configuration", ),
+    },
+)
