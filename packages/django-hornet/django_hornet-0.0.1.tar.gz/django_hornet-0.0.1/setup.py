7542c62562d1bfd74058c4200e4f5c4bda0e07ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 16:05:29 +0000

    Add package django-hornet/0.0.1/django_hornet-0.0.1.tar.gz

---
+++ packages/django-hornet/django_hornet-0.0.1.tar.gz/django_hornet-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-hornet",
+    ppackages=find_packages(), 
+    
+    version="0.0.1",
+    description="Django Hornet Wrapper",
+    url="https://github.com/null-none/django-hornet",
+    keywords=["django", "hotnet", "ajax"],
+    classifiers=["Programming Language :: Python :: 3"],
+    include_package_data=True,
+)
