45755ad9f1a23be26f9f1b37b7d44eaf888dab06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:22 +0000

    Add package z3c.breadcrumb/4.0/z3c_breadcrumb-4.0.tar.gz

---
+++ packages/z3c.breadcrumb/z3c_breadcrumb-4.0.tar.gz/z3c_breadcrumb-4.0/setup.py
@@ -0,0 +1,79 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup"""
+
+import os
+
+from setuptools import setup
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name="z3c.breadcrumb",
+    version="4.0",
+    author="Roger Ineichen, Stephan Richter and the Zope Community",
+    author_email="zope-dev@zope.dev",
+    description="A pluggable breadcrumbs implementation based on adapters.",
+    long_description=(
+        read("README.rst") + "\n\n" + "Detailed Documentation\n"
+        "**********************\n"
+        + "\n"
+        + read("src", "z3c", "breadcrumb", "README.rst")
+        + "\n\n"
+        + read("CHANGES.rst")
+    ),
+    license="ZPL-2.1",
+    keywords="zope3 breadcrumb adapter",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        "Topic :: Internet :: WWW/HTTP",
+        "Framework :: Zope :: 3",
+    ],
+    url="https://github.com/zopefoundation/z3c.breadcrumb",
+    python_requires=">=3.9",
+    extras_require=dict(
+        test=[
+            "zope.container",
+            "zope.site",
+            "zope.testing",
+        ],
+    ),
+    install_requires=[
+        "setuptools",
+        "zope.component",
+        "zope.interface",
+        "zope.location",
+        "zope.publisher",
+        "zope.schema",
+        "zope.traversing",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
---
+++ packages/z3c.breadcrumb/z3c_breadcrumb-4.0.tar.gz/z3c_breadcrumb-4.0/src/z3c/breadcrumb/tests.py
@@ -0,0 +1,69 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Tests"""
+
+import doctest
+import unittest
+
+import zope.location
+import zope.site.testing
+import zope.traversing.testing
+from zope.interface.verify import verifyObject
+from zope.publisher.browser import TestRequest
+
+
+def doctest_Breadcrumbs_interface():
+    """Test that Breadcrumbs matches the interface
+
+    >>> from z3c.breadcrumb import browser, interfaces
+    >>> breadcrumbs = browser.Breadcrumbs(None, None)
+    >>> verifyObject(interfaces.IBreadcrumbs, breadcrumbs)
+    True
+
+    """
+
+
+def doctest_GenericBreadcrumb_interface():
+    """Test that GenericBreadcrumb matches the interface
+
+    >>> from z3c.breadcrumb import browser, interfaces
+    >>> breadcrumb = browser.GenericBreadcrumb(rootFolder, TestRequest())
+    >>> verifyObject(interfaces.IBreadcrumb, breadcrumb)
+    True
+
+    """
+
+
+def setUp(test):
+    site = zope.site.testing.siteSetUp(True)
+    zope.traversing.testing.setUp()
+    test.globs["rootFolder"] = site
+
+
+def tearDown(test):
+    zope.site.testing.siteTearDown()
+
+
+def test_suite():
+    return unittest.TestSuite(
+        [
+            doctest.DocFileSuite(
+                "README.rst",
+                setUp=setUp,
+                tearDown=tearDown,
+                optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+            ),
+            doctest.DocTestSuite(setUp=setUp, tearDown=tearDown),
+        ]
+    )
