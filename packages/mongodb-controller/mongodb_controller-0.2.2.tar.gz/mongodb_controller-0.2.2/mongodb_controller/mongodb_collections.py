09c26abfb3e0b687409b803b6a15101ac33050af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:08 +0000

    Add package mongodb-controller/0.2.2/mongodb_controller-0.2.2.tar.gz

---
+++ packages/mongodb-controller/mongodb_controller-0.2.2.tar.gz/mongodb_controller-0.2.2/mongodb_controller/mongodb_collections.py
@@ -0,0 +1,11 @@
+from .mongodb_connector import client
+
+
+COLLECTION_2110 = client['database-rpa']['dataset-menu2110']
+COLLECTION_2160 = client['database-rpa']['dataset-menu2160']
+COLLECTION_2205 = client['database-rpa']['dataset-menu2205']
+COLLECTION_2205_SNAPSHOT = client['database-rpa']['dataset-menu2205-snapshot']
+COLLECTION_8186 = client['database-rpa']['dataset-menu8186']
+COLLECTION_BBG_TIMESERIES = client['database-rpa']['dataset-bbg-timeseries']
+
+COLLECTION_ESAFE = client['database-ESAFE']['dataset-menuESAFE500068']
---
+++ packages/mongodb-controller/mongodb_controller-0.2.2.tar.gz/mongodb_controller-0.2.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mongodb_controller',
+    version='0.2.2',
+    packages=find_packages(),
+    install_requires=[
+        'pymongo>=4.10.1',
+        'pandas>=2.2.3',
+        'python-dotenv>=1.0.0',
+        'aws-s3-controller>=0.7.5',
+        'shining-pebbles>=0.5.3',
+    ],
+    author='June Young Park',
+    author_email='juneyoungpaak@gmail.com',
+    description='A Python module for managing fund time series data using MongoDB',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nailen1/mongodb_controller.git',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+    keywords='mongodb database fund time-series data-management',
+)
