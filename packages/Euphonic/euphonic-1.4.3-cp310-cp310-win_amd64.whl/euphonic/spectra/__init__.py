3454f52aa0aeefa8581aa49789817132a46868c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 16:05:37 +0000

    Add package Euphonic/1.4.3/euphonic-1.4.3-cp310-cp310-win_amd64.whl

---
+++ packages/Euphonic/euphonic-1.4.3-cp310-cp310-win_amd64.whl/euphonic/spectra/__init__.py
@@ -0,0 +1,29 @@
+from .base import (
+    CallableQuantity,
+    Spectrum,
+    Spectrum1D,
+    Spectrum2D,
+    XTickLabels,
+    apply_kinematic_constraints,
+)
+from .collections import (
+    LineData,
+    Metadata,
+    OneLineData,
+    Spectrum1DCollection,
+    Spectrum2DCollection,
+)
+
+__all__ = [
+    "apply_kinematic_constraints",
+    "CallableQuantity",
+    "LineData",
+    "Metadata",
+    "OneLineData",
+    "Spectrum",
+    "Spectrum1D",
+    "Spectrum2D",
+    "Spectrum1DCollection",
+    "Spectrum2DCollection",
+    "XTickLabels",
+]
