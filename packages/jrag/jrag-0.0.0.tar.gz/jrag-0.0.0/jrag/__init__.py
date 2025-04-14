9368b893d22aae5e8eb36d5fad207e67f972b2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:21:16 +0000

    Add package jrag/0.0.0/jrag-0.0.0.tar.gz

---
+++ packages/jrag/jrag-0.0.0.tar.gz/jrag-0.0.0/jrag/__init__.py
@@ -0,0 +1,11 @@
+from .jrag import (  # noqa
+    _flatten_json_config,
+    _flatten_json_default,
+    _format_value,
+    _json_to_rag_string,
+    add_text,
+    tag_list,
+    to_text,
+)
+
+__version__ = "0.0.0"
