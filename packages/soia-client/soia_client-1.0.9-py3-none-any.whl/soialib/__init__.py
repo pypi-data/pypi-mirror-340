92d5b23ace8ef73d61a142fdd50dc1011990cc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 14:05:31 +0000

    Add package soia-client/1.0.9/soia_client-1.0.9-py3-none-any.whl

---
+++ packages/soia-client/soia_client-1.0.9-py3-none-any.whl/soialib/__init__.py
@@ -0,0 +1,19 @@
+from soialib.keyed_items import KeyedItems
+from soialib.method import Method
+from soialib.serializer import Serializer
+from soialib.serializers import (
+    array_serializer,
+    optional_serializer,
+    primitive_serializer,
+)
+from soialib.timestamp import Timestamp
+
+__all__ = [
+    "KeyedItems",
+    "Method",
+    "Serializer",
+    "Timestamp",
+    "array_serializer",
+    "optional_serializer",
+    "primitive_serializer",
+]
