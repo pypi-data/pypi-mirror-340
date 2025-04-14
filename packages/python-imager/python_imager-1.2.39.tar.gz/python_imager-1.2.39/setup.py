87f1aab135f139a8adc0278dcaeab5592e82fa59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 04:05:54 +0000

    Add package python-imager/1.2.39/python_imager-1.2.39.tar.gz

---
+++ packages/python-imager/python_imager-1.2.39.tar.gz/python_imager-1.2.39/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# python3 -m build
+# twine upload  dist/* --verbose
+
+VERSION = '1.2.39'
+DESCRIPTION = 'Python image program'
+with open("README.md", "r", encoding="utf8") as file:
+    LONG_DESCRIPTION = file.read()
+
+setup(
+    name="python-imager",
+    version=VERSION,
+    author="Rigavid",
+    author_email="tsana.code@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=["numpy", "opencv-python", "screeninfo"],
+    keywords=['python', 'image'],
+    classifiers= [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license="LICENCE",
+)
