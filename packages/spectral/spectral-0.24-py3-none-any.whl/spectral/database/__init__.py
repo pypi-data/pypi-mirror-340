16e4ce8c1eee21c18dfc4f1f843c5354e1498f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:22:52 +0000

    Add package spectral/0.24/spectral-0.24-py3-none-any.whl

---
+++ packages/spectral/spectral-0.24-py3-none-any.whl/spectral/database/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .aster import AsterDatabase
+from .ecostress import EcostressDatabase
+from .relab import RelabDatabase
+from .usgs import USGSDatabase
