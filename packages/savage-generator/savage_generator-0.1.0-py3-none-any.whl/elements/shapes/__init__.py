3676137790c676b38f3d50d32dd0f257adb9ec60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:22:29 +0000

    Add package savage-generator/0.1.0/savage_generator-0.1.0-py3-none-any.whl

---
+++ packages/savage-generator/savage_generator-0.1.0-py3-none-any.whl/elements/shapes/__init__.py
@@ -0,0 +1,9 @@
+from .shape import Shape
+from .circle import Circle
+from .ellipse import Ellipse
+from .line import Line
+from .polygon import Polygon
+from .polyline import Polyline
+from .rect import Rect
+
+__all__ = ["Shape", "Circle", "Ellipse", "Line", "Polygon", "Polyline", "Rect"]
