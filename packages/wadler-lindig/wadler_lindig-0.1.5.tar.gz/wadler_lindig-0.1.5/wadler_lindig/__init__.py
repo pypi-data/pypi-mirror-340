b8a8ad634a668c0e9f27fbb73bb0caeb23c598cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 04:06:20 +0000

    Add package wadler-lindig/0.1.5/wadler_lindig-0.1.5.tar.gz

---
+++ packages/wadler-lindig/wadler_lindig-0.1.5.tar.gz/wadler_lindig-0.1.5/wadler_lindig/__init__.py
@@ -0,0 +1,20 @@
+from ._ansi import ansi_format as ansi_format, ansi_strip as ansi_strip
+from ._definitions import (
+    array_summary as array_summary,
+    bracketed as bracketed,
+    comma as comma,
+    join as join,
+    named_objs as named_objs,
+    pdiff as pdiff,
+    pdoc as pdoc,
+    pformat as pformat,
+    pprint as pprint,
+)
+from ._wadler_lindig import (
+    AbstractDoc as AbstractDoc,
+    BreakDoc as BreakDoc,
+    ConcatDoc as ConcatDoc,
+    GroupDoc as GroupDoc,
+    NestDoc as NestDoc,
+    TextDoc as TextDoc,
+)
