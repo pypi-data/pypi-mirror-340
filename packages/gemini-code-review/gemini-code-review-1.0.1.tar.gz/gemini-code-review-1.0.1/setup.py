8396321bd1d8d9cfe8b8722c16aa564d8a48431f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:51 +0000

    Add package gemini-code-review/1.0.1/gemini-code-review-1.0.1.tar.gz

---
+++ packages/gemini-code-review/gemini-code-review-1.0.1.tar.gz/gemini-code-review-1.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gemini-code-review",
+    version="1.0.1",
+    keywords=("gitlab", "ai", "code review"),
+    description="code review via diff",
+    long_description="code review via diff with ai model",
+    license="MIT Licence",
+
+    url="https://xxx.com",
+    author="xuwuqiang",
+    author_email="xwqiang2008@outlook.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        "google-ai-generativelanguage==0.6.10",
+        "google-generativeai",
+        "github3.py==1.3.0",
+        "unidiff",
+        "setuptools==65.6.3"
+    ],
+
+    scripts=[],
+    # 如果出现 ModuleNotFoundError: No module named,用 py_modules
+    py_modules=[],
+    entry_points={
+        'console_scripts': [
+            'review = gemini.review_code:command_review',
+            'commit = gemini.commit_code:command_git_commit'
+        ]
+    }
+)
