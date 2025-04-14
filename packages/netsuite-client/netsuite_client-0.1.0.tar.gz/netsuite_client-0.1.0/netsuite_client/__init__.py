fb2493639690a1968ac743d1615bc1852dc53579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:21:03 +0000

    Add package netsuite-client/0.1.0/netsuite_client-0.1.0.tar.gz

---
+++ packages/netsuite-client/netsuite_client-0.1.0.tar.gz/netsuite_client-0.1.0/netsuite_client/__init__.py
@@ -0,0 +1,16 @@
+"""
+NetSuite Client Library
+~~~~~~~~~~~~~~~~~~~~~~
+
+A Python client library for interacting with NetSuite's REST and SuiteQL APIs.
+
+:copyright: (c) 2024-present by Continuous AI, Inc.
+:license: BSD 3-Clause, see LICENSE for more details.
+"""
+
+from .client import NetSuiteClient
+from .client import NetSuiteError
+
+__version__ = "0.1.0"
+__author__ = "Continuous AI, Inc."
+__license__ = "BSD 3-Clause" 
