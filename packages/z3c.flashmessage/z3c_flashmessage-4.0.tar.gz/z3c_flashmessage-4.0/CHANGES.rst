a7f954cc8b6f2b408d3fb19c9423e1c6b1cc7da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:50 +0000

    Add package z3c.flashmessage/4.0/z3c_flashmessage-4.0.tar.gz

---
+++ packages/z3c.flashmessage/z3c_flashmessage-4.0.tar.gz/z3c_flashmessage-4.0/CHANGES.rst
@@ -0,0 +1,81 @@
+=======
+CHANGES
+=======
+
+4.0 (2025-04-14)
+================
+
+- Replace ``pkg_resources`` namespace with PEP 420 native namespace.
+
+
+3.1 (2025-04-03)
+================
+
+- Add support for Python 3.12, 3.13.
+
+- Drop support for Python 3.7, 3.8.
+
+
+3.0 (2023-02-08)
+================
+
+- Drop support for Python 2.7, 3.4, 3.5, 3.6.
+
+- Add support for Python 3.8, 3.9, 3.10, 3.11.
+
+- Ensure all objects have consistent resolution orders.
+
+
+2.1 (2018-11-12)
+================
+
+- Claim support for Python 3.6, 3.7, PyPy and PyPy3.
+
+- Drop support for Python 3.3.
+
+- Drop support for ``python setup.py test``.
+
+
+2.0 (2016-08-08)
+================
+
+- Standardize namespace ``__init__``.
+
+- Claim compatibility for Python 3.3, 3.4, and 3.5.
+
+1.3 (2010-10-28)
+================
+
+- ``SessionMessageSource`` implicitly created sessions when the client was
+  reading the messages from the source. Changed internal API so reading no
+  longer creates a session when it not yet exists.
+
+1.2 (2010-10-19)
+================
+
+* Removed test dependency on `zope.app.zcmlfiles`.
+
+
+1.1 (2010-10-02)
+================
+
+* Removed test dependency on `zope.app.testing`.
+
+
+1.0 (2007-12-06)
+================
+
+* Updated dependency to `zope.session` instead of `zope.app.session` to get
+  rid of deprecation warnings.
+
+
+1.0b2 (2007-09-12)
+==================
+
+* Bugfix: When there was more than one message in a source not all messages
+  would be returned by the receiver.
+
+1.0b1 (2007-08-22)
+==================
+
+* Initial public release.
---
+++ packages/z3c.flashmessage/z3c_flashmessage-4.0.tar.gz/z3c_flashmessage-4.0/setup.py
@@ -0,0 +1,62 @@
+import os.path
+
+from setuptools import setup
+
+
+def read(*path_elements):
+    with open(os.path.join(*path_elements)) as f:
+        return "\n\n" + f.read()
+
+
+TEST_REQUIREMENTS = [
+    'zope.publisher',
+    'zope.component',
+    'zope.security',
+    'zope.testrunner >= 6.4',
+    'zope.app.wsgi[testlayer] >= 4.0',
+    'Webtest',
+]
+
+setup(
+    name="z3c.flashmessage",
+    version='4.0',
+    author="Jasper Op de Coul, Christian Theune",
+    author_email="jasper@infrae.com, mail@gocept.com",
+    description="A package for sending `flash messages` to users.",
+    long_description=(
+        '.. contents::\n\n' +
+        read('src', 'z3c', 'flashmessage', 'README.rst') +
+        read('CHANGES.rst')
+    ),
+    url='https://github.com/zopefoundation/z3c.flashmessage',
+    license="ZPL-2.1",
+    keywords="zope3 message zope session",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope :: 3',
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    python_requires='>=3.9',
+    install_requires=[
+        'setuptools',
+        'ZODB',
+        'zope.interface',
+        'zope.schema',
+        'zope.session'
+    ],
+    extras_require=dict(test=TEST_REQUIREMENTS),
+)
