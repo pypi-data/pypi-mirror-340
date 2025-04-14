d4abd1cd543c8650351620a65cfc2cbfbc89b2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 05:05:42 +0000

    Add package CGCD/0.0.0/cgcd-0.0.0.tar.gz

---
+++ packages/CGCD/cgcd-0.0.0.tar.gz/cgcd-0.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CGCD",
+    version="0.0.0",
+    author="JF Benavente, JE Tenopala",
+    author_email="jf.benavente@ciemat.es, jtenopalap1800@alumno.ipn.mx",
+    description="Computer Glow Curve Deconvolution",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/JFBenavente/CGCD",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "math", "numpy", "pandas", "matplotlib", "scipy","IPython","plotly",
+        "os","shutil","io","openpyxl"
+    ],
+    include_package_data=True,
+    license="MIT",
+)
