70b725724b4312503d3afde1dd6989f859124c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:03 +0000

    Add package z3c.objpath/3.0/z3c_objpath-3.0-py3-none-any.whl

---
+++ packages/z3c.objpath/z3c_objpath-3.0-py3-none-any.whl/z3c/objpath/tests.py
@@ -0,0 +1,27 @@
+import doctest
+import unittest
+
+from zope.interface.verify import verifyObject
+
+
+class ObjectPathTests(unittest.TestCase):
+    """Testing .path.*"""
+
+    def test_module_provides_interface(self):
+        from . import _path
+        from .interfaces import IObjectPath
+        self.assertTrue(verifyObject(IObjectPath, _path))
+
+
+def test_suite():
+    optionflags = (
+        doctest.ELLIPSIS
+        | doctest.REPORT_NDIFF
+        | doctest.NORMALIZE_WHITESPACE
+    )
+
+    return unittest.TestSuite([
+        unittest.defaultTestLoader.loadTestsFromTestCase(ObjectPathTests),
+        doctest.DocFileSuite(
+            'README.rst', optionflags=optionflags)
+    ])
