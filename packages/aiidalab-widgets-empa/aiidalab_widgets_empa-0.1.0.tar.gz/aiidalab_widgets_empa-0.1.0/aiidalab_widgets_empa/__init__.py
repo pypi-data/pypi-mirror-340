164293a012a9817ef930474d6a80066bb106c8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 04:05:49 +0000

    Add package aiidalab-widgets-empa/0.1.0/aiidalab_widgets_empa-0.1.0.tar.gz

---
+++ packages/aiidalab-widgets-empa/aiidalab_widgets_empa-0.1.0.tar.gz/aiidalab_widgets_empa-0.1.0/aiidalab_widgets_empa/__init__.py
@@ -0,0 +1,8 @@
+from .computational_resources import ProcessResourcesWidget, ResourcesEstimatorWidget
+from .version import __version__
+
+__all__ = [
+    "ProcessResourcesWidget",
+    "ResourcesEstimatorWidget",
+    "__version__",
+]
