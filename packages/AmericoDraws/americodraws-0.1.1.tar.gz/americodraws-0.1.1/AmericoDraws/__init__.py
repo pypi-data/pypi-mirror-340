eb139085353eeb3d39470d6b7dc42372fec7856a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:21:22 +0000

    Add package AmericoDraws/0.1.1/americodraws-0.1.1.tar.gz

---
+++ packages/AmericoDraws/americodraws-0.1.1.tar.gz/americodraws-0.1.1/AmericoDraws/__init__.py
@@ -0,0 +1,12 @@
+"""
+AmericoDraws - A library for processing images for robotic drawing.
+
+This library provides tools to convert images into drawing paths
+suitable for robotic arms.
+"""
+
+__version__ = '0.1.1'
+__author__ = 'Lucas Dantas'
+
+# Import main functions for easy access
+from .image_processor import process_image_for_robot
