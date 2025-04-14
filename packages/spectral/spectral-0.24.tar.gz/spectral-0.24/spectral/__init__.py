dbc45ef6f4775592a8da3f0e5085301e10dbabd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:22:52 +0000

    Add package spectral/0.24/spectral-0.24.tar.gz

---
+++ packages/spectral/spectral-0.24.tar.gz/spectral-0.24/spectral/__init__.py
@@ -0,0 +1,38 @@
+'''
+Basic package setup and global imports.
+'''
+
+# flake8: noqa
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__version__ = '0.24'
+
+import sys
+if sys.byteorder == 'little':
+    byte_order = 0   # little endian
+else:
+    byte_order = 1   # big endian
+
+BSQ = 0
+BIL = 1
+BIP = 2
+
+COMPLEX_SIZES = [64, 128, 256]
+
+from .utilities.errors import SpyException
+from .config import SpySettings, spy_colors
+settings = SpySettings()
+
+from .spectral import (open_image, load_training_sets, BandInfo)
+from .io import *
+from .algorithms import *
+from .graphics import *
+from .database import *
+
+# Import some submodules into top-level namespace
+from .algorithms import detectors
+
+from .spectral import _init
+_init()
+del _init
