1d6e553182c33d897b7facb154f1373d8df78a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:11:05 +0000

    Add package agentic-chunker/0.0.6/agentic_chunker-0.0.6.tar.gz

---
+++ packages/agentic-chunker/agentic_chunker-0.0.6.tar.gz/agentic_chunker-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+version_file = Path(__file__).parent / "agentic_chunker/version.py"
+exec(version_file.read_text())
+
+setup(
+    name='agentic_chunker',
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        'openai>=1.24.1',
+        'tqdm>=4.11.1',
+    ],
+    description="A tool for improving the quality of chunks.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Gurram Siddarth Reddy',
+)
