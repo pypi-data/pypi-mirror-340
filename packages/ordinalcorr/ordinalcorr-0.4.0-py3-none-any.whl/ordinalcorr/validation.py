d67387dc9eda25101b91d927e546a78c8d847091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:58 +0000

    Add package ordinalcorr/0.4.0/ordinalcorr-0.4.0-py3-none-any.whl

---
+++ packages/ordinalcorr/ordinalcorr-0.4.0-py3-none-any.whl/ordinalcorr/validation.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class ValidationError(Exception):
+    pass
+
+
+def check_if_zero_variance(x):
+    if len(set(x)) == 1:
+        raise ValidationError(
+            "all elements in the input are the same and zero variance"
+        )
+
+
+def check_if_data_is_dichotomous(y):
+    is_dichotomous = set(np.unique(y)) == {0, 1}
+    if not is_dichotomous:
+        raise ValidationError(
+            "dichotomous (binary) variable 'y' must be consist of {0, 1}"
+        )
