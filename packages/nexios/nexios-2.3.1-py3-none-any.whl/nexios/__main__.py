542de269f73b2468f6b5f5d07ae3834771bf3987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 16:05:54 +0000

    Add package nexios/2.3.1/nexios-2.3.1-py3-none-any.whl

---
+++ packages/nexios/nexios-2.3.1-py3-none-any.whl/nexios/__main__.py
@@ -0,0 +1,16 @@
+__version__ = "2.3.1"
+
+ascii_art = f"""
+  _   _                 _
+ | \\ | |               (_)
+ |  \\| |   ___  __  __  _    ___    ___
+ | . ` |  / _ \\ \\ \\/ / | |  / _ \\  / __|
+ | |\\  | |  __/  >  <  | | | (_) | \\__ \\
+ |_| \\_|  \\___| /_/\\_\\ |_|  \\___/  |___/
+
+    🚀 Welcome to Nexios 🚀
+      The sleek ASGI Backend Framework
+      Version: {__version__}
+"""
+
+print(ascii_art)
---
+++ packages/nexios/nexios-2.3.1-py3-none-any.whl/nexios/static.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from pathlib import Path
+import os
+from typing import Union
+from nexios.http.request import Request
+from nexios.http.response import NexiosResponse
+
+
+class StaticFilesHandler:
+
+    def __init__(self, directory: Union[str, Path], url_prefix: str = "/static/"):
+
+        self.directory = Path(directory).resolve()
+        self.url_prefix = url_prefix.strip("/") + "/"
+
+        if not self.directory.exists():
+            os.makedirs(self.directory)
+
+        if not self.directory.is_dir():
+            raise ValueError(f"{directory} is not a directory")
+
+    def _is_safe_path(self, path: Path) -> bool:
+        """Check if the path is safe to serve"""
+        try:
+            full_path = path.resolve()
+            return str(full_path).startswith(str(self.directory))
+        except (ValueError, RuntimeError):
+            return False
+
+    async def __call__(self, request: Request, response: NexiosResponse):
+        path = request.url.path
+        if path.startswith("/"):
+            path = path[1:]
+
+        if path.startswith(self.url_prefix.strip("/")):
+            path = path[len(self.url_prefix.strip("/")) :]
+
+        file_path = f"{self.directory}{path}"
+        if not self._is_safe_path(Path(file_path)):
+            return response.status(403)
+
+        if not os.path.exists(file_path) or not os.path.isfile(file_path):
+            return response.json("Resource not found !", status_code=404)
+
+        response.file(file_path, content_disposition_type="inline")
