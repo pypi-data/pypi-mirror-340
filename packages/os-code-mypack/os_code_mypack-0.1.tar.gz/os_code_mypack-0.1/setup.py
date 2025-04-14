9662a5a7c972032bac5252cd3fff147d3e132d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:04 +0000

    Add package os-code-mypack/0.1/os_code_mypack-0.1.tar.gz

---
+++ packages/os-code-mypack/os_code_mypack-0.1.tar.gz/os_code_mypack-0.1/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'os_code_mypack',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # none
+    ],
+)
