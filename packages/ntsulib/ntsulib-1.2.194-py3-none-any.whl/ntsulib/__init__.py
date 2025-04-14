6205977af4dd9f18b43f2ea598d2e6ca7fc8bd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:21:08 +0000

    Add package ntsulib/1.2.194/ntsulib-1.2.194-py3-none-any.whl

---
+++ packages/ntsulib/ntsulib-1.2.194-py3-none-any.whl/ntsulib/__init__.py
@@ -0,0 +1,12 @@
+from .nasyncthread import *
+from .c import *
+from .ncommon import *
+from .nencry import *
+from .ndatabase import *
+from .npyqt5 import *
+from .nserverquery import *
+from .nsys import *
+from .nthread import *
+from .nregular import *
+
+__version__ = "1.2.194"
