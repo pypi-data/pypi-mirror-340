cd03b5844ee059d37bee10be7d9d83ff9152ed3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 05:05:36 +0000

    Add package ScriptSignals/0.1.2/scriptsignals-0.1.2.tar.gz

---
+++ packages/ScriptSignals/scriptsignals-0.1.2.tar.gz/scriptsignals-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ScriptSignals",
+    version="0.1.2",
+    description="esta libreria te permitira comunicar varios scripts de python",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Facundo Efimenco",
+    author_email="facu22251@gmail.com",
+    url="https://github.com/facu-programer/PySignal",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires=">=3.6",
+)
