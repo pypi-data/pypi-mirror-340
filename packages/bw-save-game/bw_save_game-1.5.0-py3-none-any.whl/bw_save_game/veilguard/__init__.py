a38293662ff70cf77269b249e457af26a4517300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 14:05:38 +0000

    Add package bw-save-game/1.5.0/bw_save_game-1.5.0-py3-none-any.whl

---
+++ packages/bw-save-game/bw_save_game-1.5.0-py3-none-any.whl/bw_save_game/veilguard/__init__.py
@@ -0,0 +1,5 @@
+from .data import *  # noqa: F401,F403
+from .highlevel import *  # noqa: F401,F403
+from .persistence import *  # noqa: F401,F403
+from .scripts import *  # noqa: F401,F403
+from .types import *  # noqa: F401,F403
