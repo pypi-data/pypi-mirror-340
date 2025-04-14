03e128f0b46c7641a37af304ce9d5e7c8d41fb95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 16:05:30 +0000

    Add package FontsHome/0.1.2/fontshome-0.1.2.tar.gz

---
+++ packages/FontsHome/fontshome-0.1.2.tar.gz/fontshome-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FontsHome",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    package_data={
+        '': ['FontsHome/fonts/*'],
+    },
+    author="YanXinle",
+    author_email="1020121123@qq.com",
+    url="https://gitee.com/yanxinle1123/FontsHome",
+)
