81f418155b234332d9d7f8550a6a4b5e2cc5698f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:22:07 +0000

    Add package readerllamaindex/0.1.2/readerllamaindex-0.1.2.tar.gz

---
+++ packages/readerllamaindex/readerllamaindex-0.1.2.tar.gz/readerllamaindex-0.1.2/test/test.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from readerllamaindex.pineconer import Pineconer
+
+load_dotenv()
+
+
+def test_pinecone():
+    pine = Pineconer()
+    index = pine.load()
+    storage_context = pine.get_storage(index)
