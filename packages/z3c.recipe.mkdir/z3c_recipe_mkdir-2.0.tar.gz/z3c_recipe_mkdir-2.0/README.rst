b4fdbb2622dda5781256e198fc61de7273154c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 15:10:42 +0000

    Add package z3c.recipe.mkdir/2.0/z3c_recipe_mkdir-2.0.tar.gz

---
+++ packages/z3c.recipe.mkdir/z3c_recipe_mkdir-2.0.tar.gz/z3c_recipe_mkdir-2.0/README.rst
@@ -0,0 +1,11 @@
+.. image:: https://readthedocs.org/projects/z3crecipemkdir/badge/?version=latest
+    :target: https://z3crecipemkdir.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+z3c.recipe.mkdir README
+***********************
+
+This package provides a ``zc.buildout`` recipe used to generate directories
+within a buildout.
+
+Please see ``docs/index.rst`` for detailed documentation.
---
+++ packages/z3c.recipe.mkdir/z3c_recipe_mkdir-2.0.tar.gz/z3c_recipe_mkdir-2.0/setup.py
@@ -0,0 +1,65 @@
+import os
+
+from setuptools import setup
+
+
+version = '2.0'
+
+with open("README.rst") as f:
+    README = f.read()
+
+with open(os.path.join("src", "z3c", "recipe", "mkdir", "README.rst")) as f:
+    DETAILED = f.read()
+
+with open("CHANGES.rst") as f:
+    CHANGES = f.read()
+
+
+setup(
+    name='z3c.recipe.mkdir',
+    version=version,
+    description="Buildout recipe to create directories.",
+    long_description=README + "\n\n" + CHANGES,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Buildout",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Zope Public License",
+    ],
+    keywords='buildout directory folder mkdir',
+    author='Uli Fouquet',
+    author_email='grok-dev@zope.org',
+    url='https://github.com/zopefoundation/z3c.recipe.mkdir',
+    license='ZPL-2.1',
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.9',
+    install_requires=[
+        'setuptools',
+        'zc.buildout >= 1.5',
+    ],
+    extras_require={
+        'test': [
+            'zope.testing',
+            'zope.testrunner >= 6.4',
+        ],
+        'testing': [
+            'zope.testing',
+            'zope.testrunner >= 6.4',
+        ],
+        'docs': ['Sphinx'],
+    },
+    entry_points="""
+      [zc.buildout]
+      default = z3c.recipe.mkdir:Recipe
+      """,
+)
