63f3216caf66ffd2101e5f7ea2f12d17414f1213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 14:05:29 +0000

    Add package vkbottle-types/5.199.63.6/vkbottle_types-5.199.63.6.tar.gz

---
+++ packages/vkbottle-types/vkbottle_types-5.199.63.6.tar.gz/vkbottle_types-5.199.63.6/vkbottle_types/responses/donut.py
@@ -0,0 +1,9 @@
+import inspect
+
+from vkbottle_types.codegen.responses.donut import *  # noqa: F403,F401
+from vkbottle_types.codegen.responses.groups import (  # noqa: F403,F401
+    GroupsGetMembersFieldsResponse,
+    GroupsGetMembersFieldsResponseModel,
+)
+
+from .base_response import BaseResponse
---
+++ packages/vkbottle-types/vkbottle_types-5.199.63.6.tar.gz/vkbottle_types-5.199.63.6/vkbottle_types/responses/pages.py
@@ -0,0 +1,5 @@
+import inspect
+
+from vkbottle_types.codegen.responses.pages import *  # noqa: F403,F401
+
+from .base_response import BaseResponse
