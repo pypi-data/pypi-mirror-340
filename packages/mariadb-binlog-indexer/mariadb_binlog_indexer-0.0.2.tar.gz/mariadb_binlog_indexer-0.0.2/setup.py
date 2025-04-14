9381d686bae5176dd2ca46737eb6d40f9b20a27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:22:56 +0000

    Add package mariadb-binlog-indexer/0.0.2/mariadb_binlog_indexer-0.0.2.tar.gz

---
+++ packages/mariadb-binlog-indexer/mariadb_binlog_indexer-0.0.2.tar.gz/mariadb_binlog_indexer-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="mariadb_binlog_indexer",
+    version="0.0.2",
+    description="MariaDB Binlog Indexer for Faster Querying",
+    long_description=open("README.md").read(),  # noqa: SIM115
+    long_description_content_type="text/markdown",
+    packages=["mariadb_binlog_indexer"],
+    package_data={
+        "mariadb_binlog_indexer": ["lib/*.h", "lib/*.so"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.10",
+    install_requires=["duckdb==1.2.2"],
+)
