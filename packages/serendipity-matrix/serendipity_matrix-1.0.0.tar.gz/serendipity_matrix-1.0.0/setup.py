e1788641a8e18997587f8db4712beab3541fb45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 16:05:49 +0000

    Add package serendipity-matrix/1.0.0/serendipity_matrix-1.0.0.tar.gz

---
+++ packages/serendipity-matrix/serendipity_matrix-1.0.0.tar.gz/serendipity_matrix-1.0.0/setup.py
@@ -0,0 +1,20 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+setup(
+    name = 'serendipity_matrix',
+    version = '1.0.0',
+    author = 'Jesús S. Aguilar-Ruiz, Alejandro García Conde',
+    #author_email=,
+    description = 'Serendipity Matrix',
+    long_description = (HERE / "README.md").read_text(encoding='utf-8'), 
+    long_description_content_type = "text/markdown",
+    #url=,
+    packages=["serendipity_matrix"],
+    # classifiers=,
+    # python_requires=,
+    install_requires = ["numpy","pandas","matplotlib"],
+    license = "BSD 3-Clause License"
+)
