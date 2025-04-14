4e87c1cb7ce8c0975fe9861646805745d85aa654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 04:06:13 +0000

    Add package finetune-cli/0.4.2.dev1/finetune_cli-0.4.2.dev1.tar.gz

---
+++ packages/finetune-cli/finetune_cli-0.4.2.dev1.tar.gz/finetune_cli-0.4.2.dev1/finetune/parquet/schemas/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+
+class SeedPrompt(BaseModel):
+    question: str
+
+
+class SeedPropmts(BaseModel):
+    seed_prompts: list[SeedPrompt]
+
+
+class FilterResult(BaseModel):
+    is_topic_meet_instructions: bool
+
+
+class Answer(BaseModel):
+    raw_topic: str
+    answer: str
+
+
+def pydantic_encoder(obj):
+    if isinstance(obj, BaseModel):
+        return obj.model_dump()
+    raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
