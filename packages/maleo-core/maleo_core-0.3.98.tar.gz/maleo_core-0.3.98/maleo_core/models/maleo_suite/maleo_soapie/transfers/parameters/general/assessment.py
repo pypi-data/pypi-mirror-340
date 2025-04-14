743f75bd007317db31cd198fcb2ac4d7a7bf22d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:21:59 +0000

    Add package maleo-core/0.3.98/maleo_core-0.3.98.tar.gz

---
+++ packages/maleo-core/maleo_core-0.3.98.tar.gz/maleo_core-0.3.98/maleo_core/models/maleo_suite/maleo_soapie/transfers/parameters/general/assessment.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from enum import StrEnum
+from pydantic import BaseModel, Field
+from maleo_core.models.base.transfers.parameters.general import BaseGeneralParameters
+from maleo_core.models.maleo_suite.maleo_soapie.transfers.general.assessment import MaleoSOAPIEAssessmentGeneralTransfers
+
+class MaleoSOAPIEAssessmentGeneralParameters:
+    class Expand(BaseModel):
+        expand:list[MaleoSOAPIEAssessmentGeneralTransfers.ExpandableFields] = Field([], description="Expanded field(s)")
+
+    class UniqueIdentifiers(StrEnum):
+        ID = "id"
+        UUID = "uuid"
+
+    class GetSingle(Expand, BaseGeneralParameters.GetSingle):
+        identifier:MaleoSOAPIEAssessmentGeneralParameters.UniqueIdentifiers = Field(..., description="Identifier")
+
+    class GetSingleQuery(Expand, BaseGeneralParameters.GetSingleQuery): pass
+
+    class CreateOrUpdate(Expand, MaleoSOAPIEAssessmentGeneralTransfers.Base): pass
