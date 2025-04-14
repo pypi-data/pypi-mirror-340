0cd04785dcea006547f1d3f73b9ce0623f9df12c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 04:06:13 +0000

    Add package dbt-sqlx/1.0.0/dbt_sqlx-1.0.0.tar.gz

---
+++ packages/dbt-sqlx/dbt_sqlx-1.0.0.tar.gz/dbt_sqlx-1.0.0/src/dbt_sqlx/support.py
@@ -0,0 +1,5 @@
+SUPPORTED_SQL_TYPES = {'oracle', 'sql-server', 'snowflake', 'redshift', 'postgres', 'mysql', 'bigquery','spark-sql'}
+SUPPORTED_PROVIDERS = ['OpenAI', 'Groq','Anthropic','MistralAI','Google_GenAI']
+PROVIDER_API_KEY_PREFIX = {
+    'mistralai': 'mistralai'
+}
