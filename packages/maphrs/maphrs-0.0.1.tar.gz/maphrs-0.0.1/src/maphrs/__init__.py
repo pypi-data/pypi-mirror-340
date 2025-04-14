2239ab3592a443ab8124b4e4c4a9498283cd553f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-14 03:22:52 +0000

    Add package maphrs/0.0.1/maphrs-0.0.1.tar.gz

---
+++ packages/maphrs/maphrs-0.0.1.tar.gz/maphrs-0.0.1/src/maphrs/__init__.py
@@ -0,0 +1,8 @@
+from maphrs.objects.eletronics import Resistor, COMMERCIAL_RESISTORS
+from maphrs.statistics.descriptive import relative_error
+
+__all__ = [
+    'Resistor',
+    'COMMERCIAL_RESISTORS',
+    'relative_error'
+]
