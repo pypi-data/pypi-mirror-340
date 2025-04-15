8d1622875936384b0fea615e6e1dc7ca71ec08ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:21:23 +0000

    Add package chonkie/1.0.3/chonkie-1.0.3.tar.gz

---
+++ packages/chonkie/chonkie-1.0.3.tar.gz/chonkie-1.0.3/src/chonkie/embeddings/__init__.py
@@ -0,0 +1,18 @@
+"""Embeddings classes for text embedding."""
+
+from .auto import AutoEmbeddings
+from .base import BaseEmbeddings
+from .cohere import CohereEmbeddings
+from .model2vec import Model2VecEmbeddings
+from .openai import OpenAIEmbeddings
+from .sentence_transformer import SentenceTransformerEmbeddings
+
+# Add all embeddings classes to __all__
+__all__ = [
+    "BaseEmbeddings",
+    "Model2VecEmbeddings",
+    "SentenceTransformerEmbeddings",
+    "OpenAIEmbeddings",
+    "CohereEmbeddings",
+    "AutoEmbeddings",
+]
