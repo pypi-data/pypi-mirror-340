f21e6aecbd447a24efa5fa2d5b75a6f59b53f959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:20:57 +0000

    Add package yyAutoAiframework/1.0.2/yyautoaiframework-1.0.2-py3-none-any.whl

---
+++ packages/yyAutoAiframework/yyautoaiframework-1.0.2-py3-none-any.whl/yyAutoAiframework/__init__.py
@@ -0,0 +1,11 @@
+# 导入子模块或类、函数
+from client import OpenAIClient,MoonshotClient,DeepSeekClient,DashscopeClient,BaseAIClient,ARKClient,QianFanClient
+from agi_common import PromptObject,DialogManagerFactory,AGIExtraParam,DialogManager,AGIResponse
+from util import AGIUtil,FileExtractUtil,RagUtil
+from compents import RagBotComponent
+from vector_db import BaseVectorDb,ChromaVectorDb,ESVectorDb
+
+# 定义 __all__ 列表，明确指定对外暴露的内容
+__all__ = ["AGIUtil", "FileExtractUtil","RagUtil","OpenAIClient", "MoonshotClient", "DeepSeekClient", "DashscopeClient","ARKClient",
+           "QianFanClient","BaseAIClient","PromptObject","DialogManagerFactory","AGIExtraParam","RagBotComponent",
+           "BaseVectorDb","ChromaVectorDb","ESVectorDb","DialogManager","AGIResponse"]
