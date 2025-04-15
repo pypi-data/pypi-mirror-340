d14c022cc3c58c1ef4fc5830af5297faa3ff013f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:07:03 +0000

    Add package fileformats-medimage-extras/0.10.2/fileformats_medimage_extras-0.10.2.tar.gz

---
+++ packages/fileformats-medimage-extras/fileformats_medimage_extras-0.10.2.tar.gz/fileformats_medimage_extras-0.10.2/fileformats/extras/medimage/tests/test_generators.py
@@ -0,0 +1,37 @@
+import pytest
+from fileformats.medimage import (
+    NiftiGzX,
+    NiftiGzXBvec,
+    T1w,
+    Dmri,
+    Fmri,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate,
+)
+
+
+def test_t1w_generator():
+    img = NiftiGzX[T1w].sample()
+    assert len(img.dims()) == 3
+
+
+def test_fmri_generator():
+    img = NiftiGzX[Fmri].sample()
+    assert len(img.dims()) == 4
+
+
+def test_dmri_generator():
+    img = NiftiGzXBvec[Dmri].sample()
+    assert len(img.dims()) == 4
+
+
+@pytest.mark.xfail(reason="Need to generate more realistic Siemens raw pet data")
+def test_siemens_pet_listmode_generator():
+    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode.sample()
+    assert img.metadata["PatientName"] == "FirstName^LastName"
+
+
+@pytest.mark.xfail(reason="Need to generate more realistic Siemens raw pet data")
+def test_siemens_pet_countrate_generator():
+    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate.sample()
+    assert img.metadata["PatientName"] == "FirstName^LastName"
