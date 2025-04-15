9cf5f288e237367bfd9915ccac48df27f3678708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:22:12 +0000

    Add package AskCheckSum/1.0.1/askchecksum-1.0.1-cp311-cp311-win_amd64.whl

---
+++ packages/AskCheckSum/askchecksum-1.0.1-cp311-cp311-win_amd64.whl/AskCheckSum.pyi
@@ -0,0 +1,19 @@
+
+
+"""
+askCheckSum 模块
+
+此模块提供用于计算数据库 Checksum 的功能。
+"""
+
+def get_ddb_checksum(szTest: str) -> int:
+    """
+    计算数据库 Checksum。
+
+    参数:
+        szTest (str): 输入字符串。
+
+    返回:
+        int: 计算出的 Checksum 值。
+    """
+    ...
