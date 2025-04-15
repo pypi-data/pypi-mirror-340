969b0e189a343e7c936e887fe953271d44095216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:20:49 +0000

    Add package maxminconquer/0.1.1/maxminconquer-0.1.1.tar.gz

---
+++ packages/maxminconquer/maxminconquer-0.1.1.tar.gz/maxminconquer-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="maxminconquer",
+    version="0.1.1",
+    packages=find_packages(),
+    description="A package to find the max and min values using divide and conquer.",
+    author="GIK",
+    author_email="giksecon28@gmail.com",
+    url="https://github.com/your-username/maxminconquer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
