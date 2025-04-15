35f4f3118b5f30f511dbc270ec9cdad2bfef3f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:21:13 +0000

    Add package bigraph-schema/0.0.54/bigraph-schema-0.0.54.tar.gz

---
+++ packages/bigraph-schema/bigraph-schema-0.0.54.tar.gz/bigraph-schema-0.0.54/bigraph_schema/edge.py
@@ -0,0 +1,49 @@
+"""
+====
+Edge
+====
+
+Base class for all edges in the bigraph schema.
+"""
+
+class Edge:
+    config_schema = {}
+
+
+    def __init__(self, config=None, core=None):
+        if core is None:
+            raise Exception('must provide a core')
+
+        self.core = core
+
+        if config is None:
+            config = {}
+
+        self.config = self.core.fill(
+            self.config_schema,
+            config)
+
+        self.initialize(self.config)
+
+
+    def initialize(self, config):
+        pass
+
+
+    def initial_state(self):
+        return {}
+
+
+    def inputs(self):
+        return {}
+
+
+    def outputs(self):
+        return {}
+
+
+    def interface(self):
+        """Returns the schema for this type"""
+        return {
+            'inputs': self.inputs(),
+            'outputs': self.outputs()}
