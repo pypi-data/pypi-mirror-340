6c74fe69f88c0916d1ca6395b667fc0689d4920a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:20:51 +0000

    Add package pymodaq_plugins_mockexamples/4.0.3/pymodaq_plugins_mockexamples-4.0.3.tar.gz

---
+++ packages/pymodaq_plugins_mockexamples/pymodaq_plugins_mockexamples-4.0.3.tar.gz/pymodaq_plugins_mockexamples-4.0.3/src/pymodaq_plugins_mockexamples/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from pymodaq.utils.logger import set_logger  # to be imported by other modules.
+from importlib.metadata import PackageNotFoundError, version
+
+from .utils import Config
+
+config = Config()
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    __version__ = '0.0.0dev'
+
