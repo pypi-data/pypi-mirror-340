c8bab3d76cc847ffc04723a988bd5928cfc0de84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:20:48 +0000

    Add package nullscream/0.1.4/nullscream-0.1.4.tar.gz

---
+++ packages/nullscream/nullscream-0.1.4.tar.gz/nullscream-0.1.4/src/nullscream/noop_class.py
@@ -0,0 +1,32 @@
+from nullscream.noop_function import noop
+
+
+class MagicType:
+    def __getattr__(self, name):
+        return MagicType()
+
+    def __call__(self, *args, **kwargs):
+        return MagicType()
+
+    def __str__(self):
+        return "MagicType instance"
+
+    def __fspath__(self):
+        return ""
+
+    def __iter__(self):
+        return iter([])
+
+
+class NoopClass:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return MagicType()
+
+    def __getattr__(self, name):
+        return MagicType()
+
+    def __setattr__(self, key, value):
+        pass
