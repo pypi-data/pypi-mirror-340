0ad921213841167c6062cc7a238370ba33de832f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:20:47 +0000

    Add package aman-llm/0.1.4/aman_llm-0.1.4.tar.gz

---
+++ packages/aman-llm/aman_llm-0.1.4.tar.gz/aman_llm-0.1.4/aman_llm/llm.py
@@ -0,0 +1,40 @@
+# llm.py
+from agno.agent import Agent
+from agno.models.google import Gemini
+from agno.tools.shell import ShellTools
+from agno.tools.file import FileTools
+from dotenv import load_dotenv
+from agno.team.team import Team
+import os
+
+load_dotenv()
+
+def llm_call(query: str, stream: bool = False):
+    shell_tool = Agent(
+        name="Shell Agent",
+        role="Executes shell commands",
+        model=Gemini(id="gemini-2.0-flash", api_key=os.getenv("GOOGLE_API_KEY")),
+        tools=[ShellTools()],
+    )
+
+    file_writer = Agent(
+        name="File Agent",
+        role="Manages file operations",
+        model=Gemini(id="gemini-2.0-flash", api_key=os.getenv("GOOGLE_API_KEY")),
+        tools=[FileTools()],
+    )
+
+    editor = Team(
+        name="Senior Software Engineer",
+        mode="coordinate",
+        model=Gemini(id="gemini-2.0-flash", api_key=os.getenv("GOOGLE_API_KEY")),
+        members=[shell_tool, file_writer],
+        enable_agentic_context=True,
+        share_member_interactions=True,
+        show_members_responses=True,
+        markdown=True,
+    )
+    
+    response = editor.print_response(query, stream=stream)
+
+    return response
