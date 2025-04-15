adf1626ac19b6c2802771d1d8a5e60364d906e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:20:58 +0000

    Add package kcommand/0.1.0.0/kcommand-0.1.0.0.tar.gz

---
+++ packages/kcommand/kcommand-0.1.0.0.tar.gz/kcommand-0.1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="kokaito-git",
+    name="kcommand",
+    version="0.1.0.0",
+    description="KCommand",
+    author_email="kokaito.git@gmail.com",
+    url="https://github.com/kokaito-git/kcommand",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        "kmodels",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.13",
+    include_package_data=True,
+)
