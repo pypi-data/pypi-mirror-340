06222646fa365ca9b2972e2c780a149e91206634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:22:10 +0000

    Add package pdfplucker/0.2.3/pdfplucker-0.2.3.tar.gz

---
+++ packages/pdfplucker/pdfplucker-0.2.3.tar.gz/pdfplucker-0.2.3/src/__init__.py
@@ -0,0 +1,15 @@
+"""
+pdfplucker - Ferramenta para extração e processamento de documentos PDF
+"""
+
+__version__ = "0.2.3"
+
+from src.processor import process_batch, process_pdf, create_converter
+from src.utils import format_result, link_subtitles
+__all__ = [
+    "process_batch",
+    "process_pdf",
+    "create_converter",
+    "format_result",
+    "link_subtitles"
+]
