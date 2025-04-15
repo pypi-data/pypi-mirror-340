29cac91eeff8f15d4ccdbdb2937e7943a9dfd790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:06:54 +0000

    Add package datadog-api-client/2.34.0/datadog_api_client-2.34.0-py3-none-any.whl

---
+++ packages/datadog-api-client/datadog_api_client-2.34.0-py3-none-any.whl/datadog_api_client/v2/model/security_monitoring_standard_data_source.py
@@ -0,0 +1,50 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019-Present Datadog, Inc.
+from __future__ import annotations
+
+
+from datadog_api_client.model_utils import (
+    ModelSimple,
+    cached_property,
+)
+
+from typing import ClassVar
+
+
+class SecurityMonitoringStandardDataSource(ModelSimple):
+    """
+    Source of events, either logs or audit trail.
+
+    :param value: If omitted defaults to "logs". Must be one of ["logs", "audit", "app_sec_spans", "spans", "security_runtime", "network"].
+    :type value: str
+    """
+
+    allowed_values = {
+        "logs",
+        "audit",
+        "app_sec_spans",
+        "spans",
+        "security_runtime",
+        "network",
+    }
+    LOGS: ClassVar["SecurityMonitoringStandardDataSource"]
+    AUDIT: ClassVar["SecurityMonitoringStandardDataSource"]
+    APP_SEC_SPANS: ClassVar["SecurityMonitoringStandardDataSource"]
+    SPANS: ClassVar["SecurityMonitoringStandardDataSource"]
+    SECURITY_RUNTIME: ClassVar["SecurityMonitoringStandardDataSource"]
+    NETWORK: ClassVar["SecurityMonitoringStandardDataSource"]
+
+    @cached_property
+    def openapi_types(_):
+        return {
+            "value": (str,),
+        }
+
+
+SecurityMonitoringStandardDataSource.LOGS = SecurityMonitoringStandardDataSource("logs")
+SecurityMonitoringStandardDataSource.AUDIT = SecurityMonitoringStandardDataSource("audit")
+SecurityMonitoringStandardDataSource.APP_SEC_SPANS = SecurityMonitoringStandardDataSource("app_sec_spans")
+SecurityMonitoringStandardDataSource.SPANS = SecurityMonitoringStandardDataSource("spans")
+SecurityMonitoringStandardDataSource.SECURITY_RUNTIME = SecurityMonitoringStandardDataSource("security_runtime")
+SecurityMonitoringStandardDataSource.NETWORK = SecurityMonitoringStandardDataSource("network")
