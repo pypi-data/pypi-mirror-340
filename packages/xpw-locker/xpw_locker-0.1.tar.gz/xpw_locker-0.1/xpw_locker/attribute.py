98055b307a36ef13604a96cac592a1b2994b4a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:21:59 +0000

    Add package xpw-locker/0.1/xpw_locker-0.1.tar.gz

---
+++ packages/xpw-locker/xpw_locker-0.1.tar.gz/xpw_locker-0.1/xpw_locker/attribute.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+__project__ = "xpw-locker"
+__version__ = "0.1"
+__urlhome__ = "https://github.com/podboy/locker/"
+__description__ = "Access authentication"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
